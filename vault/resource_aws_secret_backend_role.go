36b40aa4791bf772c7b48495f22a4493eed08fb0
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-09-26 09:06:56 +0800

    Check error on setting aggregate types

--- vault/resource_aws_secret_backend_role.go
+++ vault/resource_aws_secret_backend_role.go
@@ -168,9 +168,14 @@ func awsSecretBackendRoleRead(d *schema.ResourceData, meta interface{}) error {
 
 		if _, ok := d.GetOk("credential_type"); ok {
 			d.Set("credential_type", credentialTypes[0])
-			d.Set("policy_arns", secret.Data["policy_arns"])
-			d.Set("role_arns", secret.Data["role_arns"])
 			d.Set("policy_document", secret.Data["policy_document"])
+
+			if err := d.Set("role_arns", secret.Data["role_arns"]); err != nil {
+				return fmt.Errorf("error setting role_arns for role %q: %s", path, err)
+			}
+			if err := d.Set("policy_arns", secret.Data["policy_arns"]); err != nil {
+				return fmt.Errorf("error setting policy_arns for role %q: %s", path, err)
+			}
 
 		} else {
 			// Deprecated style
