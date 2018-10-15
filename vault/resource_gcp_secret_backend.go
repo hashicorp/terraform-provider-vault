da16d300bca627bd559a4a1cdbc59e5cbda8a7ca
Gavin Williams <fatmcgav@gmail.com>
2018-10-15 12:27:14 +0100

    [gcp] Add new `vault_gcp_secret_backend` resource for managing GCP Secret backends [1]
    This requires an upstream change to Vault's `api.MountConfigInput` type
    which will be raised as a seperate PR.
    
    [1] https://www.vaultproject.io/docs/secrets/gcp/index.html

---
+++ vault/resource_gcp_secret_backend.go
@@ -0,0 +1,189 @@
+package vault
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/vault/api"
+)
+
+func gcpSecretBackendResource() *schema.Resource {
+	return &schema.Resource{
+		Create: gcpSecretBackendCreate,
+		Read:   gcpSecretBackendRead,
+		Update: gcpSecretBackendUpdate,
+		Delete: gcpSecretBackendDelete,
+		Exists: gcpSecretBackendExists,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"path": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Default:     "gcp",
+				Description: "Path to mount the backend at.",
+				ValidateFunc: func(v interface{}, k string) (ws []string, errs []error) {
+					value := v.(string)
+					if strings.HasSuffix(value, "/") {
+						errs = append(errs, fmt.Errorf("path cannot end in '/'"))
+					}
+					return
+				},
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					return old+"/" == new || new+"/" == old
+				},
+			},
+			"credentials": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "JSON-encoded credentials to use to connect to GCP",
+				Sensitive:   true,
+				// We rebuild the attached JSON string to a simple singleline
+				// string. This makes terraform not want to change when an extra
+				// space is included in the JSON string. It is also necesarry
+				// when disable_read is false for comparing values.
+				StateFunc:    NormalizeDataJSON,
+				ValidateFunc: ValidateDataJSON,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "Human-friendly description of the mount for the backend.",
+			},
+			"default_lease_ttl_seconds": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     "3600",
+				Description: "Default lease duration for secrets in seconds",
+			},
+			"max_lease_ttl_seconds": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     "86400",
+				Description: "Maximum possible lease duration for secrets in seconds",
+			},
+		},
+	}
+}
+
+func gcpSecretBackendCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	path := d.Get("path").(string)
+	description := d.Get("description").(string)
+	defaultTTL := d.Get("default_lease_ttl_seconds").(int)
+	maxTTL := d.Get("max_lease_ttl_seconds").(int)
+	credentials := d.Get("credentials").(string)
+
+	d.Partial(true)
+	log.Printf("[DEBUG] Mounting GCP backend at %q", path)
+	err := client.Sys().Mount(path, &api.MountInput{
+		Type:        "gcp",
+		Description: description,
+		Config: api.MountConfigInput{
+			Credentials:     credentials,
+			DefaultLeaseTTL: fmt.Sprintf("%ds", defaultTTL),
+			MaxLeaseTTL:     fmt.Sprintf("%ds", maxTTL),
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("Error mounting to %q: %s", path, err)
+	}
+	log.Printf("[DEBUG] Mounted GCP backend at %q", path)
+	d.SetId(path)
+
+	d.SetPartial("path")
+	d.SetPartial("description")
+	d.SetPartial("default_lease_ttl_seconds")
+	d.SetPartial("max_lease_ttl_seconds")
+	d.Partial(false)
+
+	return gcpSecretBackendRead(d, meta)
+}
+
+func gcpSecretBackendRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	path := d.Id()
+
+	// TODO: Fill this out
+	log.Printf("[DEBUG] Reading GCP backend mount %q from Vault", path)
+	mounts, err := client.Sys().ListMounts()
+	if err != nil {
+		return fmt.Errorf("error reading mount %q: %s", path, err)
+	}
+	log.Printf("[DEBUG] Read GCP backend mount %q from Vault", path)
+
+	// the API always returns the path with a trailing slash, so let's make
+	// sure we always specify it as a trailing slash.
+	mount, ok := mounts[strings.Trim(path, "/")+"/"]
+	if !ok {
+		log.Printf("[WARN] Mount %q not found, removing backend from state.", path)
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("path", path)
+	d.Set("description", mount.Description)
+	d.Set("default_lease_ttl_seconds", mount.Config.DefaultLeaseTTL)
+	d.Set("max_lease_ttl_seconds", mount.Config.MaxLeaseTTL)
+
+	return nil
+}
+
+func gcpSecretBackendUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	path := d.Id()
+	d.Partial(true)
+	if d.HasChange("default_lease_ttl_seconds") || d.HasChange("max_lease_ttl_seconds") {
+		config := api.MountConfigInput{
+			DefaultLeaseTTL: fmt.Sprintf("%ds", d.Get("default_lease_ttl_seconds")),
+			MaxLeaseTTL:     fmt.Sprintf("%ds", d.Get("max_lease_ttl_seconds")),
+		}
+		log.Printf("[DEBUG] Updating lease TTLs for %q", path)
+		err := client.Sys().TuneMount(path, config)
+		if err != nil {
+			return fmt.Errorf("error updating mount TTLs for %q: %s", path, err)
+		}
+		log.Printf("[DEBUG] Updated lease TTLs for %q", path)
+		d.SetPartial("default_lease_ttl_seconds")
+		d.SetPartial("max_lease_ttl_seconds")
+	}
+
+	d.Partial(false)
+	return gcpSecretBackendRead(d, meta)
+}
+
+func gcpSecretBackendDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	path := d.Id()
+
+	log.Printf("[DEBUG] Unmounting GCP backend %q", path)
+	err := client.Sys().Unmount(path)
+	if err != nil {
+		return fmt.Errorf("Error unmounting GCP backend from %q: %s", path, err)
+	}
+	log.Printf("[DEBUG] Unmounted GCP backend %q", path)
+	return nil
+}
+
+func gcpSecretBackendExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	client := meta.(*api.Client)
+	path := d.Id()
+	log.Printf("[DEBUG] Checking if GCP backend exists at %q", path)
+	mounts, err := client.Sys().ListMounts()
+	if err != nil {
+		return true, fmt.Errorf("Error retrieving list of mounts: %s", err)
+	}
+	log.Printf("[DEBUG] Checked if GCP backend exists at %q", path)
+	_, ok := mounts[strings.Trim(path, "/")+"/"]
+	return ok, nil
+}
---
+++ vault/resource_gcp_secret_backend_test.go
@@ -0,0 +1,94 @@
+package vault
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/vault/api"
+)
+
+func TestGCPSecretBackend(t *testing.T) {
+	path := acctest.RandomWithPrefix("tf-test-gcp")
+	resource.Test(t, resource.TestCase{
+		Providers:    testProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccGCPSecretBackendCheckDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testGCPSecretBackend_initialConfig(path),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "path", path),
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "description", "test description"),
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "default_lease_ttl_seconds", "3600"),
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "max_lease_ttl_seconds", "86400"),
+				),
+			},
+			{
+				Config: testGCPSecretBackend_updateConfig(path),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "path", path),
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "description", "test description"),
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "default_lease_ttl_seconds", "1800"),
+					resource.TestCheckResourceAttr("vault_gcp_secret_backend.test", "max_lease_ttl_seconds", "43200"),
+				),
+			},
+		},
+	})
+}
+
+func testAccGCPSecretBackendCheckDestroy(s *terraform.State) error {
+	client := testProvider.Meta().(*api.Client)
+
+	mounts, err := client.Sys().ListMounts()
+	if err != nil {
+		return err
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "vault_gcp_secret_backend" {
+			continue
+		}
+		for path, mount := range mounts {
+			path = strings.Trim(path, "/")
+			rsPath := strings.Trim(rs.Primary.Attributes["path"], "/")
+			if mount.Type == "gcp" && path == rsPath {
+				return fmt.Errorf("Mount %q still exists", path)
+			}
+		}
+	}
+	return nil
+}
+
+func testGCPSecretBackend_initialConfig(path string) string {
+	return fmt.Sprintf(`
+resource "vault_gcp_secret_backend" "test" {
+  path = "%s"
+  credentials = <<EOF
+{
+  "hello": "world"
+}
+EOF
+  description = "test description"
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds = 86400
+}`, path)
+}
+
+func testGCPSecretBackend_updateConfig(path string) string {
+	return fmt.Sprintf(`
+resource "vault_gcp_secret_backend" "test" {
+  path = "%s"
+  credentials = <<EOF
+{
+  "hello": "world"
+}
+EOF
+  description = "test description"
+  default_lease_ttl_seconds = 1800
+  max_lease_ttl_seconds = 43200
+}`, path)
+}
