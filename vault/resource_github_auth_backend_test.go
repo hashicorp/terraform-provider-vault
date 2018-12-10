1096a36867e7408a97f4bcf72df4d3513f106a46
cvbarros <carlosvitorbarros@gmail.com>
2018-12-10 16:40:59 +0100

    Add test checkers for auth mounts

--- vault/resource_github_auth_backend_test.go
+++ vault/resource_github_auth_backend_test.go
@@ -2,30 +2,39 @@ package vault
 
 import (
 	"fmt"
+	"log"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/vault/api"
 )
 
 func TestAccGithubAuthBackend_basic(t *testing.T) {
 	backend := acctest.RandomWithPrefix("github")
+	resName := "vault_github_auth_backend.gh"
+	var resAuth api.AuthMount
 	resource.Test(t, resource.TestCase{
-		Providers: testProviders,
-		PreCheck:  func() { testAccPreCheck(t) },
+		Providers:    testProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckGithubAuthMountDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccGithubAuthBackendConfig_basic(backend),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "path", backend),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "organization", "vault"),
+					testAccCheckAuthMountExists(resName, &resAuth),
+					resource.TestCheckResourceAttr(resName, "path", backend),
+					resource.TestCheckResourceAttr(resName, "organization", "vault"),
 				),
 			},
 			{
 				Config: testAccGithubAuthBackendConfig_updated(backend),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "path", backend),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "organization", "other_vault"),
+					testAccCheckAuthMountExists(resName, &resAuth),
+					resource.TestCheckResourceAttr(resName, "path", backend),
+					resource.TestCheckResourceAttr(resName, "organization", "other_vault"),
 				),
 			},
 		},
@@ -34,42 +43,47 @@ func TestAccGithubAuthBackend_basic(t *testing.T) {
 
 func TestAccGithubAuthBackend_tuning(t *testing.T) {
 	backend := acctest.RandomWithPrefix("github")
+	resName := "vault_github_auth_backend.gh"
+	var resAuth api.AuthMount
 	resource.Test(t, resource.TestCase{
-		Providers: testProviders,
-		PreCheck:  func() { testAccPreCheck(t) },
+		Providers:    testProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckGithubAuthMountDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccGithubAuthBackendConfig_tuning(backend),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "path", backend),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.default_lease_ttl", "10"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.max_lease_ttl", "20"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.listing_visibility", ""),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.audit_non_hmac_request_keys.#", "2"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.audit_non_hmac_request_keys.0", "key1"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.audit_non_hmac_request_keys.1", "key2"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.audit_non_hmac_response_keys.#", "2"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.audit_non_hmac_response_keys.0", "key3"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.audit_non_hmac_response_keys.1", "key4"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.passthrough_request_headers.#", "2"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.passthrough_request_headers.0", "X-Custom-Header"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.4271161690.passthrough_request_headers.1", "X-Forwarded-To"),
+					testAccCheckAuthMountExists(resName, &resAuth),
+					resource.TestCheckResourceAttr(resName, "path", backend),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.default_lease_ttl", "10"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.max_lease_ttl", "20"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.listing_visibility", ""),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.audit_non_hmac_request_keys.#", "2"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.audit_non_hmac_request_keys.0", "key1"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.audit_non_hmac_request_keys.1", "key2"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.audit_non_hmac_response_keys.#", "2"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.audit_non_hmac_response_keys.0", "key3"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.audit_non_hmac_response_keys.1", "key4"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.passthrough_request_headers.#", "2"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.passthrough_request_headers.0", "X-Custom-Header"),
+					resource.TestCheckResourceAttr(resName, "tune.4271161690.passthrough_request_headers.1", "X-Forwarded-To"),
 				),
 			},
 			{
 				Config: testAccGithubAuthBackendConfig_tuningUpdated(backend),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "path", backend),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.default_lease_ttl", "50"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.max_lease_ttl", "70"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.listing_visibility", "unauth"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.audit_non_hmac_request_keys.#", "1"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.audit_non_hmac_request_keys.0", "key1"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.audit_non_hmac_response_keys.#", "0"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.passthrough_request_headers.#", "3"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.passthrough_request_headers.0", "X-Custom-Header"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.passthrough_request_headers.1", "X-Forwarded-To"),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "tune.2934484151.passthrough_request_headers.2", "X-Mas"),
+					testAccCheckAuthMountExists(resName, &resAuth),
+					resource.TestCheckResourceAttr(resName, "path", backend),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.default_lease_ttl", "50"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.max_lease_ttl", "70"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.listing_visibility", "unauth"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.audit_non_hmac_request_keys.#", "1"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.audit_non_hmac_request_keys.0", "key1"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.audit_non_hmac_response_keys.#", "0"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.passthrough_request_headers.#", "3"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.passthrough_request_headers.0", "X-Custom-Header"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.passthrough_request_headers.1", "X-Forwarded-To"),
+					resource.TestCheckResourceAttr(resName, "tune.2934484151.passthrough_request_headers.2", "X-Mas"),
 				),
 			},
 		},
@@ -78,26 +92,91 @@ func TestAccGithubAuthBackend_tuning(t *testing.T) {
 
 func TestAccGithubAuthBackend_description(t *testing.T) {
 	backend := acctest.RandomWithPrefix("github")
+	resName := "vault_github_auth_backend.gh"
+	var resAuth api.AuthMount
 	resource.Test(t, resource.TestCase{
-		Providers: testProviders,
-		PreCheck:  func() { testAccPreCheck(t) },
+		Providers:    testProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckGithubAuthMountDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccGithubAuthBackendConfig_description(backend, "Github Auth Mount"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "path", backend),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "description", "Github Auth Mount"),
+					testAccCheckAuthMountExists(resName, &resAuth),
+					resource.TestCheckResourceAttr(resName, "path", backend),
+					resource.TestCheckResourceAttr(resName, "description", "Github Auth Mount"),
 				),
 			},
 			{
 				Config: testAccGithubAuthBackendConfig_description(backend, "Github Auth Mount Updated"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "path", backend),
-					resource.TestCheckResourceAttr("vault_github_auth_backend.gh", "description", "Github Auth Mount Updated"),
+					testAccCheckAuthMountExists(resName, &resAuth),
+					resource.TestCheckResourceAttr(resName, "path", backend),
+					resource.TestCheckResourceAttr(resName, "description", "Github Auth Mount Updated"),
 				),
 			},
 		},
 	})
+}
+func testAccCheckAuthMountExists(n string, out *api.AuthMount) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testProvider.Meta().(*api.Client)
+		return authMountExistsHelper(n, s, client, out)
+	}
+}
+
+func testAccCheckGithubAuthMountDestroy(s *terraform.State) error {
+	return testAccCheckAuthMountDestroy(s, "vault_github_auth_backend")
+}
+
+func testAccCheckAuthMountDestroy(s *terraform.State, resType string) error {
+	client := testProvider.Meta().(*api.Client)
+	return authMountDestroyHelper(s, client, resType)
+}
+
+func authMountExistsHelper(n string, s *terraform.State, client *api.Client, out *api.AuthMount) error {
+	rs, ok := s.RootModule().Resources[n]
+	if !ok {
+		return fmt.Errorf("Not found: %s", n)
+	}
+
+	if rs.Primary.ID == "" {
+		return fmt.Errorf("No id for %s is set", n)
+	}
+
+	auths, err := client.Sys().ListAuth()
+	if err != nil {
+		return fmt.Errorf("error reading from Vault: %s", err)
+	}
+
+	resp := auths[strings.Trim(rs.Primary.ID, "/")+"/"]
+	if resp == nil {
+		return fmt.Errorf("auth mount %s not present", rs.Primary.ID)
+	}
+	log.Printf("[INFO] Auth mount resource '%v' confirmed to exist at path: %v", n, rs.Primary.ID)
+	*out = *resp
+
+	return nil
+}
+
+func authMountDestroyHelper(s *terraform.State, client *api.Client, resType string) error {
+	for _, r := range s.RootModule().Resources {
+		if r.Type != resType {
+			continue
+		}
+
+		auths, err := client.Sys().ListAuth()
+		if err != nil {
+			return fmt.Errorf("error reading from Vault: %s", err)
+		}
+
+		resp := auths[strings.Trim(r.Primary.ID, "/")+"/"]
+		if resp == nil {
+			log.Printf("[INFO] Auth mount resource confirmed to be destroyed from path: %v", r.Primary.ID)
+			return nil
+		}
+	}
+	return fmt.Errorf("Auth mount resource still exists")
 }
 
 func testAccGithubAuthBackendConfig_basic(backend string) string {
