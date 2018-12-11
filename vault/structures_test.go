dca070eddee057f46ea32e6e7540a5f966af890a
cvbarros <carlosvitorbarros@gmail.com>
2018-12-11 09:08:28 +0100

    Fix failing structure test

--- vault/structures_test.go
+++ vault/structures_test.go
@@ -10,8 +10,8 @@ import (
 func TestExpandAuthMethodTune(t *testing.T) {
 	flattened := []interface{}{
 		map[string]interface{}{
-			"default_lease_ttl":           10,
-			"max_lease_ttl":               20,
+			"default_lease_ttl":           "10m",
+			"max_lease_ttl":               "20m",
 			"audit_non_hmac_request_keys": []interface{}{"foo", "bar"},
 			"listing_visibility":          "unauth",
 			"passthrough_request_headers": []interface{}{"X-Custom", "X-Mas"},
@@ -19,8 +19,8 @@ func TestExpandAuthMethodTune(t *testing.T) {
 	}
 	actual := expandAuthMethodTune(flattened)
 	expected := api.MountConfigInput{
-		DefaultLeaseTTL:           "10",
-		MaxLeaseTTL:               "20",
+		DefaultLeaseTTL:           "10m",
+		MaxLeaseTTL:               "20m",
 		AuditNonHMACRequestKeys:   []string{"foo", "bar"},
 		AuditNonHMACResponseKeys:  nil,
 		ListingVisibility:         "unauth",
@@ -37,16 +37,16 @@ func TestExpandAuthMethodTune(t *testing.T) {
 
 func TestFlattenAuthMethodTune(t *testing.T) {
 	expanded := &api.MountConfigOutput{
-		DefaultLeaseTTL:           10,
-		MaxLeaseTTL:               20,
+		DefaultLeaseTTL:           600,
+		MaxLeaseTTL:               1200,
 		AuditNonHMACRequestKeys:   []string{"foo", "bar"},
 		ListingVisibility:         "",
 		PassthroughRequestHeaders: []string{"X-Custom", "X-Mas"},
 	}
 
 	expected := map[string]interface{}{
-		"default_lease_ttl":           10,
-		"max_lease_ttl":               20,
+		"default_lease_ttl":           "10m",
+		"max_lease_ttl":               "20m",
 		"audit_non_hmac_request_keys": []interface{}{"foo", "bar"},
 		"passthrough_request_headers": []interface{}{"X-Custom", "X-Mas"},
 		"listing_visibility":          "",
