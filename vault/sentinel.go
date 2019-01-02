04765043ddb55dddc75a43a3d583ef63f666be22
Sergey Trasko <sergey.trasko@gmail.com>
2019-01-02 15:32:55 +0200

    Added support for EGP and RGP (sentinel)

---
+++ vault/sentinel.go
@@ -0,0 +1,131 @@
+package vault
+
+import (
+	"fmt"
+	"context"
+	"errors"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/terraform/helper/schema"
+	"log"
+)
+
+func readSentinelPolicy(client *api.Client, policyType string, name string) (map[string]interface{}, error) {
+	r := client.NewRequest("GET", fmt.Sprintf("/v1/sys/policies/%s/%s", policyType, name))
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	resp, err := client.RawRequestWithContext(ctx, r)
+	if resp != nil {
+		defer resp.Body.Close()
+		if resp.StatusCode == 404 {
+			return nil, nil
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	secret, err := api.ParseSecret(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if secret == nil || secret.Data == nil {
+		return nil, errors.New("data from server response is empty")
+	}
+	return secret.Data, nil
+}
+
+func PutSentinelPolicy(client *api.Client, policyType string, name string, body map[string]interface{}) error {
+	r := client.NewRequest("PUT", fmt.Sprintf("/v1/sys/policies/%s/%s", policyType, name))
+	if err := r.SetJSONBody(body); err != nil {
+		return err
+	}
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	resp, err := client.RawRequestWithContext(ctx, r)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func DeleteSentinelPolicy(client *api.Client, policyType string, name string) error {
+	r := client.NewRequest("DELETE", fmt.Sprintf("/v1/sys/policies/%s/%s", policyType, name))
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	resp, err := client.RawRequestWithContext(ctx, r)
+	if err == nil {
+		defer resp.Body.Close()
+	}
+	return err
+}
+
+func ValidateSentinelEnforcementLevel(v interface{}, k string) (ws []string, errs []error) {
+	value := v.(string)
+	if value != "advisory" && value != "soft-mandatory" && value != "hard-mandatory" {
+		errs = append(errs, fmt.Errorf("unexpected value %s", value))
+	}
+	return
+}
+
+func sentinelPolicyDelete(policyType string, d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	name := d.Id()
+
+	log.Printf("[DEBUG] Deleting %s policy %s from Vault", policyType, name)
+
+	err := DeleteSentinelPolicy(client, policyType, name)
+	if err != nil {
+		return fmt.Errorf("error deleting from Vault: %s", err)
+	}
+
+	return nil
+}
+
+func sentinelPolicyRead(policyType string, attributes []string, d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	name := d.Id()
+
+	policy, err := readSentinelPolicy(client, policyType, name)
+
+	if err != nil {
+		return fmt.Errorf("error reading from Vault: %s", err)
+	}
+
+	for _, value := range attributes {
+		d.Set(value, policy[value])
+	}
+	d.Set("name", name)
+
+	return nil
+}
+
+func sentinelPolicyWrite(policyType string, attributes []string, d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	name := d.Get("name").(string)
+
+	log.Printf("[DEBUG] Writing %s policy %s to Vault", policyType, name)
+	body := map[string]interface{}{
+
+	};
+	for _, value := range attributes {
+		body[value] = d.Get(value)
+	}
+
+	err := PutSentinelPolicy(client, policyType, name, body)
+
+	if err != nil {
+		return fmt.Errorf("error writing to Vault: %s", err)
+	}
+
+	d.SetId(name)
+
+	return sentinelPolicyRead(policyType, attributes, d, meta)
+}
---
+++ website/docs/r/rgp_policy.html.md
@@ -0,0 +1,42 @@
+---
+layout: "vault"
+page_title: "Vault: vault_rgp_policy resource"
+sidebar_current: "docs-vault-resource-rgp-policy"
+description: |-
+  Writes Sentinel role governing policies for Vault
+---
+
+# vault\_rgp\_policy
+
+Provides a resource to manage Role Governing Policy (RGP) via [Sentinel](https://www.vaultproject.io/docs/enterprise/sentinel/index.html).
+
+**Note** this feature is available only with Vault Enterprise.
+
+## Example Usage
+
+```hcl
+resource "vault_rgp_policy" "allow-all" {
+  name = "allow-all"
+  enforcement_level = "soft-mandatory"
+
+  policy = <<EOT
+main = rule {
+  true
+}
+EOT
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) The name of the policy
+
+* `enforcement_level` - (Required) Enforcement level of Sentinel policy. Can be either `advisory` or `soft-mandatory` or `hard-mandatory`
+
+* `policy` - (Required) String containing a Sentinel policy
+
+## Attributes Reference
+
+No additional attributes are exported by this resource.
