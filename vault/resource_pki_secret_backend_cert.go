52d2617836791fe5066655960b043a69046ea424
Jesse Nelson <spheromak@gmail.com>
2019-02-19 12:36:24 -0800

    Problem: Nil ca chain in response causes segfault
    Solution: guard for a nil response for ca chain.

--- vault/resource_pki_secret_backend_cert.go
+++ vault/resource_pki_secret_backend_cert.go
@@ -182,9 +182,13 @@ func pkiSecretBackendCertCreate(d *schema.ResourceData, meta interface{}) error 
 	}
 	log.Printf("[DEBUG] Created certificate %s by %s on PKI secret backend %q", commonName, name, backend)
 
+	caChain := resp.Data["ca_chain"]
+	if caChain != nil {
+		d.Set("ca_chain", strings.Join(convertIntoSliceOfString(caChain)[:], "\n"))
+	}
+
 	d.Set("certificate", resp.Data["certificate"])
 	d.Set("issuing_ca", resp.Data["issuing_ca"])
-	d.Set("ca_chain", strings.Join(convertIntoSliceOfString(resp.Data["ca_chain"])[:], "\n"))
 	d.Set("private_key", resp.Data["private_key"])
 	d.Set("private_key_type", resp.Data["private_key_type"])
 	d.Set("serial_number", resp.Data["serial_number"])
