a606c52c8ab6e282f314279da338f020476d527d
aromeyer <aromeyer@vente-privee.com>
2019-03-20 11:38:53 +0000

    correct vendor.json with correct revision

--- vendor/github.com/hashicorp/vault-plugin-auth-jwt/backend.go
+++ vendor/github.com/hashicorp/vault-plugin-auth-jwt/backend.go
@@ -3,12 +3,10 @@ package jwtauth
 import (
 	"context"
 	"sync"
-	"time"
 
 	oidc "github.com/coreos/go-oidc"
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
-	cache "github.com/patrickmn/go-cache"
 )
 
 const (
@@ -18,7 +16,7 @@ const (
 
 // Factory is used by framework
 func Factory(ctx context.Context, c *logical.BackendConfig) (logical.Backend, error) {
-	b := backend()
+	b := backend(c)
 	if err := b.Setup(ctx, c); err != nil {
 		return nil, err
 	}
@@ -31,16 +29,14 @@ type jwtAuthBackend struct {
 	l            sync.RWMutex
 	provider     *oidc.Provider
 	cachedConfig *jwtConfig
-	oidcStates   *cache.Cache
 
 	providerCtx       context.Context
 	providerCtxCancel context.CancelFunc
 }
 
-func backend() *jwtAuthBackend {
+func backend(c *logical.BackendConfig) *jwtAuthBackend {
 	b := new(jwtAuthBackend)
 	b.providerCtx, b.providerCtxCancel = context.WithCancel(context.Background())
-	b.oidcStates = cache.New(oidcStateTimeout, 1*time.Minute)
 
 	b.Backend = &framework.Backend{
 		AuthRenew:   b.pathLoginRenew,
@@ -50,11 +46,6 @@ func backend() *jwtAuthBackend {
 		PathsSpecial: &logical.Paths{
 			Unauthenticated: []string{
 				"login",
-				"oidc/auth_url",
-				"oidc/callback",
-
-				// Uncomment to mount simple UI handler for local development
-				// "ui",
 			},
 			SealWrapStorage: []string{
 				"config",
@@ -66,11 +57,7 @@ func backend() *jwtAuthBackend {
 				pathRoleList(b),
 				pathRole(b),
 				pathConfig(b),
-
-				// Uncomment to mount simple UI handler for local development
-				// pathUI(b),
 			},
-			pathOIDC(b),
 		),
 		Clean: b.cleanup,
 	}
