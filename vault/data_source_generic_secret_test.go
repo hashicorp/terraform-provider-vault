85b086e639052328de9141341cced7d00626712f
Paddy Carver <paddy@carvers.co>
2019-05-23 12:04:29 -0700

    Fix V2GenericSecret test value.
    The test case for updating a v2 generic secret had a check function that
    hardcoded in the initial value, and then proceeded to check that against
    the updated value, which would never work.
    
    This updates to add a check function for the updated config, with the
    updated value coded in.

--- vault/data_source_generic_secret_test.go
+++ vault/data_source_generic_secret_test.go
@@ -32,7 +32,7 @@ func TestV2Secret(t *testing.T) {
 			},
 			{
 				Config: testv2DataSourceGenericSecretUpdated_config,
-				Check:  testDataSourceGenericSecret_check,
+				Check:  testDataSourceGenericSecretUpdated_check,
 			},
 		},
 	})
@@ -115,6 +115,29 @@ func testDataSourceGenericSecret_check(s *terraform.State) error {
 	}
 
 	if got, want := iState.Attributes["data.zip"], "zap"; got != want {
+		return fmt.Errorf("data[\"zip\"] contains %s; want %s", got, want)
+	}
+
+	return nil
+}
+
+func testDataSourceGenericSecretUpdated_check(s *terraform.State) error {
+	resourceState := s.Modules[0].Resources["data.vault_generic_secret.test"]
+	if resourceState == nil {
+		return fmt.Errorf("resource not found in state %v", s.Modules[0].Resources)
+	}
+
+	iState := resourceState.Primary
+	if iState == nil {
+		return fmt.Errorf("resource has no primary instance")
+	}
+
+	wantJson := `{"zip":"kablamo"}`
+	if got, want := iState.Attributes["data_json"], wantJson; got != want {
+		return fmt.Errorf("data_json contains %s; want %s", got, want)
+	}
+
+	if got, want := iState.Attributes["data.zip"], "kablamo"; got != want {
 		return fmt.Errorf("data[\"zip\"] contains %s; want %s", got, want)
 	}
 
