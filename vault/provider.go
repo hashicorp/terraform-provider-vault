de3d6f6adff589fe72be27cb9269657f851ef21c
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-08-07 14:49:14 +0800

    Use Mutex to lock Identity Group Policies

--- vault/provider.go
+++ vault/provider.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform/helper/logging"
+	"github.com/hashicorp/terraform/helper/mutexkv"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/hashicorp/vault/api"
@@ -25,6 +26,12 @@ const (
 	// We aim to deprecate items in this category.
 	UnknownPath = "unknown"
 )
+
+// This is a global MutexKV for use within this provider.
+// Use this when you need to have multiple resources or even multiple instances
+// of the same resource write to the same path in Vault.
+// The key of the mutex should be the path in Vault.
+var vaultMutexKV = mutexkv.NewMutexKV()
 
 func Provider() terraform.ResourceProvider {
 	dataSourcesMap, err := parse(DataSourceRegistry)
