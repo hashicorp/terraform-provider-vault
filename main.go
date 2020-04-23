b36394c23b49e98ca2f9925355e06ca94880ba77
Becca Petrin <beccapetrin@gmail.com>
2020-04-23 11:43:43 -0700

    move provider to separate package

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/terraform-providers/terraform-provider-vault/provider"
 	"github.com/terraform-providers/terraform-provider-vault/vault"
 )
 
 func main() {
+	p := provider.New(vault.Provider())
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: vault.Provider})
+		ProviderFunc: p.ResourceProvider})
 }
