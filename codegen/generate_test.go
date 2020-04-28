79a3b6c1e0ecf4dce6ccc947657b595c8b116965
Becca Petrin <beccapetrin@gmail.com>
2020-04-28 15:41:15 -0700

    refactor test to use testcases rather than indexes

--- codegen/generate_test.go
+++ codegen/generate_test.go
@@ -4,89 +4,90 @@ import (
 	"testing"
 )
 
-// testPaths is a sampling of real paths that
-// originate in the OpenAPI doc, pulled via:
-// $ cat testdata/openapi.json | jq '.paths' | jq 'keys[]'
-var testPaths = []string{
-	"/database/roles",
-	"/database/roles/{name}",
-	"/auth/userpass/users/{username}/password",
-	"/auth/userpass/users/{username}/policies",
-	"/transit/export/{type}/{name}/{version}",
-}
-
 func TestCodeFilePath(t *testing.T) {
-	expected := []struct {
-		DataSourceFilePath string
-		ResourceFilePath   string
+	testCases := []struct {
+		input                      string
+		expectedDataSourceFilePath string
+		expectedResourceFilePath   string
 	}{
 		{
-			DataSourceFilePath: "/generated/datasources/database/roles.go",
-			ResourceFilePath:   "/generated/resources/database/roles.go",
+			input:                      "/database/roles",
+			expectedDataSourceFilePath: "/generated/datasources/database/roles.go",
+			expectedResourceFilePath:   "/generated/resources/database/roles.go",
 		},
 		{
-			DataSourceFilePath: "/generated/datasources/database/roles/name.go",
-			ResourceFilePath:   "/generated/resources/database/roles/name.go",
+			input:                      "/database/roles/{name}",
+			expectedDataSourceFilePath: "/generated/datasources/database/roles/name.go",
+			expectedResourceFilePath:   "/generated/resources/database/roles/name.go",
 		},
 		{
-			DataSourceFilePath: "/generated/datasources/auth/userpass/users/username/password.go",
-			ResourceFilePath:   "/generated/resources/auth/userpass/users/username/password.go",
+			input:                      "/auth/userpass/users/{username}/password",
+			expectedDataSourceFilePath: "/generated/datasources/auth/userpass/users/username/password.go",
+			expectedResourceFilePath:   "/generated/resources/auth/userpass/users/username/password.go",
 		},
 		{
-			DataSourceFilePath: "/generated/datasources/auth/userpass/users/username/policies.go",
-			ResourceFilePath:   "/generated/resources/auth/userpass/users/username/policies.go",
+			input:                      "/auth/userpass/users/{username}/policies",
+			expectedDataSourceFilePath: "/generated/datasources/auth/userpass/users/username/policies.go",
+			expectedResourceFilePath:   "/generated/resources/auth/userpass/users/username/policies.go",
 		},
 		{
-			DataSourceFilePath: "/generated/datasources/transit/export/type/name/version.go",
-			ResourceFilePath:   "/generated/resources/transit/export/type/name/version.go",
+			input:                      "/transit/export/{type}/{name}/{version}",
+			expectedDataSourceFilePath: "/generated/datasources/transit/export/type/name/version.go",
+			expectedResourceFilePath:   "/generated/resources/transit/export/type/name/version.go",
 		},
 	}
-	for i, testPath := range testPaths {
-		actualDataSourceFilePath := codeFilePath(templateTypeDataSource, testPath)
-		if actualDataSourceFilePath != pathToHomeDir+expected[i].DataSourceFilePath {
-			t.Fatalf("expected %q but received %q", pathToHomeDir+expected[i].DataSourceFilePath, actualDataSourceFilePath)
+	for _, testCase := range testCases {
+		actualDataSourceFilePath := codeFilePath(templateTypeDataSource, testCase.input)
+		if actualDataSourceFilePath != pathToHomeDir+testCase.expectedDataSourceFilePath {
+			t.Fatalf("testCases %q but received %q", pathToHomeDir+testCase.expectedDataSourceFilePath, actualDataSourceFilePath)
 		}
-		actualResourceFilePath := codeFilePath(templateTypeResource, testPath)
-		if actualResourceFilePath != pathToHomeDir+expected[i].ResourceFilePath {
-			t.Fatalf("expected %q but received %q", pathToHomeDir+expected[i].ResourceFilePath, actualResourceFilePath)
+		actualResourceFilePath := codeFilePath(templateTypeResource, testCase.input)
+		if actualResourceFilePath != pathToHomeDir+testCase.expectedResourceFilePath {
+			t.Fatalf("testCases %q but received %q", pathToHomeDir+testCase.expectedResourceFilePath, actualResourceFilePath)
 		}
 	}
 }
 
 func TestDocFilePath(t *testing.T) {
-	expected := []struct {
-		DataSourceFilePath string
-		ResourceFilePath   string
+	testCases := []struct {
+		input                      string
+		expectedDataSourceFilePath string
+		expectedResourceFilePath   string
 	}{
 		{
-			DataSourceFilePath: "/website/docs/generated/datasources/database/roles.md",
-			ResourceFilePath:   "/website/docs/generated/resources/database/roles.md",
+			input:                      "/database/roles",
+			expectedDataSourceFilePath: "/website/docs/generated/datasources/database/roles.md",
+			expectedResourceFilePath:   "/website/docs/generated/resources/database/roles.md",
 		},
 		{
-			DataSourceFilePath: "/website/docs/generated/datasources/database/roles/name.md",
-			ResourceFilePath:   "/website/docs/generated/resources/database/roles/name.md",
+			input:                      "/database/roles/{name}",
+			expectedDataSourceFilePath: "/website/docs/generated/datasources/database/roles/name.md",
+			expectedResourceFilePath:   "/website/docs/generated/resources/database/roles/name.md",
 		},
 		{
-			DataSourceFilePath: "/website/docs/generated/datasources/auth/userpass/users/username/password.md",
-			ResourceFilePath:   "/website/docs/generated/resources/auth/userpass/users/username/password.md",
+			input:                      "/auth/userpass/users/{username}/password",
+			expectedDataSourceFilePath: "/website/docs/generated/datasources/auth/userpass/users/username/password.md",
+			expectedResourceFilePath:   "/website/docs/generated/resources/auth/userpass/users/username/password.md",
 		},
 		{
-			DataSourceFilePath: "/website/docs/generated/datasources/auth/userpass/users/username/policies.md",
-			ResourceFilePath:   "/website/docs/generated/resources/auth/userpass/users/username/policies.md",
+			input:                      "/auth/userpass/users/{username}/policies",
+			expectedDataSourceFilePath: "/website/docs/generated/datasources/auth/userpass/users/username/policies.md",
+			expectedResourceFilePath:   "/website/docs/generated/resources/auth/userpass/users/username/policies.md",
 		},
 		{
-			DataSourceFilePath: "/website/docs/generated/datasources/transit/export/type/name/version.md",
-			ResourceFilePath:   "/website/docs/generated/resources/transit/export/type/name/version.md",
+			input:                      "/transit/export/{type}/{name}/{version}",
+			expectedDataSourceFilePath: "/website/docs/generated/datasources/transit/export/type/name/version.md",
+			expectedResourceFilePath:   "/website/docs/generated/resources/transit/export/type/name/version.md",
 		},
 	}
-	for i, testPath := range testPaths {
-		actualDataSourceDocPath := docFilePath(templateTypeDataSource, testPath)
-		if actualDataSourceDocPath != pathToHomeDir+expected[i].DataSourceFilePath {
-			t.Fatalf("expected %q but received %q", pathToHomeDir+expected[i].DataSourceFilePath, actualDataSourceDocPath)
+	for _, testCase := range testCases {
+		actualDataSourceDocPath := docFilePath(templateTypeDataSource, testCase.input)
+		if actualDataSourceDocPath != pathToHomeDir+testCase.expectedDataSourceFilePath {
+			t.Fatalf("testCases %q but received %q", pathToHomeDir+testCase.expectedDataSourceFilePath, actualDataSourceDocPath)
 		}
-		actualResourceDocPath := docFilePath(templateTypeResource, testPath)
-		if actualResourceDocPath != pathToHomeDir+expected[i].ResourceFilePath {
-			t.Fatalf("expected %q but received %q", pathToHomeDir+expected[i].ResourceFilePath, actualResourceDocPath)
+		actualResourceDocPath := docFilePath(templateTypeResource, testCase.input)
+		if actualResourceDocPath != pathToHomeDir+testCase.expectedResourceFilePath {
+			t.Fatalf("testCases %q but received %q", pathToHomeDir+testCase.expectedResourceFilePath, actualResourceDocPath)
 		}
 	}
 }
