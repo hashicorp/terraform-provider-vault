38e1c4d429e693f046a0866d5d34e6a09bd92ed6
Spencer Owen <owenspencer@gmail.com>
2020-05-16 09:34:51 -0600

    nomadSecretBackendCreate

--- vault/resource_nomad_secret_backend.go
+++ vault/resource_nomad_secret_backend.go
@@ -69,25 +69,27 @@ func nomadSecretBackendCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*api.Client)
 
 	path := d.Get("path").(string)
-	description := d.Get("description").(string)
-	defaultTTL := d.Get("default_lease_ttl_seconds").(int)
-	maxTTL := d.Get("max_lease_ttl_seconds").(int)
 	address := d.Get("address").(string)
 	token := d.Get("token").(string)
 
-	d.Partial(true)
-	log.Printf("[DEBUG] Mounting Nomad backend at %q", path)
-	err := client.Sys().Mount(path, &api.MountInput{
+	configPath := nomadSecretBackendConfigPath(path)
+
+	info := &api.MountInput{
 		Type:        "nomad",
-		Description: description,
+		Description: d.Get("description").(string),
 		Config: api.MountConfigInput{
-			DefaultLeaseTTL: fmt.Sprintf("%ds", defaultTTL),
-			MaxLeaseTTL:     fmt.Sprintf("%ds", maxTTL),
+			DefaultLeaseTTL: fmt.Sprintf("%ds", d.Get("default_lease_ttl_seconds")),
+			MaxLeaseTTL:     fmt.Sprintf("%ds", d.Get("max_lease_ttl_seconds")),
 		},
-	})
-	if err != nil {
-		return fmt.Errorf("error mounting to %q: %s", path, err)
 	}
+
+	d.Partial(true)
+	log.Printf("[DEBUG] Mounting Consul backend at %q", path)
+
+	if err := client.Sys().Mount(path, info); err != nil {
+		return fmt.Errorf("Error mounting to %q: %s", path, err)
+	}
+
 	log.Printf("[DEBUG] Mounted Nomad backend at %q", path)
 	d.SetId(path)
 
@@ -96,45 +98,62 @@ func nomadSecretBackendCreate(d *schema.ResourceData, meta interface{}) error {
 	d.SetPartial("default_lease_ttl_seconds")
 	d.SetPartial("max_lease_ttl_seconds")
 
-	log.Printf("[DEBUG] Writing connection credentials to %q", path+"/config/access")
+	log.Printf("[DEBUG] Writing connection credentials to %q", configPath)
 	data := map[string]interface{}{
 		"address": address,
 		"token":   token,
 	}
-	_, err = client.Logical().Write(path+"/config/access", data)
-	if err != nil {
-		return fmt.Errorf("error configuring connection credentials for %q: %s", path, err)
+	if _, err := client.Logical().Write(configPath, data); err != nil {
+		return fmt.Errorf("Error writing Nomad configuration for %q: %s", path, err)
 	}
-	log.Printf("[DEBUG] Wrote connection credentials to %q", path+"/config/access")
+	log.Printf("[DEBUG] Wrote Nomad configuration to %q", configPath)
 	d.SetPartial("address")
 	d.SetPartial("token")
 	d.Partial(false)
-	return nomadSecretBackendRead(d, meta)
+	return nil
+	// return nomadSecretBackendRead(d, meta) //TODO: Why is this set in rabbit but not consul?
 }
 
 func nomadSecretBackendRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*api.Client)
 
 	path := d.Id()
+	configPath := nomadSecretBackendConfigPath(path)
 
 	log.Printf("[DEBUG] Reading Nomad secret backend mount %q from Vault", path)
+
 	mounts, err := client.Sys().ListMounts()
 	if err != nil {
 		return fmt.Errorf("error reading mount %q: %s", path, err)
 	}
-	log.Printf("[DEBUG] Read Nomad secret backend mount %q from Vault", path)
+
+	// path can have a trailing slash, but doesn't need to have one
+	// this standardises on having a trailing slash, which is how the
+	// API always responds.
 	mount, ok := mounts[strings.Trim(path, "/")+"/"]
 	if !ok {
 		log.Printf("[WARN] Mount %q not found, removing backend from state.", path)
 		d.SetId("")
 		return nil
 	}
+
 	d.Set("path", path)
 	d.Set("description", mount.Description)
 	d.Set("default_lease_ttl_seconds", mount.Config.DefaultLeaseTTL)
 	d.Set("max_lease_ttl_seconds", mount.Config.MaxLeaseTTL)
 
-	//TODO: What are the outputs? Can nomad return the token?
+	log.Printf("[DEBUG] Reading %s from Vault", configPath)
+	secret, err := client.Logical().Read(configPath)
+	if err != nil {
+		return fmt.Errorf("error reading from Vault: %s", err)
+	}
+
+	log.Printf("[DEBUG] secret: %#v", secret)
+
+	// token, sadly, we can't read out
+	// the API doesn't support it
+	// So... if it drifts, it drift.
+	d.Set("address", secret.Data["address"].(string))
 
 	return nil
 }
@@ -143,38 +162,38 @@ func nomadSecretBackendUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*api.Client)
 
 	path := d.Id()
+	configPath := consulSecretBackendConfigPath(path)
+
 	d.Partial(true)
+
 	if d.HasChange("default_lease_ttl_seconds") || d.HasChange("max_lease_ttl_seconds") {
 		config := api.MountConfigInput{
 			DefaultLeaseTTL: fmt.Sprintf("%ds", d.Get("default_lease_ttl_seconds")),
 			MaxLeaseTTL:     fmt.Sprintf("%ds", d.Get("max_lease_ttl_seconds")),
 		}
+
 		log.Printf("[DEBUG] Updating lease TTLs for %q", path)
 		err := client.Sys().TuneMount(path, config)
 		if err != nil {
 			return fmt.Errorf("error updating mount TTLs for %q: %s", path, err)
 		}
+
 		log.Printf("[DEBUG] Updated lease TTLs for %q", path)
 		d.SetPartial("default_lease_ttl_seconds")
 		d.SetPartial("max_lease_ttl_seconds")
 	}
-	if d.HasChange("connection_uri") || d.HasChange("username") || d.HasChange("password") || d.HasChange("verify_connection") {
-		log.Printf("[DEBUG] Updating connecion credentials at %q", path+"/config/access")
+	if d.HasChange("address") || d.HasChange("token") {
+		log.Printf("[DEBUG] Updating Nomad configuration at %q", configPath)
 		data := map[string]interface{}{
-			"connection_uri":    d.Get("connection_uri").(string),
-			"username":          d.Get("username").(string),
-			"password":          d.Get("password").(string),
-			"verify_connection": d.Get("verify_connection").(bool),
+			"address": d.Get("address").(string),
+			"token":   d.Get("token").(string),
 		}
-		_, err := client.Logical().Write(path+"/config/access", data)
-		if err != nil {
-			return fmt.Errorf("error configuring connection credentials for %q: %s", path, err)
+		if _, err := client.Logical().Write(configPath, data); err != nil {
+			return fmt.Errorf("Error configuring Nomad configuration for %q: %s", path, err)
 		}
-		log.Printf("[DEBUG] Updated root credentials at %q", path+"/config/access")
-		d.SetPartial("connection_url")
-		d.SetPartial("username")
-		d.SetPartial("password")
-		d.SetPartial("verify_connection")
+		log.Printf("[DEBUG] Updated Nomad configuration at %q", configPath)
+		d.SetPartial("address")
+		d.SetPartial("token")
 	}
 	d.Partial(false)
 	return nomadSecretBackendRead(d, meta)
@@ -184,12 +203,12 @@ func nomadSecretBackendDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*api.Client)
 
 	path := d.Id()
-	log.Printf("[DEBUG] Unmounting RabbitMQ backend %q", path)
+	log.Printf("[DEBUG] Unmounting Nomad backend %q", path)
 	err := client.Sys().Unmount(path)
 	if err != nil {
-		return fmt.Errorf("error unmounting RabbitMQ backend from %q: %s", path, err)
+		return fmt.Errorf("error unmounting Nomad backend from %q: %s", path, err)
 	}
-	log.Printf("[DEBUG] Unmounted RabbitMQ backend %q", path)
+	log.Printf("[DEBUG] Unmounted Nomad backend %q", path)
 	return nil
 }
 
@@ -197,12 +216,16 @@ func nomadSecretBackendExists(d *schema.ResourceData, meta interface{}) (bool, e
 	client := meta.(*api.Client)
 
 	path := d.Id()
-	log.Printf("[DEBUG] Checking if RabbitMQ backend exists at %q", path)
+	log.Printf("[DEBUG] Checking if Nomad backend exists at %q", path)
 	mounts, err := client.Sys().ListMounts()
 	if err != nil {
 		return true, fmt.Errorf("error retrieving list of mounts: %s", err)
 	}
-	log.Printf("[DEBUG] Checked if RabbitMQ backend exists at %q", path)
+	log.Printf("[DEBUG] Checked if Nomad backend exists at %q", path)
 	_, ok := mounts[strings.Trim(path, "/")+"/"]
 	return ok, nil
+}
+
+func nomadSecretBackendConfigPath(backend string) string {
+	return strings.Trim(backend, "/") + "/config/access"
 }
