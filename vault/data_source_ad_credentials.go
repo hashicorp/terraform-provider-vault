97c603b536af6f30204c95e3715ffbd52970beb2
Robin Breathe <robin@isometry.net>
2021-07-09 22:32:58 +0200

    Secrets from the AD, AWS, Azure & Nomad Secrets Engines are sensitive

--- vault/data_source_ad_credentials.go
+++ vault/data_source_ad_credentials.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
 	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 )
 
@@ -28,11 +27,13 @@ func adAccessCredentialsDataSource() *schema.Resource {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "Password for the service account.",
+				Sensitive:   true,
 			},
 			"last_password": {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "Last known password for the service account.",
+				Sensitive:   true,
 			},
 			"username": {
 				Type:        schema.TypeString,
