b791207c971bbbb9c97d48edfb523e9f277a8681
Ben Ash <32777270+benashz@users.noreply.github.com>
2021-07-22 10:46:19 -0400

    fix: drop vendor (#1110)

--- scripts/errcheck.sh
+++ scripts/errcheck.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-# Check gofmt
+set -euo pipefail
+
 echo "==> Checking for unchecked errors..."
 
 if ! which errcheck > /dev/null; then
     echo "==> Installing errcheck..."
+    # avoid accidentally updating the project's go.mod file
+    pushd /tmp > /dev/null
     go get -u github.com/kisielk/errcheck
+    popd
 fi
 
 err_files=$(errcheck -ignoretests \
                      -ignore 'github.com/hashicorp/terraform/helper/schema:Set' \
                      -ignore 'bytes:.*' \
                      -ignore 'io:Close|Write' \
-                     $(go list ./...| grep -v /vendor/))
+                     $(go list ./...))
 
 if [[ -n ${err_files} ]]; then
     echo 'Unchecked errors found in the following places:'
