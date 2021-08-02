8ffeeb0223a6d0bcd034db7c43e0e84e9900a11a
Yoan Blanc <yblanc@edgelab.ch>
2021-08-02 13:57:15 +0200

    feat: provider to init itself as late as required
    Signed-off-by: Yoan Blanc <yblanc@edgelab.ch>

--- vault/provider.go
+++ vault/provider.go
@@ -669,6 +669,212 @@ var (
 	}
 )
 
+type Client struct {
+	*api.Client
+	data *schema.ResourceData
+}
+
+func (c *Client) lazyInit() error {
+	// client has already be initialized.
+	if c.Client != nil {
+		return nil
+	}
+
+	d := c.data
+
+	clientConfig := api.DefaultConfig()
+	addr := d.Get("address").(string)
+	if addr != "" {
+		clientConfig.Address = addr
+	}
+
+	clientAuthI := d.Get("client_auth").([]interface{})
+	if len(clientAuthI) > 1 {
+		return fmt.Errorf("client_auth block may appear only once")
+	}
+
+	clientAuthCert := ""
+	clientAuthKey := ""
+	if len(clientAuthI) == 1 {
+		clientAuth := clientAuthI[0].(map[string]interface{})
+		clientAuthCert = clientAuth["cert_file"].(string)
+		clientAuthKey = clientAuth["key_file"].(string)
+	}
+
+	err := clientConfig.ConfigureTLS(&api.TLSConfig{
+		CACert:   d.Get("ca_cert_file").(string),
+		CAPath:   d.Get("ca_cert_dir").(string),
+		Insecure: d.Get("skip_tls_verify").(bool),
+
+		ClientCert: clientAuthCert,
+		ClientKey:  clientAuthKey,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to configure TLS for Vault API: %s", err)
+	}
+
+	clientConfig.HttpClient.Transport = logging.NewTransport("Vault", clientConfig.HttpClient.Transport)
+
+	client, err := api.NewClient(clientConfig)
+	if err != nil {
+		return fmt.Errorf("failed to configure Vault API: %s", err)
+	}
+
+	client.SetCloneHeaders(true)
+
+	// Set headers if provided
+	headers := d.Get("headers").([]interface{})
+	parsedHeaders := client.Headers().Clone()
+
+	if parsedHeaders == nil {
+		parsedHeaders = make(http.Header)
+	}
+
+	for _, h := range headers {
+		header := h.(map[string]interface{})
+		if name, ok := header["name"]; ok {
+			parsedHeaders.Add(name.(string), header["value"].(string))
+		}
+	}
+	client.SetHeaders(parsedHeaders)
+
+	client.SetMaxRetries(d.Get("max_retries").(int))
+
+	// Try an get the token from the config or token helper
+	token, err := providerToken(d)
+	if err != nil {
+		return err
+	}
+
+	// Attempt to use auth/<mount>login if 'auth_login' is provided in provider config
+	authLoginI := d.Get("auth_login").([]interface{})
+	if len(authLoginI) > 1 {
+		return fmt.Errorf("auth_login block may appear only once")
+	}
+
+	if len(authLoginI) == 1 {
+		authLogin := authLoginI[0].(map[string]interface{})
+		authLoginPath := authLogin["path"].(string)
+		authLoginNamespace := ""
+		if authLoginNamespaceI, ok := authLogin["namespace"]; ok {
+			authLoginNamespace = authLoginNamespaceI.(string)
+			client.SetNamespace(authLoginNamespace)
+		}
+		authLoginParameters := authLogin["parameters"].(map[string]interface{})
+
+		method := authLogin["method"].(string)
+		if method == "aws" {
+			if err := signAWSLogin(authLoginParameters); err != nil {
+				return fmt.Errorf("error signing AWS login request: %s", err)
+			}
+		}
+
+		secret, err := client.Logical().Write(authLoginPath, authLoginParameters)
+		if err != nil {
+			return err
+		}
+		token = secret.Auth.ClientToken
+	}
+	if token != "" {
+		client.SetToken(token)
+	}
+	if client.Token() == "" {
+		return errors.New("no vault token found")
+	}
+
+	tokenName := d.Get("token_name").(string)
+	if tokenName == "" {
+		tokenName = "terraform"
+	}
+
+	// In order to enforce our relatively-short lease TTL, we derive a
+	// temporary child token that inherits all of the policies of the
+	// token we were given but expires after max_lease_ttl_seconds.
+	//
+	// The intent here is that Terraform will need to re-fetch any
+	// secrets on each run and so we limit the exposure risk of secrets
+	// that end up stored in the Terraform state, assuming that they are
+	// credentials that Vault is able to revoke.
+	//
+	// Caution is still required with state files since not all secrets
+	// can explicitly be revoked, and this limited scope won't apply to
+	// any secrets that are *written* by Terraform to Vault.
+
+	// Set the namespace to the token's namespace only for the
+	// child token creation
+	tokenInfo, err := client.Auth().Token().LookupSelf()
+	if err != nil {
+		return err
+	}
+	if tokenNamespaceRaw, ok := tokenInfo.Data["namespace_path"]; ok {
+		tokenNamespace := tokenNamespaceRaw.(string)
+		if tokenNamespace != "" {
+			client.SetNamespace(tokenNamespace)
+		}
+	}
+
+	renewable := false
+	childTokenLease, err := client.Auth().Token().Create(&api.TokenCreateRequest{
+		DisplayName:    tokenName,
+		TTL:            fmt.Sprintf("%ds", d.Get("max_lease_ttl_seconds").(int)),
+		ExplicitMaxTTL: fmt.Sprintf("%ds", d.Get("max_lease_ttl_seconds").(int)),
+		Renewable:      &renewable,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create limited child token: %s", err)
+	}
+
+	childToken := childTokenLease.Auth.ClientToken
+	policies := childTokenLease.Auth.Policies
+
+	log.Printf("[INFO] Using Vault token with the following policies: %s", strings.Join(policies, ", "))
+
+	// Set the token to the generated child token
+	client.SetToken(childToken)
+
+	// Set the namespace to the requested namespace, if provided
+	namespace := d.Get("namespace").(string)
+	if namespace != "" {
+		client.SetNamespace(namespace)
+	}
+
+	c.Client = client
+
+	return nil
+}
+
+// Clone creates a same object, but a copy.
+func (c *Client) Clone() (*Client, error) {
+	clone := &Client{
+		data: c.data,
+	}
+
+	var err error
+	if c.Client != nil {
+		clone.Client, err = c.Client.Clone()
+	}
+
+	return clone, err
+}
+
+// Logical performs the lazy initialization, then delegates.
+func (c *Client) Logical() *api.Logical {
+	if err := c.lazyInit(); err != nil {
+		panic(err)
+	}
+
+	return c.Client.Logical()
+}
+
+// Sys performs the lazy initialization, then delegates.
+func (c *Client) Sys() *api.Sys {
+	if err := c.lazyInit(); err != nil {
+		panic(err)
+	}
+
+	return c.Client.Sys()
+}
+
 func providerToken(d *schema.ResourceData) (string, error) {
 	if token := d.Get("token").(string); token != "" {
 		return token, nil
@@ -702,162 +908,7 @@ func providerToken(d *schema.ResourceData) (string, error) {
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	clientConfig := api.DefaultConfig()
-	addr := d.Get("address").(string)
-	if addr != "" {
-		clientConfig.Address = addr
-	}
-
-	clientAuthI := d.Get("client_auth").([]interface{})
-	if len(clientAuthI) > 1 {
-		return nil, fmt.Errorf("client_auth block may appear only once")
-	}
-
-	clientAuthCert := ""
-	clientAuthKey := ""
-	if len(clientAuthI) == 1 {
-		clientAuth := clientAuthI[0].(map[string]interface{})
-		clientAuthCert = clientAuth["cert_file"].(string)
-		clientAuthKey = clientAuth["key_file"].(string)
-	}
-
-	err := clientConfig.ConfigureTLS(&api.TLSConfig{
-		CACert:   d.Get("ca_cert_file").(string),
-		CAPath:   d.Get("ca_cert_dir").(string),
-		Insecure: d.Get("skip_tls_verify").(bool),
-
-		ClientCert: clientAuthCert,
-		ClientKey:  clientAuthKey,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to configure TLS for Vault API: %s", err)
-	}
-
-	clientConfig.HttpClient.Transport = logging.NewTransport("Vault", clientConfig.HttpClient.Transport)
-
-	client, err := api.NewClient(clientConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to configure Vault API: %s", err)
-	}
-
-	client.SetCloneHeaders(true)
-
-	// Set headers if provided
-	headers := d.Get("headers").([]interface{})
-	parsedHeaders := client.Headers().Clone()
-
-	if parsedHeaders == nil {
-		parsedHeaders = make(http.Header)
-	}
-
-	for _, h := range headers {
-		header := h.(map[string]interface{})
-		if name, ok := header["name"]; ok {
-			parsedHeaders.Add(name.(string), header["value"].(string))
-		}
-	}
-	client.SetHeaders(parsedHeaders)
-
-	client.SetMaxRetries(d.Get("max_retries").(int))
-
-	// Try an get the token from the config or token helper
-	token, err := providerToken(d)
-	if err != nil {
-		return nil, err
-	}
-
-	// Attempt to use auth/<mount>login if 'auth_login' is provided in provider config
-	authLoginI := d.Get("auth_login").([]interface{})
-	if len(authLoginI) > 1 {
-		return "", fmt.Errorf("auth_login block may appear only once")
-	}
-
-	if len(authLoginI) == 1 {
-		authLogin := authLoginI[0].(map[string]interface{})
-		authLoginPath := authLogin["path"].(string)
-		authLoginNamespace := ""
-		if authLoginNamespaceI, ok := authLogin["namespace"]; ok {
-			authLoginNamespace = authLoginNamespaceI.(string)
-			client.SetNamespace(authLoginNamespace)
-		}
-		authLoginParameters := authLogin["parameters"].(map[string]interface{})
-
-		method := authLogin["method"].(string)
-		if method == "aws" {
-			if err := signAWSLogin(authLoginParameters); err != nil {
-				return nil, fmt.Errorf("error signing AWS login request: %s", err)
-			}
-		}
-
-		secret, err := client.Logical().Write(authLoginPath, authLoginParameters)
-		if err != nil {
-			return nil, err
-		}
-		token = secret.Auth.ClientToken
-	}
-	if token != "" {
-		client.SetToken(token)
-	}
-	if client.Token() == "" {
-		return nil, errors.New("no vault token found")
-	}
-
-	tokenName := d.Get("token_name").(string)
-	if tokenName == "" {
-		tokenName = "terraform"
-	}
-
-	// In order to enforce our relatively-short lease TTL, we derive a
-	// temporary child token that inherits all of the policies of the
-	// token we were given but expires after max_lease_ttl_seconds.
-	//
-	// The intent here is that Terraform will need to re-fetch any
-	// secrets on each run and so we limit the exposure risk of secrets
-	// that end up stored in the Terraform state, assuming that they are
-	// credentials that Vault is able to revoke.
-	//
-	// Caution is still required with state files since not all secrets
-	// can explicitly be revoked, and this limited scope won't apply to
-	// any secrets that are *written* by Terraform to Vault.
-
-	// Set the namespace to the token's namespace only for the
-	// child token creation
-	tokenInfo, err := client.Auth().Token().LookupSelf()
-	if err != nil {
-		return nil, err
-	}
-	if tokenNamespaceRaw, ok := tokenInfo.Data["namespace_path"]; ok {
-		tokenNamespace := tokenNamespaceRaw.(string)
-		if tokenNamespace != "" {
-			client.SetNamespace(tokenNamespace)
-		}
-	}
-
-	renewable := false
-	childTokenLease, err := client.Auth().Token().Create(&api.TokenCreateRequest{
-		DisplayName:    tokenName,
-		TTL:            fmt.Sprintf("%ds", d.Get("max_lease_ttl_seconds").(int)),
-		ExplicitMaxTTL: fmt.Sprintf("%ds", d.Get("max_lease_ttl_seconds").(int)),
-		Renewable:      &renewable,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create limited child token: %s", err)
-	}
-
-	childToken := childTokenLease.Auth.ClientToken
-	policies := childTokenLease.Auth.Policies
-
-	log.Printf("[INFO] Using Vault token with the following policies: %s", strings.Join(policies, ", "))
-
-	// Set tht token to the generated child token
-	client.SetToken(childToken)
-
-	// Set the namespace to the requested namespace, if provided
-	namespace := d.Get("namespace").(string)
-	if namespace != "" {
-		client.SetNamespace(namespace)
-	}
-	return client, nil
+	return &Client{nil, d}, nil
 }
 
 func parse(descs map[string]*Description) (map[string]*schema.Resource, error) {
