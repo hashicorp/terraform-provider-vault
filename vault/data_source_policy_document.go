b5295c98e776cec559cf5363d124beb9a6084983
Ben Ash <bash@hashicorp.com>
2021-11-26 15:42:11 -0500

    Add support for patch to vault_policy_document

--- vault/data_source_policy_document.go
+++ vault/data_source_policy_document.go
@@ -27,7 +27,16 @@ type PolicyRule struct {
 	DeniedParameters   map[string][]string
 }
 
-var allowedCapabilities = []string{"create", "read", "update", "delete", "list", "sudo", "deny"}
+var allowedCapabilities = []string{
+	"create",
+	"read",
+	"update",
+	"delete",
+	"list",
+	"sudo",
+	"deny",
+	"patch",
+}
 
 func policyDocumentDataSource() *schema.Resource {
 	return &schema.Resource{
@@ -136,7 +145,7 @@ func policyDocumentDataSourceRead(d *schema.ResourceData, meta interface{}) erro
 	policy := &Policy{}
 
 	if rawRules, hasRawRules := d.GetOk("rule"); hasRawRules {
-		var rawRuleIntfs = rawRules.([]interface{})
+		rawRuleIntfs := rawRules.([]interface{})
 		rules := make([]*PolicyRule, len(rawRuleIntfs))
 
 		for i, ruleI := range rawRuleIntfs {
