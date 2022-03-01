58672f337ca46f3fe8dbf2b0c0175ccecdf5a681
Daniel Kimsey <dekimsey@protonmail.com>
2022-02-28 22:06:57 -0600

    Quick write-up of consul test, but its failing

--- vault/data_source_consul_access_credentials_test.go
+++ vault/data_source_consul_access_credentials_test.go
@@ -12,7 +12,6 @@ import (
 
 func TestAccDataSourceConsul_basic(t *testing.T) {
 	path := acctest.RandomWithPrefix("tf-test-consul")
-	accessor := "592fca4f-3ce9-4a00-8730-9a2fac6afabd"
 	token := "026a0c16-87cd-4c2d-b3f3-fb539f592b7e"
 	resource.Test(t, resource.TestCase{
 		Providers:    testProviders,
@@ -20,22 +19,37 @@ func TestAccDataSourceConsul_basic(t *testing.T) {
 		CheckDestroy: testAccConsulSecretBackendCheckDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceConsul_initialConfig(path, token, accessor),
+				Config: testAccDataSourceConsul_initialConfig(path, token),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.vault_consul_access_credential.test", "path", path),
-					resource.TestCheckResourceAttr("data.vault_consul_access_credential.test", "token", token),
-					resource.TestCheckResourceAttr("data.vault_consul_access_credential.test", "accessor", accessor),
+					resource.TestCheckResourceAttr("data.vault_consul_access_credentials.test", "backend", path),
+					resource.TestCheckResourceAttrSet("data.vault_consul_access_credentials.test", "token"),
+					resource.TestCheckResourceAttrSet("data.vault_consul_access_credentials.test", "accessor"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceConsul_initialConfig(path, token, accessor string) string {
+func testAccDataSourceConsul_initialConfig(path string, token string) string {
 	return fmt.Sprintf(`
-data "vault_consul_access_credential" "test" {
-  path = "%s"
-  token = "%s"
-  accessor = "%s"
-}`, path, token, accessor)
+resource "vault_consul_secret_backend" "test" {
+	path = "%s"
+	address = "127.0.0.1:8500"
+	token = "%s"
+}
+resource "vault_consul_secret_backend_role" "this" {
+	backend = vault_consul_secret_backend.test.path
+	name = "test"
+	# FIXME: Docs say policies is optional when token_type == management
+	# https://www.vaultproject.io/api/secret/consul#parameters-for-consul-versions-1-4-and-above
+	policies = ["foo", "bar"]
+	token_type = "management"
+	ttl = 120
+	max_ttl = 240
+	local = true
+}
+data "vault_consul_access_credentials" "test" {
+  backend = vault_consul_secret_backend.test.path
+  role = "test"
+}`, path, token)
 }
