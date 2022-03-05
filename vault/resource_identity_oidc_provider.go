f83ff020c27f88b2f55e29ee180bea3e09378880
vinay-gopalan <86625824+vinay-gopalan@users.noreply.github.com>
2022-03-04 16:04:30 -0800

    Merge Client and Provider resources to OIDC feature branch (#1368)

---
+++ vault/resource_identity_oidc_provider.go
@@ -0,0 +1,129 @@
+package vault
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/vault/api"
+)
+
+const identityOIDCProviderPathTemplate = "identity/oidc/provider"
+
+func identityOIDCProviderResource() *schema.Resource {
+	return &schema.Resource{
+		Create: identityOIDCProviderCreateUpdate,
+		Update: identityOIDCProviderCreateUpdate,
+		Read:   identityOIDCProviderRead,
+		Delete: identityOIDCProviderDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Description: "The name of the client.",
+				Required:    true,
+			},
+			"issuer": {
+				Type: schema.TypeString,
+				Description: "Specifies what will be used as the `scheme://host:port` component for the `iss` claim of ID tokens." +
+					"If provided explicitly, it must point to a Vault instance that is network reachable by clients for ID token validation.",
+				Computed: true,
+			},
+			"allowed_client_ids": {
+				Type: schema.TypeSet,
+				Description: "The client IDs that are permitted to use the provider. If empty, no clients are allowed. " +
+					"If \"*\", all clients are allowed.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+			},
+			"scopes_supported": {
+				Type:        schema.TypeSet,
+				Description: "The scopes available for requesting on the provider.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+			},
+		},
+	}
+}
+
+func identityOIDCProviderRequestData(d *schema.ResourceData) map[string]interface{} {
+	fields := []string{"issuer", "allowed_client_ids", "scopes_supported"}
+	data := map[string]interface{}{}
+
+	for _, k := range fields {
+		if v, ok := d.GetOk(k); ok {
+			if k == "allowed_client_ids" || k == "scopes_supported" {
+				data[k] = v.(*schema.Set).List()
+				continue
+			}
+			data[k] = v
+		}
+	}
+
+	return data
+}
+
+func getOIDCProviderPath(name string) string {
+	return fmt.Sprintf("%s/%s", identityOIDCProviderPathTemplate, name)
+}
+
+func identityOIDCProviderCreateUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+	name := d.Get("name").(string)
+	path := getOIDCProviderPath(name)
+
+	data := identityOIDCProviderRequestData(d)
+	_, err := client.Logical().Write(path, data)
+	if err != nil {
+		return fmt.Errorf("error writing OIDC Provider %s, err=%w", path, err)
+	}
+	log.Printf("[DEBUG] Wrote OIDC Provider to %s", path)
+
+	d.SetId(path)
+
+	return identityOIDCProviderRead(d, meta)
+}
+
+func identityOIDCProviderRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+	path := d.Id()
+
+	log.Printf("[DEBUG] Reading OIDC Provider for %s", path)
+	resp, err := client.Logical().Read(path)
+	if err != nil {
+		return fmt.Errorf("error reading OIDC Provider for %s: %s", path, err)
+	}
+	log.Printf("[DEBUG] Read OIDC Provider for %s", path)
+	if resp == nil {
+		log.Printf("[WARN] OIDC Provider %s not found, removing from state", path)
+		d.SetId("")
+		return nil
+	}
+
+	for _, k := range []string{"issuer", "allowed_client_ids", "scopes_supported"} {
+		if err := d.Set(k, resp.Data[k]); err != nil {
+			return fmt.Errorf("error setting state key \"%s\" on OIDC Provider %s, err=%w", k, path, err)
+		}
+	}
+	return nil
+}
+
+func identityOIDCProviderDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+	path := d.Id()
+
+	log.Printf("[DEBUG] Deleting OIDC Provider %s", path)
+
+	_, err := client.Logical().Delete(path)
+	if err != nil {
+		return fmt.Errorf("error deleting OIDC Provider %q", path)
+	}
+	log.Printf("[DEBUG] Deleted OIDC Provider %q", path)
+
+	return nil
+}
---
+++ vault/resource_identity_oidc_provider_test.go
@@ -0,0 +1,103 @@
+package vault
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/vault/api"
+
+	"github.com/hashicorp/terraform-provider-vault/testutil"
+)
+
+func TestAccIdentityOIDCProvider(t *testing.T) {
+	name := acctest.RandomWithPrefix("test-scope")
+	resourceName := "vault_identity_oidc_provider.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testutil.TestAccPreCheck(t) },
+		Providers:    testProviders,
+		CheckDestroy: testAccCheckOIDCProviderDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityOIDCProviderConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "allowed_client_ids.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "scopes_supported.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "scopes_supported.0", "groups"),
+				),
+			},
+		},
+	})
+}
+
+func testAccIdentityOIDCProviderConfig(name string) string {
+	return fmt.Sprintf(`
+resource "vault_identity_oidc_key" "test" {
+  name               = "default"
+  allowed_client_ids = ["*"]
+  rotation_period    = 3600
+  verification_ttl   = 3600
+}
+
+resource "vault_identity_oidc_assignment" "test" {
+  name       = "my-assignment"
+  entity_ids = ["fake-ascbascas-2231a-sdfaa"]
+  group_ids  = ["fake-sajkdsad-32414-sfsada"]
+}
+
+resource "vault_identity_oidc_client" "test" {
+  name          = "application"
+  key           = vault_identity_oidc_key.test.name
+  redirect_uris = [
+	"http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback", 
+	"http://127.0.0.1:8251/callback",
+	"http://127.0.0.1:8080/callback"
+  ]
+  assignments = [
+    vault_identity_oidc_assignment.test.name
+  ]
+  id_token_ttl     = 2400
+  access_token_ttl = 7200
+}
+
+resource "vault_identity_oidc_scope" "test" {
+  name        = "groups"
+  template    = "{\"groups\": {{identity.entity.groups.names}} }"
+  description = "Groups scope."
+}
+
+data "vault_identity_oidc_client_creds" "test" {
+  name = vault_identity_oidc_client.test.name
+}
+
+resource "vault_identity_oidc_provider" "test" {
+  name = "%s"
+  allowed_client_ids = [
+     data.vault_identity_oidc_client_creds.test.client_id
+  ]
+  scopes_supported = [
+    vault_identity_oidc_scope.test.name
+  ]
+}`, name)
+}
+
+func testAccCheckOIDCProviderDestroy(s *terraform.State) error {
+	client := testProvider.Meta().(*api.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "vault_identity_oidc_provider" {
+			continue
+		}
+		resp, err := client.Logical().Read(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("error checking for OIDC provider at %s, err=%w", rs.Primary.ID, err)
+		}
+		if resp != nil {
+			return fmt.Errorf("OIDC provider still exists at %s", rs.Primary.ID)
+		}
+	}
+	return nil
+}
