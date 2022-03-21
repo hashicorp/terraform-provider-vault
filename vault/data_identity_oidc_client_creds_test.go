eeb66cf4cd5947a22fde7fca524a6869b3fc1b14
Vinay Gopalan <vinay@hashicorp.com>
2022-03-21 16:09:30 -0700

    add test for client creds data source

---
+++ vault/data_identity_oidc_client_creds_test.go
@@ -0,0 +1,54 @@
+package vault
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-vault/testutil"
+)
+
+func TestDataSourceIdentityOIDCClientCreds(t *testing.T) {
+	name := acctest.RandomWithPrefix("test-client")
+
+	resource.Test(t, resource.TestCase{
+		Providers: testProviders,
+		PreCheck:  func() { testutil.TestAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceIdentityOIDCClientCreds_config(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.vault_identity_oidc_client_creds.creds", "name", name),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceIdentityOIDCClientCreds_config(name string) string {
+	return fmt.Sprintf(`
+resource "vault_identity_oidc_key" "key" {
+  name               = "key"
+  allowed_client_ids = ["*"]
+  rotation_period    = 3600
+  verification_ttl   = 3600
+}
+
+resource "vault_identity_oidc_client" "test" {
+  name          = "%s"
+  key           = vault_identity_oidc_key.key.name
+  redirect_uris = [
+	"http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback", 
+	"http://127.0.0.1:8251/callback",
+	"http://127.0.0.1:8080/callback"
+  ]
+  id_token_ttl     = 2400
+  access_token_ttl = 7200
+}
+
+data "vault_identity_oidc_client_creds" "creds" {
+  name = vault_identity_oidc_client.test.name
+}`, name)
+}
