30da4ded7f8368ababe831bf4285a4a42e806eec
Vinay Gopalan <vinay@hashicorp.com>
2022-03-21 14:42:49 -0700

    add openID schema to config data source

--- vault/data_identity_oidc_openid_config.go
+++ vault/data_identity_oidc_openid_config.go
@@ -1,7 +1,9 @@
 package vault
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -19,11 +21,83 @@ func identityOIDCOpenIDConfigDataSource() *schema.Resource {
 				Required:    true,
 				Description: "The name of the provider.",
 			},
-			"openid_config": {
-				Type: schema.TypeMap,
-				Description: "OpenID Connect Metadata for a named OIDC provider." +
-					"The response is a compliant OpenID Provider Configuration Response.",
-				Computed: true,
+			"issuer": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The URL of the issuer for the provider.",
+			},
+			"jwks_uri": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The well known keys URI for the provider.",
+			},
+			"authorization_endpoint": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The Authorization Endpoint for the provider.",
+			},
+			"token_endpoint": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The Token Endpoint for the provider.",
+			},
+			"userinfo_endpoint": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The User Info Endpoint for the provider.",
+			},
+			"request_uri_parameter_supported": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Specifies whether Request URI Parameter is supported by the provider.",
+			},
+			"id_token_signing_alg_values_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The signing algorithms supported by the provider.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"response_types_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The response types supported by the provider.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"scopes_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The scopes supported by the provider.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"subject_types_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The subject types supported by the provider.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"grant_types_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The grant types supported by the provider.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"token_endpoint_auth_methods_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The token endpoint auth methods supported by the provider.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
 			},
 		},
 	}
@@ -39,21 +113,41 @@ func readOIDCOpenIDConfigResource(d *schema.ResourceData, meta interface{}) erro
 	if err != nil {
 		return fmt.Errorf("error performing GET at %s, err=%w", path, err)
 	}
+
+	if resp == nil {
+		return fmt.Errorf("expected a response body, got nil response")
+	}
+
 	if resp != nil {
 		defer resp.Body.Close()
 	}
 
-	secret, err := api.ParseSecret(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("error parsing JSON data from response body, err=%w", err)
+		return err
+	}
+
+	var data map[string]interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		return err
 	}
 
 	log.Printf("[DEBUG] Read %q from Vault", path)
 
 	d.SetId(path)
 
-	if err := d.Set("openid_config", secret.Data); err != nil {
-		return err
+	openIDConfigFields := []string{
+		"issuer", "jwks_uri", "authorization_endpoint", "token_endpoint",
+		"userinfo_endpoint", "request_uri_parameter_supported",
+		"id_token_signing_alg_values_supported", "response_types_supported",
+		"scopes_supported", "subject_types_supported", "grant_types_supported",
+		"token_endpoint_auth_methods_supported",
+	}
+
+	for _, k := range openIDConfigFields {
+		if err := d.Set(k, data[k]); err != nil {
+			return fmt.Errorf("error setting state key %q on OpenID Config %q, err=%w", k, path, err)
+		}
 	}
 
 	return nil
