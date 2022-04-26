808e488f99a60e640b2ee90894a12179782e35c1
Ben Ash <bash@hashicorp.com>
2022-04-26 15:03:54 -0400

    Fix race condition on duplicate entity alias creation
    The creation of entity aliases should be serialized, since we want to
    handle the case when the user is attempting to create duplicate entity
    aliases during the same Teraform execution. A duplicate entity alias
    shares the same mount accessor and name with another entity alias.
    
    The fix is to lock the /identity/entity-alias/id path on all create,
    update, and delete operations. This allows the provider to reliably
    detect the duplicate entity alias condition, and prevent duplicate
    alias creation.
    
    Other fixes:
    - all CRUD functions are of the *Context type, and can return meaning
      diag.Diagnostics
    - created a new entity package under internal/identity/entity with some
      helper functions

--- vault/resource_identity_entity_alias.go
+++ vault/resource_identity_entity_alias.go
@@ -1,9 +1,12 @@
 package vault
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/vault/api"
 )
@@ -12,11 +15,10 @@ const identityEntityAliasPath = "/identity/entity-alias"
 
 func identityEntityAliasResource() *schema.Resource {
 	return &schema.Resource{
-		Create: identityEntityAliasCreate,
-		Update: identityEntityAliasUpdate,
-		Read:   identityEntityAliasRead,
-		Delete: identityEntityAliasDelete,
-		Exists: identityEntityAliasExists,
+		CreateContext: identityEntityAliasCreate,
+		UpdateContext: identityEntityAliasUpdate,
+		ReadContext:   identityEntityAliasRead,
+		DeleteContext: identityEntityAliasDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -51,15 +53,16 @@ func identityEntityAliasResource() *schema.Resource {
 	}
 }
 
-func identityEntityAliasCreate(d *schema.ResourceData, meta interface{}) error {
+func identityEntityAliasCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	path := identityEntityAliasPath
+	vaultMutexKV.Lock(path)
+	defer vaultMutexKV.Unlock(path)
 	client := meta.(*api.Client)
 
 	name := d.Get("name").(string)
 	mountAccessor := d.Get("mount_accessor").(string)
 	canonicalID := d.Get("canonical_id").(string)
 	customMetadata := d.Get("custom_metadata").(map[string]interface{})
-
-	path := identityEntityAliasPath
 
 	data := map[string]interface{}{
 		"name":            name,
@@ -68,39 +71,106 @@ func identityEntityAliasCreate(d *schema.ResourceData, meta interface{}) error {
 		"custom_metadata": customMetadata,
 	}
 
-	resp, err := client.Logical().Write(path, data)
+	diags := diag.Diagnostics{}
 
+	var duplicates []string
+	/*
+		aliases, err := getEntityAliasesByName(client, name)
+		if err != nil {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("Failed to get entity aliases, err=%s", err),
+			})
+
+			return diags
+		}
+
+		for _, config := range aliases {
+			if config.Data["mount_accessor"].(string) == mountAccessor {
+				duplicates = append(duplicates, config.Data["id"].(string))
+			}
+		}
+	*/
+	a, err := getEntityAliasesByMountAccessor(client, mountAccessor)
 	if err != nil {
-		return fmt.Errorf("error writing IdentityEntityAlias to %q: %s", name, err)
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("Failed to get entity aliases by mount accessor, err=%s", err),
+		})
+
+		return diags
+	}
+
+	for _, alias := range a {
+		if v, ok := alias["name"]; ok && v.(string) == name {
+			duplicates = append(duplicates, alias["id"].(string))
+		}
+	}
+
+	if len(duplicates) > 0 {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary: fmt.Sprintf(
+				"entity alias %q already exists for mount accessor %q, "+
+					"ids=%q", name, mountAccessor, strings.Join(duplicates, ",")),
+			Detail: "In the case where this error occurred during the creation of more than one alias, " +
+				"it may be necessary to assign a unique alias name to each of affected resources and " +
+				"then rerun the apply. After a successful apply the desired original alias names can then be " +
+				"reassigned",
+		})
+
+		return diags
+	}
+
+	resp, err := client.Logical().Write(path, data)
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary: fmt.Sprintf(
+				"error writing IdentityEntityAlias to %q: %s", name, err),
+		})
+
+		return diags
 	}
 
 	if resp == nil {
-		aliasIDMsg := "Unable to determine alias id."
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary: fmt.Sprintf(
+				"unexpected empty response during entity alias creation name=%q", name),
+		})
 
-		if aliasID, err := findAliasID(client, canonicalID, name, mountAccessor); err == nil {
-			aliasIDMsg = fmt.Sprintf("Alias resource ID %q may be imported.", aliasID)
-		}
+		return diags
 
-		return fmt.Errorf("IdentityEntityAlias %q already exists. %s", name, aliasIDMsg)
 	}
 
 	log.Printf("[DEBUG] Wrote IdentityEntityAlias %q", name)
 
 	d.SetId(resp.Data["id"].(string))
 
-	return identityEntityAliasRead(d, meta)
+	return identityEntityAliasRead(ctx, d, meta)
 }
 
-func identityEntityAliasUpdate(d *schema.ResourceData, meta interface{}) error {
+func identityEntityAliasUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	vaultMutexKV.Lock(identityEntityAliasPath)
+	defer vaultMutexKV.Unlock(identityEntityAliasPath)
+
 	client := meta.(*api.Client)
 	id := d.Id()
 
 	log.Printf("[DEBUG] Updating IdentityEntityAlias %q", id)
 	path := identityEntityAliasIDPath(id)
 
+	diags := diag.Diagnostics{}
+
 	resp, err := client.Logical().Read(path)
 	if err != nil {
-		return fmt.Errorf("error updating IdentityEntityAlias %q: %s", id, err)
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("error updating IdentityEntityAlias %q: %s", id, err),
+		})
+
+		return diags
 	}
 
 	data := map[string]interface{}{
@@ -124,104 +194,130 @@ func identityEntityAliasUpdate(d *schema.ResourceData, meta interface{}) error {
 	_, err = client.Logical().Write(path, data)
 
 	if err != nil {
-		return fmt.Errorf("error updating IdentityEntityAlias %q: %s", id, err)
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("error updating IdentityEntityAlias %q: %s", id, err),
+		})
+
+		return diags
 	}
 	log.Printf("[DEBUG] Updated IdentityEntityAlias %q", id)
 
-	return identityEntityAliasRead(d, meta)
+	return identityEntityAliasRead(ctx, d, meta)
 }
 
-func identityEntityAliasRead(d *schema.ResourceData, meta interface{}) error {
+func identityEntityAliasRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*api.Client)
 	id := d.Id()
 
 	path := identityEntityAliasIDPath(id)
 
-	log.Printf("[DEBUG] Reading IdentityEntityAlias %s from %q", id, path)
+	diags := diag.Diagnostics{}
+
+	log.Printf("[DEBUG] Reading IdentityEntityAlias %q from %q", id, path)
 	resp, err := client.Logical().Read(path)
 	if err != nil {
-		return fmt.Errorf("error reading IdentityEntityAlias %q: %s", id, err)
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("error reading IdentityEntityAlias %q: %s", id, err),
+		})
+
+		return diags
 	}
 	log.Printf("[DEBUG] Read IdentityEntityAlias %s", id)
 	if resp == nil {
 		log.Printf("[WARN] IdentityEntityAlias %q not found, removing from state", id)
 		d.SetId("")
-		return nil
+
+		return diags
 	}
 
 	d.SetId(resp.Data["id"].(string))
 	for _, k := range []string{"name", "mount_accessor", "canonical_id", "custom_metadata"} {
 		if err := d.Set(k, resp.Data[k]); err != nil {
-			return fmt.Errorf("error setting state key %q on IdentityEntityAlias %q:  err=%q", k, id, err)
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("error setting state key %q on IdentityEntityAlias %q:  err=%q", k, id, err),
+			})
+
+			return diags
 		}
 	}
-	return nil
+
+	return diags
 }
 
-func identityEntityAliasDelete(d *schema.ResourceData, meta interface{}) error {
+func identityEntityAliasDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	vaultMutexKV.Lock(identityEntityAliasPath)
+	defer vaultMutexKV.Unlock(identityEntityAliasPath)
 	client := meta.(*api.Client)
 	id := d.Id()
 
 	path := identityEntityAliasIDPath(id)
+
+	diags := diag.Diagnostics{}
 
 	log.Printf("[DEBUG] Deleting IdentityEntityAlias %q", id)
 	_, err := client.Logical().Delete(path)
 	if err != nil {
-		return fmt.Errorf("error IdentityEntityAlias %q", id)
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("error IdentityEntityAlias %q", id),
+		})
+		return diags
 	}
 	log.Printf("[DEBUG] Deleted IdentityEntityAlias %q", id)
 
-	return nil
+	return diags
 }
 
-func identityEntityAliasExists(d *schema.ResourceData, meta interface{}) (bool, error) {
-	client := meta.(*api.Client)
-	id := d.Id()
-
-	path := identityEntityAliasIDPath(id)
-	key := id
-
-	// use the name if no ID is set
-	if len(id) == 0 {
-		key = d.Get("name").(string)
-		path = identityEntityAliasNamePath(key)
+func getEntityAliasesByName(client *api.Client, name string) ([]*api.Secret, error) {
+	resp, err := client.Logical().List(identityEntityAliasPath + "/id")
+	if resp == nil || err != nil {
+		return nil, err
 	}
 
-	log.Printf("[DEBUG] Checking if IdentityEntityAlias %q exists", key)
-	resp, err := client.Logical().Read(path)
-	if err != nil {
-		return true, fmt.Errorf("error checking if IdentityEntityAlias %q exists: %s", key, err)
-	}
-	log.Printf("[DEBUG] Checked if IdentityEntityAlias %q exists", key)
+	var result []*api.Secret
+	for _, id := range resp.Data["keys"].([]interface{}) {
+		config, err := client.Logical().Read(identityEntityAliasIDPath(id.(string)))
+		if err != nil || config == nil {
+			continue
+		}
 
-	return resp != nil, nil
-}
-
-func identityEntityAliasNamePath(name string) string {
-	return fmt.Sprintf("%s/name/%s", identityEntityAliasPath, name)
-}
-
-func identityEntityAliasIDPath(id string) string {
-	return fmt.Sprintf("%s/id/%s", identityEntityAliasPath, id)
-}
-
-func findAliasID(client *api.Client, canonicalID, name, mountAccessor string) (string, error) {
-	path := identityEntityIDPath(canonicalID)
-
-	resp, err := client.Logical().Read(path)
-	if err != nil {
-		return "", fmt.Errorf("error reading entity aliases: %s", err)
+		if config.Data["name"].(string) == name {
+			result = append(result, config)
+		}
 	}
 
-	if resp != nil {
-		aliases := resp.Data["aliases"].([]interface{})
-		for _, aliasRaw := range aliases {
-			alias := aliasRaw.(map[string]interface{})
-			if alias["name"] == name && alias["mount_accessor"] == mountAccessor {
-				return alias["id"].(string), nil
+	return result, err
+}
+
+func getEntityAliasesByMountAccessor(client *api.Client, accessor string) ([]map[string]interface{}, error) {
+	resp, err := client.Logical().List(identityEntityPath + "/id")
+	if resp == nil || err != nil {
+		return nil, err
+	}
+
+	result := make([]map[string]interface{}, 0)
+	for _, id := range resp.Data["keys"].([]interface{}) {
+		config, err := client.Logical().Read(identityEntityIDPath(id.(string)))
+		if err != nil || config == nil {
+			continue
+		}
+
+		if aliases, ok := config.Data["aliases"]; ok {
+			for _, v := range aliases.([]interface{}) {
+				alias := v.(map[string]interface{})
+				if alias["mount_accessor"].(string) == accessor {
+					result = append(result, alias)
+				}
 			}
 		}
 	}
 
-	return "", fmt.Errorf("unable to determine alias ID. canonical ID: %q  name: %q  mountAccessor: %q", canonicalID, name, mountAccessor)
+	return result, err
+}
+
+func identityEntityAliasIDPath(id string) string {
+	return fmt.Sprintf("%s/id/%s", identityEntityAliasPath, id)
 }
