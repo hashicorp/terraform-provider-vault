e902c9ce54158bc7cf3344954065268474edc093
Ben Ash <bash@hashicorp.com>
2022-05-10 13:11:27 -0400

    Update identity_entity resource tests for SDK changes.
    Other fixes:
    - use assertVaultState() in testAccIdentityEntityCheckAttrs() instead of
      copy-paste code

--- vault/resource_identity_entity_test.go
+++ vault/resource_identity_entity_test.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"reflect"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
@@ -21,6 +19,7 @@ import (
 func TestAccIdentityEntity(t *testing.T) {
 	entity := acctest.RandomWithPrefix("test-entity")
 
+	resourceName := "vault_identity_entity.entity"
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testutil.TestAccPreCheck(t) },
 		Providers:    testProviders,
@@ -28,7 +27,7 @@ func TestAccIdentityEntity(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIdentityEntityConfig(entity),
-				Check:  testAccIdentityEntityCheckAttrs(),
+				Check:  testAccIdentityEntityCheckAttrs(resourceName),
 			},
 		},
 	})
@@ -37,6 +36,7 @@ func TestAccIdentityEntity(t *testing.T) {
 func TestAccIdentityEntityUpdate(t *testing.T) {
 	entity := acctest.RandomWithPrefix("test-entity")
 
+	resourceName := "vault_identity_entity.entity"
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testutil.TestAccPreCheck(t) },
 		Providers:    testProviders,
@@ -44,18 +44,18 @@ func TestAccIdentityEntityUpdate(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIdentityEntityConfig(entity),
-				Check:  testAccIdentityEntityCheckAttrs(),
+				Check:  testAccIdentityEntityCheckAttrs(resourceName),
 			},
 			{
 				Config: testAccIdentityEntityConfigUpdate(entity),
 				Check: resource.ComposeTestCheckFunc(
-					testAccIdentityEntityCheckAttrs(),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "name", fmt.Sprintf("%s-2", entity)),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "metadata.version", "2"),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "policies.#", "2"),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "policies.0", "dev"),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "policies.1", "test"),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "disabled", "true"),
+					testAccIdentityEntityCheckAttrs(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", fmt.Sprintf("%s-2", entity)),
+					resource.TestCheckResourceAttr(resourceName, "metadata.version", "2"),
+					resource.TestCheckResourceAttr(resourceName, "policies.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "policies.0", "dev"),
+					resource.TestCheckResourceAttr(resourceName, "policies.1", "test"),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "true"),
 				),
 			},
 		},
@@ -65,6 +65,7 @@ func TestAccIdentityEntityUpdate(t *testing.T) {
 func TestAccIdentityEntityUpdateRemoveValues(t *testing.T) {
 	entity := acctest.RandomWithPrefix("test-entity")
 
+	resourceName := "vault_identity_entity.entity"
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testutil.TestAccPreCheck(t) },
 		Providers:    testProviders,
@@ -72,16 +73,17 @@ func TestAccIdentityEntityUpdateRemoveValues(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIdentityEntityConfig(entity),
-				Check:  testAccIdentityEntityCheckAttrs(),
+				Check:  testAccIdentityEntityCheckAttrs(resourceName),
 			},
 			{
 				Config: testAccIdentityEntityConfigUpdateRemove(entity),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "name", fmt.Sprintf("%s-2", entity)),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "external_policies", "false"),
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "disabled", "false"),
-					resource.TestCheckNoResourceAttr("vault_identity_entity.entity", "metadata"),
-					resource.TestCheckNoResourceAttr("vault_identity_entity.entity", "policies")),
+					resource.TestCheckResourceAttr(resourceName, "name", fmt.Sprintf("%s-2", entity)),
+					resource.TestCheckResourceAttr(resourceName, "external_policies", "false"),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "metadata.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "policies.#", "0"),
+				),
 			},
 		},
 	})
@@ -93,6 +95,7 @@ func TestAccIdentityEntityUpdateRemoveValues(t *testing.T) {
 func TestAccIdentityEntityUpdateRemovePolicies(t *testing.T) {
 	entity := acctest.RandomWithPrefix("test-entity")
 
+	resourceName := "vault_identity_entity.entity"
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testutil.TestAccPreCheck(t) },
 		Providers:    testProviders,
@@ -100,13 +103,14 @@ func TestAccIdentityEntityUpdateRemovePolicies(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIdentityEntityConfig(entity),
-				Check:  testAccIdentityEntityCheckAttrs(),
+				Check:  testAccIdentityEntityCheckAttrs(resourceName),
 			},
 			{
 				Config: testAccIdentityEntityConfigUpdateRemovePolicies(entity),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("vault_identity_entity.entity", "external_policies", "true"),
-					resource.TestCheckNoResourceAttr("vault_identity_entity.entity", "policies")),
+					resource.TestCheckResourceAttr(resourceName, "external_policies", "true"),
+					resource.TestCheckResourceAttr(resourceName, "policies.#", "0"),
+				),
 			},
 		},
 	})
@@ -130,98 +134,28 @@ func testAccCheckIdentityEntityDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccIdentityEntityCheckAttrs() resource.TestCheckFunc {
+func testAccIdentityEntityCheckAttrs(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		resourceState := s.Modules[0].Resources["vault_identity_entity.entity"]
-		if resourceState == nil {
-			return fmt.Errorf("resource not found in state")
-		}
-
-		instanceState := resourceState.Primary
-		if instanceState == nil {
-			return fmt.Errorf("resource not found in state")
-		}
-
-		id := instanceState.ID
-
-		path := entity.JoinEntityID(id)
-		client := testProvider.Meta().(*api.Client)
-		resp, err := client.Logical().Read(path)
+		rs, err := testGetResourceFromRootModule(s, resourceName)
 		if err != nil {
-			return fmt.Errorf("%q doesn't exist", path)
+			return err
 		}
 
-		attrs := map[string]string{
-			"name":     "name",
-			"policies": "policies",
+		path := entity.JoinEntityID(rs.Primary.ID)
+		tAttrs := []*vaultStateTest{
+			{
+				rs:        resourceName,
+				stateAttr: "name",
+				vaultAttr: "name",
+			},
+			{
+				rs:        resourceName,
+				stateAttr: "policies",
+				vaultAttr: "policies",
+			},
 		}
-		for stateAttr, apiAttr := range attrs {
-			if resp.Data[apiAttr] == nil && instanceState.Attributes[stateAttr] == "" {
-				continue
-			}
-			var match bool
-			switch resp.Data[apiAttr].(type) {
-			case json.Number:
-				apiData, err := resp.Data[apiAttr].(json.Number).Int64()
-				if err != nil {
-					return fmt.Errorf("expected API field %s to be an int, was %q", apiAttr, resp.Data[apiAttr])
-				}
-				stateData, err := strconv.ParseInt(instanceState.Attributes[stateAttr], 10, 64)
-				if err != nil {
-					return fmt.Errorf("expected state field %s to be an int, was %q", stateAttr, instanceState.Attributes[stateAttr])
-				}
-				match = apiData == stateData
-			case bool:
-				if _, ok := resp.Data[apiAttr]; !ok && instanceState.Attributes[stateAttr] == "" {
-					match = true
-				} else {
-					stateData, err := strconv.ParseBool(instanceState.Attributes[stateAttr])
-					if err != nil {
-						return fmt.Errorf("expected state field %s to be a bool, was %q", stateAttr, instanceState.Attributes[stateAttr])
-					}
-					match = resp.Data[apiAttr] == stateData
-				}
-			case []interface{}:
-				apiData := resp.Data[apiAttr].([]interface{})
-				length := instanceState.Attributes[stateAttr+".#"]
-				if length == "" {
-					if len(resp.Data[apiAttr].([]interface{})) != 0 {
-						return fmt.Errorf("expected state field %s to have %d entries, had 0", stateAttr, len(apiData))
-					}
-					match = true
-				} else {
-					count, err := strconv.Atoi(length)
-					if err != nil {
-						return fmt.Errorf("expected %s.# to be a number, got %q", stateAttr, instanceState.Attributes[stateAttr+".#"])
-					}
-					if count != len(apiData) {
-						return fmt.Errorf("expected %s to have %d entries in state, has %d", stateAttr, len(apiData), count)
-					}
 
-					for i := 0; i < count; i++ {
-						found := false
-						for stateKey, stateValue := range instanceState.Attributes {
-							if strings.HasPrefix(stateKey, stateAttr) {
-								if apiData[i] == stateValue {
-									found = true
-									break
-								}
-							}
-						}
-						if !found {
-							return fmt.Errorf("Expected item %d of %s (%s in state) of %q to be in state but wasn't", i, apiAttr, stateAttr, apiData[i])
-						}
-					}
-					match = true
-				}
-			default:
-				match = resp.Data[apiAttr] == instanceState.Attributes[stateAttr]
-			}
-			if !match {
-				return fmt.Errorf("expected %s (%s in state) of %q to be %q, got %q", apiAttr, stateAttr, path, instanceState.Attributes[stateAttr], resp.Data[apiAttr])
-			}
-		}
-		return nil
+		return assertVaultState(s, path, tAttrs...)
 	}
 }
 
