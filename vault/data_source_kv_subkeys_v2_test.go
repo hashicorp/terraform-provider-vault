77b2bd2e2e38a3c7b3e561241f9dd8e28f5f611a
Vinay Gopalan <vinay@hashicorp.com>
2022-06-03 01:56:41 -0700

    rename subkeys datasource test

--- vault/data_source_kv_subkeys_v2_test.go
+++ vault/data_source_kv_subkeys_v2_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/terraform-provider-vault/testutil"
 )
 
-func TestAccDataSourceKVSubkeys_basic(t *testing.T) {
+func TestDataSourceKVSubkeys_basic(t *testing.T) {
 	resourceName := "data.vault_kv_secret_subkeys_v2.test"
 	mount := acctest.RandomWithPrefix("tf-kvv2")
 	secretPath := acctest.RandomWithPrefix("foo")
