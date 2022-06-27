7686a5cd5891119429b6a16518a480b876734570
Austin Gebauer <agebauer@hashicorp.com>
2022-06-27 11:29:36 -0700

    Adds support for kubernetes secrets engine

---
+++ vault/resource_kubernetes_secret_backend.go
@@ -0,0 +1,178 @@
+package vault
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-vault/internal/consts"
+	"github.com/hashicorp/terraform-provider-vault/internal/provider"
+	"github.com/hashicorp/terraform-provider-vault/util"
+)
+
+const (
+	fieldKubernetesHost    = "kubernetes_host"
+	fieldKubernetesCACert  = "kubernetes_ca_cert"
+	fieldServiceAccountJWT = "service_account_jwt"
+	fieldDisableLocalCAJWT = "disable_local_ca_jwt"
+)
+
+func kubernetesSecretBackendResource() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: kubernetesSecretBackendCreateUpdate,
+		ReadContext:   kubernetesSecretBackendRead,
+		UpdateContext: kubernetesSecretBackendCreateUpdate,
+		DeleteContext: kubernetesSecretBackendDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: getKubernetesSecretsMountSchema(),
+	}
+}
+
+func getKubernetesSecretsMountSchema() map[string]*schema.Schema {
+	s := getMountSchema("type")
+	for k, v := range getKubernetesSecretsSchema() {
+		s[k] = v
+	}
+	return s
+}
+
+func getKubernetesSecretsSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		fieldKubernetesHost: {
+			Type:        schema.TypeString,
+			Description: "The Kubernetes API URL to connect to.",
+			Optional:    true,
+			Computed:    true,
+		},
+		fieldKubernetesCACert: {
+			Type: schema.TypeString,
+			Description: "A PEM-encoded CA certificate used by the secret engine to " +
+				"verify the Kubernetes API server certificate. Defaults to the " +
+				"local pod’s CA if found, or otherwise the host's root CA set.",
+			Optional: true,
+			Computed: true,
+		},
+		fieldServiceAccountJWT: {
+			Type: schema.TypeString,
+			Description: "The JSON web token of the service account used by the " +
+				"secrets engine to manage Kubernetes credentials. Defaults to the " +
+				"local pod’s JWT if found.",
+			Optional: true,
+		},
+		fieldDisableLocalCAJWT: {
+			Type: schema.TypeBool,
+			Description: "Disable defaulting to the local CA certificate and service " +
+				"account JWT when running in a Kubernetes pod.",
+			Optional: true,
+			Default:  false,
+		},
+	}
+}
+
+func kubernetesSecretBackendCreateUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, err := provider.GetClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	var path string
+	if d.IsNewResource() {
+		path = d.Get(consts.FieldPath).(string)
+		if err := createMount(d, client, path, consts.MountTypeKubernetes); err != nil {
+			return diag.FromErr(err)
+		}
+	} else {
+		if err := updateMount(d, meta, true); err != nil {
+			return diag.FromErr(err)
+		}
+		path = d.Id()
+	}
+	d.SetId(path)
+
+	data := make(map[string]interface{})
+	fields := []string{
+		fieldKubernetesCACert,
+		fieldServiceAccountJWT,
+		fieldDisableLocalCAJWT,
+	}
+	for _, k := range fields {
+		if d.HasChange(k) {
+			data[k] = d.Get(k)
+		}
+	}
+
+	// kubernetes_host always needs to be provided on configuration updates.
+	// Otherwise, an error will occur if the KUBERNETES_SERVICE_HOST and
+	// KUBERNETES_SERVICE_PORT environment variables aren't set.
+	data[fieldKubernetesHost] = d.Get(fieldKubernetesHost)
+
+	configPath := fmt.Sprintf("%s/config", path)
+	if _, err := client.Logical().Write(configPath, data); err != nil {
+		return diag.Errorf(`error writing Kubernetes backend config %q, err=%s`,
+			configPath, err)
+	}
+
+	return kubernetesSecretBackendRead(ctx, d, meta)
+}
+
+func kubernetesSecretBackendRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, err := provider.GetClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	path := d.Id()
+	resp, err := client.Logical().Read(path + "/config")
+	if err != nil {
+		return diag.Errorf("error reading Kubernetes backend at %s/config: err=%s",
+			path, err)
+	}
+	if resp == nil {
+		log.Printf("[WARN] Kubernetes config not found, removing from state")
+		d.SetId("")
+		return nil
+	}
+
+	// fieldServiceAccountJWT can't be read from the API
+	// and is intentionally omitted from this list
+	fields := []string{
+		fieldKubernetesHost,
+		fieldKubernetesCACert,
+		fieldDisableLocalCAJWT,
+	}
+	for _, k := range fields {
+		if err := d.Set(k, resp.Data[k]); err != nil {
+			return diag.Errorf("error setting state key %q on Kubernetes backend config, err=%s",
+				k, err)
+		}
+	}
+
+	if err := readMount(d, meta, true); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func kubernetesSecretBackendDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, err := provider.GetClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	path := d.Id()
+	if err := client.Sys().Unmount(path); err != nil {
+		if util.Is404(err) {
+			log.Printf("[WARN] %q not found, removing from state", path)
+			d.SetId("")
+		}
+		return diag.Errorf("error unmounting Kubernetes backend from %q, err=%s", path, err)
+	}
+	log.Printf("[DEBUG] Unmounted Kubernetes backend at %q", path)
+
+	return nil
+}
