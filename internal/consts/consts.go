aaa7b1a5ba8caeba1ed1c8fd05a49ef0f695ba26
Austin Gebauer <34121980+austingebauer@users.noreply.github.com>
2022-07-19 17:11:49 -0700

    Adds support for kubernetes secrets engine (#1515)
    * Adds support for kubernetes secrets engine
    
    * rename datasource read func
    
    * mark service_account_jwt as sensitive
    
    * Update vault/provider.go
    
    Co-authored-by: Ben Ash <32777270+benashz@users.noreply.github.com>
    
    * Update vault/provider.go
    
    Co-authored-by: Ben Ash <32777270+benashz@users.noreply.github.com>
    
    * add mount schema with mustAddSchema
    
    * use testCheckMountDestroyed
    
    * change token_max_ttl, token_default_ttl to integer seconds
    
    * adds documentation
    
    * remove computed from kubernetes_host, kubernetes_ca_cert
    
    * change env var in comment to be accurate
    
    * Add k8s-secrets docs to the sidebar (#1539)
    
    - fix formatting issues
    - tweak the example a bit
    - document import
    
    * adds comment to test about needed config to run it
    
    Co-authored-by: Ben Ash <32777270+benashz@users.noreply.github.com>

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -4,24 +4,34 @@ const (
 	/*
 		common field names
 	*/
-	FieldPath           = "path"
-	FieldParameters     = "parameters"
-	FieldMethod         = "method"
-	FieldNamespace      = "namespace"
-	FieldNamespaceID    = "namespace_id"
-	FieldBackend        = "backend"
-	FieldPathFQ         = "path_fq"
-	FieldData           = "data"
-	FieldMount          = "mount"
-	FieldName           = "name"
-	FieldVersion        = "version"
-	FieldMetadata       = "metadata"
-	FieldNames          = "names"
-	FieldLeaseID        = "lease_id"
-	FieldLeaseDuration  = "lease_duration"
-	FieldLeaseRenewable = "lease_renewable"
-	FieldDepth          = "depth"
-	FieldDataJSON       = "data_json"
+	FieldPath                     = "path"
+	FieldParameters               = "parameters"
+	FieldMethod                   = "method"
+	FieldNamespace                = "namespace"
+	FieldNamespaceID              = "namespace_id"
+	FieldBackend                  = "backend"
+	FieldPathFQ                   = "path_fq"
+	FieldData                     = "data"
+	FieldMount                    = "mount"
+	FieldName                     = "name"
+	FieldVersion                  = "version"
+	FieldMetadata                 = "metadata"
+	FieldNames                    = "names"
+	FieldLeaseID                  = "lease_id"
+	FieldLeaseDuration            = "lease_duration"
+	FieldLeaseRenewable           = "lease_renewable"
+	FieldDepth                    = "depth"
+	FieldDataJSON                 = "data_json"
+	FieldRole                     = "role"
+	FieldDescription              = "description"
+	FieldTTL                      = "ttl"
+	FieldDefaultLeaseTTL          = "default_lease_ttl_seconds"
+	FieldMaxLeaseTTL              = "max_lease_ttl_seconds"
+	FieldAuditNonHMACRequestKeys  = "audit_non_hmac_request_keys"
+	FieldAuditNonHMACResponseKeys = "audit_non_hmac_response_keys"
+	FieldLocal                    = "local"
+	FieldSealWrap                 = "seal_wrap"
+	FieldExternalEntropyAccess    = "external_entropy_access"
 
 	/*
 		common environment variables
@@ -32,12 +42,13 @@ const (
 	/*
 		common mount types
 	*/
-	MountTypeDatabase = "database"
-	MountTypePKI      = "pki"
-	MountTypeAWS      = "aws"
-	MountTypeKMIP     = "kmip"
-	MountTypeRabbitMQ = "rabbitmq"
-	MountTypeNomad    = "nomad"
+	MountTypeDatabase   = "database"
+	MountTypePKI        = "pki"
+	MountTypeAWS        = "aws"
+	MountTypeKMIP       = "kmip"
+	MountTypeRabbitMQ   = "rabbitmq"
+	MountTypeNomad      = "nomad"
+	MountTypeKubernetes = "kubernetes"
 
 	/*
 		misc. path related constants
