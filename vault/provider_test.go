3483d569214173c7abc8a3cc0053c7ffe5c03e35
Ben Ash <bash@hashicorp.com>
2022-08-02 14:39:49 -0400

    Give test ProviderFactories a try.

--- vault/provider_test.go
+++ vault/provider_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"sync"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
@@ -46,6 +47,8 @@ import (
 // each run. In case of weird behavior, restart the Vault dev server to
 // start over with a fresh Vault. (Remember to reset VAULT_TOKEN.)
 
+const providerName = "vault"
+
 func TestProvider(t *testing.T) {
 	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
@@ -58,10 +61,28 @@ var (
 )
 
 func init() {
-	testProvider = Provider()
-	testProviders = map[string]*schema.Provider{
-		"vault": testProvider,
-	}
+	initTestProvider()
+}
+
+func initTestProvider() {
+	o := sync.Once{}
+	o.Do(
+		func() {
+			if testProvider == nil {
+				testProvider = Provider()
+				testProviders = map[string]*schema.Provider{
+					providerName: testProvider,
+				}
+			}
+		},
+	)
+}
+
+var providerFactories = map[string]func() (*schema.Provider, error){
+	providerName: func() (*schema.Provider, error) {
+		initTestProvider()
+		return testProvider, nil
+	},
 }
 
 // A basic token helper script.
