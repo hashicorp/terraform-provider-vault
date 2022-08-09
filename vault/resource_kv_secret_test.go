e23506b34a074546367933bec992886108f4e196
Lars With <lars@with.de>
2022-08-09 23:47:25 +0200

    refactor api access check to functional pattern

--- vault/resource_kv_secret_test.go
+++ vault/resource_kv_secret_test.go
@@ -28,7 +28,7 @@ func TestAccKVSecret(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, consts.FieldPath, fmt.Sprintf("%s/%s", mount, name)),
 					resource.TestCheckResourceAttr(resourceName, "data.zip", "zap"),
 					resource.TestCheckResourceAttr(resourceName, "data.foo", "bar"),
-					testResourceKVSecret_basic_apiAcessCheck,
+					testResourceKVSecret_apiAcessCheck("zap"),
 				),
 			},
 			{
@@ -37,7 +37,7 @@ func TestAccKVSecret(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, consts.FieldPath, fmt.Sprintf("%s/%s", mount, name)),
 					resource.TestCheckResourceAttr(resourceName, "data.zip", "zoop"),
 					resource.TestCheckResourceAttr(resourceName, "data.foo", "bar"),
-					testResourceKVSecret_updated_apiAcessCheck,
+					testResourceKVSecret_apiAcessCheck("zoop"),
 				),
 			},
 			{
@@ -102,33 +102,27 @@ resource "vault_kv_secret" "test" {
 	return ret
 }
 
-func testResourceKVSecret_apiAcessCheck(s *terraform.State, want string) error {
-	resourceState := s.Modules[0].Resources["vault_kv_secret.test"]
-	state := resourceState.Primary
+func testResourceKVSecret_apiAcessCheck(want string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceState := s.Modules[0].Resources["vault_kv_secret.test"]
+		state := resourceState.Primary
 
-	path := state.ID
+		path := state.ID
 
-	client, err := provider.GetClient(state, testProvider.Meta())
-	if err != nil {
-		return err
+		client, err := provider.GetClient(state, testProvider.Meta())
+		if err != nil {
+			return err
+		}
+
+		secret, err := client.Logical().Read(path)
+		if err != nil {
+			return fmt.Errorf("error reading back secret: %s", err)
+		}
+
+		if got := secret.Data["zip"]; got != want {
+			return fmt.Errorf("'zip' data is %q; want %q", got, want)
+		}
+
+		return nil
 	}
-
-	secret, err := client.Logical().Read(path)
-	if err != nil {
-		return fmt.Errorf("error reading back secret: %s", err)
-	}
-
-	if got := secret.Data["zip"]; got != want {
-		return fmt.Errorf("'zip' data is %q; want %q", got, want)
-	}
-
-	return nil
-}
-
-func testResourceKVSecret_basic_apiAcessCheck(s *terraform.State) error {
-	return testResourceKVSecret_apiAcessCheck(s, "zap")
-}
-
-func testResourceKVSecret_updated_apiAcessCheck(s *terraform.State) error {
-	return testResourceKVSecret_apiAcessCheck(s, "zoop")
 }
