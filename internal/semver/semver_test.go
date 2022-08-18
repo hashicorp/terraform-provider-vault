3ab7c4abf69bb47f2b7b0698b3f7efa5dea21721
Vinay Gopalan <vinay@hashicorp.com>
2022-08-18 14:48:57 -0700

    add test for semantic version comparison

--- internal/semver/semver_test.go
+++ internal/semver/semver_test.go
@@ -1,49 +1,100 @@
 package semver
 
-//func TestSemVerComparison(t *testing.T) {
-//	client := testProvider.Meta().(*api.Client)
-//
-//	testCases := []struct {
-//		name         string
-//		minVersion   string
-//		expected     bool
-//		retryHandler *testRetryHandler
-//	}{
-//		{
-//			"less-than",
-//			"1.8.0",
-//			true,
-//		},
-//		{
-//			"greater-than",
-//			"1.12.0",
-//			false,
-//		},
-//		{
-//			"equal",
-//			"1.10.0",
-//			true,
-//		},
-//	}
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
 
-//config, ln := testHTTPServer(t, r.handler())
-//defer ln.Close()
-//
-//config.Address = fmt.Sprintf("http://%s", ln.Addr())
-//c, err := api.NewClient(config)
-//if err != nil {
-//	t.Fatal(err)
-//}
-//
-//server := testHTTPServer()
-//	for _, tt := range testCases {
-//		compare, err := semVerComparison(tt.minVersion, client)
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		if compare != tt.expected {
-//			t.Fatalf("expected semantic version to return %t, got %t", tt.expected, compare)
-//		}
-//	}
-//}
+	"github.com/hashicorp/vault/api"
+
+	"github.com/hashicorp/terraform-provider-vault/internal/consts"
+	"github.com/hashicorp/terraform-provider-vault/testutil"
+)
+
+type testSemanticVersionHandler struct {
+	version string
+}
+
+func (t *testSemanticVersionHandler) handler() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodGet {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		data := map[string]interface{}{}
+		data[consts.FieldVersion] = t.version
+
+		m, err := json.Marshal(data)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(m)
+	}
+}
+
+func TestSemVerComparison(t *testing.T) {
+	testCases := []struct {
+		name           string
+		minVersion     string
+		expected       bool
+		versionHandler *testSemanticVersionHandler
+		wantErr        bool
+	}{
+		{
+			"server-greater-than",
+			"1.8.0",
+			true,
+			&testSemanticVersionHandler{
+				version: "1.11.0",
+			},
+			false,
+		},
+		{
+			"server-less-than",
+			"1.12.0",
+			false,
+			&testSemanticVersionHandler{
+				version: "1.11.0+ent",
+			},
+			false,
+		},
+		{
+			"server-equal",
+			"1.10.0",
+			true,
+			&testSemanticVersionHandler{
+				version: "1.10.0",
+			},
+			false,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			r := tt.versionHandler
+
+			config, ln := testutil.TestHTTPServer(t, r.handler())
+			defer ln.Close()
+
+			config.Address = fmt.Sprintf("http://%s", ln.Addr())
+			c, err := api.NewClient(config)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			isTFVersionGreater, _, err := SemanticVersionComparison(tt.minVersion, c)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SemanticVersionComparison() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if isTFVersionGreater != tt.expected {
+				t.Errorf("SemanticVersionComparison() got = %v, want %v", isTFVersionGreater, tt.expected)
+			}
+		})
+	}
+}
