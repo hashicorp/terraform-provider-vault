6bfc83f930192fd4e3620200d2ad27591f9cfbe9
Vinay Gopalan <vinay@hashicorp.com>
2022-08-18 18:48:56 -0700

    add test for invalid min version string

--- internal/semver/semver_test.go
+++ internal/semver/semver_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/vault/api"
@@ -24,8 +25,9 @@ func (t *testSemanticVersionHandler) handler() http.HandlerFunc {
 			return
 		}
 
-		data := map[string]interface{}{}
-		data[consts.FieldVersion] = t.version
+		data := map[string]interface{}{
+			consts.FieldVersion: t.version,
+		}
 
 		m, err := json.Marshal(data)
 		if err != nil {
@@ -45,33 +47,44 @@ func TestGreaterThanOrEqual(t *testing.T) {
 		expected       bool
 		versionHandler *testSemanticVersionHandler
 		wantErr        bool
+		expectedErr    string
 	}{
 		{
-			"server-greater-than",
-			"1.8.0",
-			true,
-			&testSemanticVersionHandler{
+			name:       "server-greater-than",
+			minVersion: "1.8.0",
+			expected:   true,
+			versionHandler: &testSemanticVersionHandler{
 				version: "1.11.0",
 			},
-			false,
+			wantErr: false,
 		},
 		{
-			"server-less-than",
-			"1.12.0",
-			false,
-			&testSemanticVersionHandler{
+			name:       "server-less-than",
+			minVersion: "1.12.0",
+			expected:   false,
+			versionHandler: &testSemanticVersionHandler{
 				version: "1.11.0+ent",
 			},
-			false,
+			wantErr: false,
 		},
 		{
-			"server-equal",
-			"1.10.0",
-			true,
-			&testSemanticVersionHandler{
+			name:       "server-equal",
+			minVersion: "1.10.0",
+			expected:   true,
+			versionHandler: &testSemanticVersionHandler{
 				version: "1.10.0",
 			},
-			false,
+			wantErr: false,
+		},
+		{
+			name:       "invalid-min-version",
+			minVersion: "invalid",
+			expected:   false,
+			versionHandler: &testSemanticVersionHandler{
+				version: "1.11.0",
+			},
+			wantErr:     true,
+			expectedErr: "Malformed version",
 		},
 	}
 
@@ -92,8 +105,11 @@ func TestGreaterThanOrEqual(t *testing.T) {
 
 			isTFVersionGreater, _, err := GreaterThanOrEqual(ctx, c, tt.minVersion)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("GreaterThanOrEqual() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GreaterThanOrEqual() got an error=%s, wantErr %v", err.Error(), tt.wantErr)
 				return
+			}
+			if err != nil && !strings.Contains(err.Error(), tt.expectedErr) {
+				t.Errorf("GreaterThanOrEqual() error = %s, expectedErr %s", err.Error(), tt.expectedErr)
 			}
 			if isTFVersionGreater != tt.expected {
 				t.Errorf("GreaterThanOrEqual() got = %v, want %v", isTFVersionGreater, tt.expected)
