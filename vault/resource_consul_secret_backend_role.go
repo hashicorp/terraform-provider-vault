02e766ba2aaddf540d32662180a30fdef7876fb1
Vinay Gopalan <vinay@hashicorp.com>
2022-09-12 10:20:47 -0700

    fix formatting on imports

--- vault/resource_consul_secret_backend_role.go
+++ vault/resource_consul_secret_backend_role.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/hashicorp/terraform-provider-vault/internal/consts"
