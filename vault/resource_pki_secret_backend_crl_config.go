06404babf39ca0d0989f1c3b2223cd4ba0f29488
Thomas Manninger <t.manninger@ixolit.com>
2022-12-14 07:49:47 +0100

    Add parameters to resource vault_pki_secret_backend_crl_config

--- vault/resource_pki_secret_backend_crl_config.go
+++ vault/resource_pki_secret_backend_crl_config.go
@@ -38,6 +38,39 @@ func pkiSecretBackendCrlConfigResource() *schema.Resource {
 				Optional:    true,
 				Description: "Disables or enables CRL building",
 			},
+			"ocsp_disable": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Disables or enables the OCSP responder in Vault.",
+			},
+			"ocsp_expiry": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The amount of time an OCSP response can be cached for, (controls the NextUpdate field), useful for OCSP stapling refresh durations.",
+				Default:     "12h",
+			},
+			"auto_rebuild": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Enables or disables periodic rebuilding of the CRL upon expiry.",
+			},
+			"auto_rebuild_grace_period": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Grace period before CRL expiry to attempt rebuild of CRL.",
+				Default:     "12h",
+			},
+			"enable_delta": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Enables or disables building of delta CRLs with up-to-date revocation information, augmenting the last complete CRL.",
+			},
+			"delta_rebuild_interval": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Interval to check for new revocations on, to regenerate the delta CRL.",
+				Default:     "15m",
+			},
 		},
 	}
 }
@@ -57,6 +90,24 @@ func pkiSecretBackendCrlConfigCreate(d *schema.ResourceData, meta interface{}) e
 	}
 	if disable, ok := d.GetOk("disable"); ok {
 		data["disable"] = disable
+	}
+	if ocsp_disable, ok := d.GetOk("ocsp_disable"); ok {
+		data["ocsp_disable"] = ocsp_disable
+	}
+	if ocsp_expiry, ok := d.GetOk("ocsp_expiry"); ok {
+		data["ocsp_expiry"] = ocsp_expiry
+	}
+	if auto_rebuild, ok := d.GetOk("auto_rebuild"); ok {
+		data["auto_rebuild"] = auto_rebuild
+	}
+	if auto_rebuild_grace_period, ok := d.GetOk("auto_rebuild_grace_period"); ok {
+		data["auto_rebuild_grace_period"] = auto_rebuild_grace_period
+	}
+	if enable_delta, ok := d.GetOk("enable_delta"); ok {
+		data["enable_delta"] = enable_delta
+	}
+	if delta_rebuild_interval, ok := d.GetOk("delta_rebuild_interval"); ok {
+		data["delta_rebuild_interval"] = delta_rebuild_interval
 	}
 
 	log.Printf("[DEBUG] Creating CRL config on PKI secret backend %q", backend)
@@ -94,6 +145,12 @@ func pkiSecretBackendCrlConfigRead(d *schema.ResourceData, meta interface{}) err
 
 	d.Set("expiry", config.Data["expiry"])
 	d.Set("disable", config.Data["disable"])
+	d.Set("ocsp_disable", config.Data["ocsp_disable"])
+	d.Set("ocsp_expiry", config.Data["ocsp_expiry"])
+	d.Set("auto_rebuild", config.Data["auto_rebuild"])
+	d.Set("auto_rebuild_grace_period", config.Data["auto_rebuild_grace_period"])
+	d.Set("enable_delta", config.Data["enable_delta"])
+	d.Set("delta_rebuild_interval", config.Data["delta_rebuild_interval"])
 
 	return nil
 }
@@ -113,6 +170,24 @@ func pkiSecretBackendCrlConfigUpdate(d *schema.ResourceData, meta interface{}) e
 	}
 	if disable, ok := d.GetOk("disable"); ok {
 		data["disable"] = disable
+	}
+	if ocsp_disable, ok := d.GetOk("ocsp_disable"); ok {
+		data["ocsp_disable"] = ocsp_disable
+	}
+	if ocsp_expiry, ok := d.GetOk("ocsp_expiry"); ok {
+		data["ocsp_expiry"] = ocsp_expiry
+	}
+	if auto_rebuild, ok := d.GetOk("auto_rebuild"); ok {
+		data["auto_rebuild"] = auto_rebuild
+	}
+	if auto_rebuild_grace_period, ok := d.GetOk("auto_rebuild_grace_period"); ok {
+		data["auto_rebuild_grace_period"] = auto_rebuild_grace_period
+	}
+	if enable_delta, ok := d.GetOk("enable_delta"); ok {
+		data["enable_delta"] = enable_delta
+	}
+	if delta_rebuild_interval, ok := d.GetOk("delta_rebuild_interval"); ok {
+		data["delta_rebuild_interval"] = delta_rebuild_interval
 	}
 
 	log.Printf("[DEBUG] Updating CRL config on PKI secret backend %q", backend)
