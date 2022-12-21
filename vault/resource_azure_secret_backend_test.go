8396d0b80046498f1c62900819121e4e2fed2521
Vinay Gopalan <vinay@hashicorp.com>
2022-12-21 10:10:27 -0800

    fix failing remount test is CI: try 1

--- vault/resource_azure_secret_backend_test.go
+++ vault/resource_azure_secret_backend_test.go
@@ -81,7 +81,7 @@ func TestAzureSecretBackend(t *testing.T) {
 func TestAzureSecretBackend_remount(t *testing.T) {
 	testutil.SkipTestAcc(t)
 
-	// path := acctest.RandomWithPrefix("tf-test-azure")
+	path := acctest.RandomWithPrefix("tf-test-azure")
 	updatedPath := acctest.RandomWithPrefix("tf-test-azure-updated")
 
 	resourceType := "vault_azure_secret_backend"
@@ -100,12 +100,12 @@ func TestAzureSecretBackend_remount(t *testing.T) {
 			resource.TestCheckResourceAttr(resourceName, "use_microsoft_graph_api", "false"))
 	}
 
-	//azureInitialCheckFuncs := append(commonChecks,
-	//	resource.TestCheckResourceAttr(resourceName, consts.FieldPath, path),
-	//)
-
 	azureUpdatedCheckFuncs := append(commonChecks,
 		resource.TestCheckResourceAttr(resourceName, consts.FieldPath, updatedPath),
+	)
+
+	azureInitialCheckFuncs := append(commonChecks,
+		resource.TestCheckResourceAttr(resourceName, consts.FieldPath, path),
 	)
 
 	resource.Test(t, resource.TestCase{
@@ -115,10 +115,10 @@ func TestAzureSecretBackend_remount(t *testing.T) {
 		},
 		CheckDestroy: testCheckMountDestroyed(resourceType, consts.MountTypeAzure, consts.FieldPath),
 		Steps: []resource.TestStep{
-			//{
-			//	Config: testAzureSecretBackend_initialConfig(path),
-			//	Check:  resource.ComposeTestCheckFunc(azureInitialCheckFuncs...),
-			//},
+			{
+				Config: testAzureSecretBackend_initialConfig(path),
+				Check:  resource.ComposeTestCheckFunc(azureInitialCheckFuncs...),
+			},
 			{
 				Config: testAzureSecretBackend_initialConfig(updatedPath),
 				Check:  resource.ComposeTestCheckFunc(azureUpdatedCheckFuncs...),
