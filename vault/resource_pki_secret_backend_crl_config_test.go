10107c48c406627bbe31c906cf0de51b11cb0564
tmanninger <47749648+tmanninger@users.noreply.github.com>
2022-12-22 17:47:21 +0100

    Add parameters to resource vault_pki_secret_backend_crl_config (#1704)
    Co-authored-by: Ben Ash <32777270+benashz@users.noreply.github.com>

--- vault/resource_pki_secret_backend_crl_config_test.go
+++ vault/resource_pki_secret_backend_crl_config_test.go
@@ -25,6 +25,25 @@ func TestPkiSecretBackendCrlConfig_basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "expiry", "72h"),
 					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "disable", "true"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "ocsp_disable", "false"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "ocsp_expiry", "12h"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "auto_rebuild", "false"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "auto_rebuild_grace_period", "12h"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "enable_delta", "false"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "delta_rebuild_interval", "15m"),
+				),
+			},
+			{
+				Config: testPkiSecretBackendCrlConfigConfig_updated(rootPath),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "expiry", "72h"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "disable", "true"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "ocsp_disable", "true"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "ocsp_expiry", "13h"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "auto_rebuild", "true"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "auto_rebuild_grace_period", "14h"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "enable_delta", "true"),
+					resource.TestCheckResourceAttr("vault_pki_secret_backend_crl_config.test", "delta_rebuild_interval", "30m"),
 				),
 			},
 		},
@@ -63,6 +82,49 @@ resource "vault_pki_secret_backend_crl_config" "test" {
 
   expiry = "72h"
   disable = true
+} 
+
+`, rootPath)
+}
+
+func testPkiSecretBackendCrlConfigConfig_updated(rootPath string) string {
+	return fmt.Sprintf(`
+resource "vault_mount" "test-root" {
+  path = "%s"
+  type = "pki"
+  description = "test root"
+  default_lease_ttl_seconds = "8640000"
+  max_lease_ttl_seconds = "8640000"
+}
+
+resource "vault_pki_secret_backend_root_cert" "test-ca" {
+	backend    = vault_mount.test-root.path
+	depends_on = ["vault_mount.test-root"]
+
+	type                 = "internal"
+	common_name          = "test-ca.example.com"
+	ttl                  = "8640000"
+	format               = "pem"
+	private_key_format   = "der"
+	key_type             = "rsa"
+	key_bits             = 4096
+	ou                   = "Test OU"
+	organization         = "ACME Ltd"
+}
+
+resource "vault_pki_secret_backend_crl_config" "test" {
+  depends_on = ["vault_mount.test-root","vault_pki_secret_backend_root_cert.test-ca"]
+
+  backend = vault_mount.test-root.path
+
+  expiry = "72h"
+  disable = true
+  ocsp_disable = true
+  ocsp_expiry = "13h"
+  auto_rebuild = true
+  auto_rebuild_grace_period = "14h"
+  enable_delta = true
+  delta_rebuild_interval = "30m"
 } 
 
 `, rootPath)
