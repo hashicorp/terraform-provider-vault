23a1f8b349356662a0c42eeabde296e062460d2b
davidadeleon <56207066+davidadeleon@users.noreply.github.com>
2023-03-07 17:47:27 -0500

    Add PKI Unified CRL parameters (#1789)
    * add parameters for cross cluster PKI cert revocation introduced in Vault 1.13.0
    
    * fix update and adjust test
    
    * isolate 1.12 test
    
    * update test skip logic to better isolate 1.12 test
    
    * clarify versions around newly added parameters

--- internal/provider/meta.go
+++ internal/provider/meta.go
@@ -37,6 +37,7 @@ var (
 	VaultVersion110 = version.Must(version.NewSemver(consts.VaultVersion110))
 	VaultVersion111 = version.Must(version.NewSemver(consts.VaultVersion111))
 	VaultVersion112 = version.Must(version.NewSemver(consts.VaultVersion112))
+	VaultVersion113 = version.Must(version.NewSemver(consts.VaultVersion113))
 )
 
 // ProviderMeta provides resources with access to the Vault client and
