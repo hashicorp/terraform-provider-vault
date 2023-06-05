8df0dced494c5a4ab42c01ad6b71b50005c4826b
Kay Craig <kay.craig@hashicorp.com>
2023-06-05 10:15:42 -0400

    update resource test

--- vault/resource_aws_secret_backend_static_role_test.go
+++ vault/resource_aws_secret_backend_static_role_test.go
@@ -1,45 +1,53 @@
 package vault
 
 import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-vault/internal/provider"
+	"github.com/hashicorp/terraform-provider-vault/testutil"
 	"testing"
 )
 
 func TestAccAWSSecretBackendStaticRole(t *testing.T) {
-	//mount := acctest.RandomWithPrefix("tf-aws-static")
+	mount := acctest.RandomWithPrefix("tf-aws-static")
+	accessKey, secretKey := testutil.GetTestAWSCreds(t)
 	//name := acctest.RandomWithPrefix("tf-role")
-	//username := "jane-doe"
+	username := testutil.SkipTestEnvUnset(t, "AWS_STATIC_USER")[0]
 	//rotationPeriod := "15m"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
-			SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion114)
+			//SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion114)
 		},
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAWSSecretBackendStaticRoleConfig,
+				Config: testAWSStaticReourceConfig(mount, accessKey, secretKey, username),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.vault_aws_secret_backend_static_role.role", "rotation_period", "bad"),
+					resource.TestCheckResourceAttr("vault_aws_secret_backend_static_role.role", "name", "test"),
+					resource.TestCheckResourceAttr("vault_aws_secret_backend_static_role.role", "username", "vault-static-roles-test"),
+					resource.TestCheckResourceAttr("vault_aws_secret_backend_static_role.role", "rotation_period", "3600"),
 				),
 			},
 		},
 	})
 }
 
-const testAWSSecretBackendStaticRoleConfig = `
+const testAWSStaticResource = `
 resource "vault_aws_secret_backend" "aws" {
 	path = "%s"
 	description = "Obtain AWS credentials."
-	access_key = "%s"
-	secret_key = "%s"
-	region = "%s"
 }
 
 resource "vault_aws_secret_backend_static_role" "role" {
-	backend = vault_aws_secret_backend.aws.path
+	mount = vault_aws_secret_backend.aws.path
 	name = "test"
-	username = "jane-doe"
-	rotation_period = "1h"
+	username = "%s"
+	rotation_period = "3600"
 }`
+
+//, mountPath, accessKey, secretKey, region)`
+
+func testAWSStaticReourceConfig(mount, access, secret, username string) string {
+	return fmt.Sprintf(testAWSStaticResource, mount, username)
+}
