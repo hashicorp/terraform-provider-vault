60b9595e3878671b0220591856f6ddd6054b608b
Kay Craig <kay.craig@hashicorp.com>
2023-06-05 11:23:34 -0400

    clean up test cases

--- vault/data_source_aws_static_credentials_test.go
+++ vault/data_source_aws_static_credentials_test.go
@@ -5,22 +5,19 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-vault/internal/consts"
-	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 	"github.com/hashicorp/terraform-provider-vault/testutil"
 	"testing"
 )
 
 func TestAccDataSourceAWSStaticCredentials(t *testing.T) {
-	//mountPath := acctest.RandomWithPrefix("tf-test-aws-static")
-	//accessKey, secretKey := testutil.GetTestAWSCreds(t) // from the environment, causes test to skip if unset
+	_, _ = testutil.GetTestAWSCreds(t)
 	username := testutil.SkipTestEnvUnset(t, "AWS_STATIC_USER")[0]
 	mount := acctest.RandomWithPrefix("tf-aws-static")
 
 	resource.Test(t, resource.TestCase{
-		IsUnitTest: false,
 		PreCheck: func() {
 			testutil.TestAccPreCheck(t)
-			SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion114)
+			//SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion114)
 		},
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
@@ -34,14 +31,6 @@ func TestAccDataSourceAWSStaticCredentials(t *testing.T) {
 		},
 	})
 }
-
-//resource "vault_aws_secret_backend" "aws" {
-//path = "%s"
-//description = "Obtain AWS credentials."
-//access_key = "%s"
-//secret_key = "%s"
-//region = "%s"
-//}
 
 const testAWSStaticDataResource = `
 resource "vault_aws_secret_backend" "aws" {
--- vault/resource_aws_secret_backend_static_role_test.go
+++ vault/resource_aws_secret_backend_static_role_test.go
@@ -4,27 +4,24 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 	"github.com/hashicorp/terraform-provider-vault/testutil"
 	"testing"
 )
 
 func TestAccAWSSecretBackendStaticRole(t *testing.T) {
 	mount := acctest.RandomWithPrefix("tf-aws-static")
-	accessKey, secretKey := testutil.GetTestAWSCreds(t)
-	//name := acctest.RandomWithPrefix("tf-role")
+	_, _ = testutil.GetTestAWSCreds(t)
 	username := testutil.SkipTestEnvUnset(t, "AWS_STATIC_USER")[0]
-	//rotationPeriod := "15m"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testutil.TestAccPreCheck(t)
-			SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion114)
+			//SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion114)
 		},
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAWSStaticReourceConfig(mount, accessKey, secretKey, username),
+				Config: testAWSStaticReourceConfig(mount, username),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("vault_aws_secret_backend_static_role.role", "name", "test"),
 					resource.TestCheckResourceAttr("vault_aws_secret_backend_static_role.role", "username", "vault-static-roles-test"),
@@ -48,8 +45,6 @@ resource "vault_aws_secret_backend_static_role" "role" {
 	rotation_period = "3600"
 }`
 
-//, mountPath, accessKey, secretKey, region)`
-
-func testAWSStaticReourceConfig(mount, access, secret, username string) string {
+func testAWSStaticReourceConfig(mount, username string) string {
 	return fmt.Sprintf(testAWSStaticResource, mount, username)
 }
