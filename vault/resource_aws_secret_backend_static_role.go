813b9275bb3143ce3268475ea7cce637d7d0c246
Kay Craig <kay.craig@hashicorp.com>
2023-06-09 14:42:22 -0400

    formatting?

--- vault/resource_aws_secret_backend_static_role.go
+++ vault/resource_aws_secret_backend_static_role.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"log"
 	"regexp"
-	
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-vault/internal/consts"
