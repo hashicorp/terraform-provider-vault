c8da7fe1f09057a7c526b4c4c26c8c284fa07abe
mmskv <max.mishuk2014@gmail.com>
2023-10-11 00:22:09 +0300

    test: check that public oidc client client_secret is ""

--- vault/data_identity_oidc_client_creds_test.go
+++ vault/data_identity_oidc_client_creds_test.go
@@ -44,7 +44,53 @@ resource "vault_identity_oidc_client" "test" {
   name          = "%s"
   key           = vault_identity_oidc_key.key.name
   redirect_uris = [
-	"http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback", 
+	"http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback",
+	"http://127.0.0.1:8251/callback",
+	"http://127.0.0.1:8080/callback"
+  ]
+  id_token_ttl     = 2400
+  access_token_ttl = 7200
+}
+
+data "vault_identity_oidc_client_creds" "creds" {
+  name = vault_identity_oidc_client.test.name
+}`, name)
+}
+
+func TestDataSourceIdentityOIDCClientCreds_PublicClient(t *testing.T) {
+	t.Parallel()
+	name := acctest.RandomWithPrefix("test-public-client")
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: providerFactories,
+		PreCheck:          func() { testutil.TestAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceIdentityOIDCClientCreds_PublicClientConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.vault_identity_oidc_client_creds.creds", "name", name),
+					resource.TestCheckResourceAttr("data.vault_identity_oidc_client_creds.creds", "client_secret", ""),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceIdentityOIDCClientCreds_PublicClientConfig(name string) string {
+	return fmt.Sprintf(`
+resource "vault_identity_oidc_key" "key" {
+  name               = "key"
+  allowed_client_ids = ["*"]
+  rotation_period    = 3600
+  verification_ttl   = 3600
+}
+
+resource "vault_identity_oidc_client" "test" {
+  name             = "%s"
+  client_type      = "public"
+  key              = vault_identity_oidc_key.key.name
+  redirect_uris    = [
+	"http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback",
 	"http://127.0.0.1:8251/callback",
 	"http://127.0.0.1:8080/callback"
   ]
