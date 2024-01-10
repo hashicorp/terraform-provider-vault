e103df8345ba208e6fd78161142b8f147ebfac73
maxcoulombe <max.coulombe@hashicorp.com>
2024-01-10 11:52:16 -0500

    * format

--- vault/resource_aws_secrets_sync_destination.go
+++ vault/resource_aws_secrets_sync_destination.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	"github.com/hashicorp/terraform-provider-vault/internal/consts"
 	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 )
