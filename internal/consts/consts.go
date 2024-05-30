4bbc945e5aeaec721ef47406a19442324f6a7df7
Milena Zlaticanin <60530402+Zlaticanin@users.noreply.github.com>
2024-05-30 16:41:46 -0700

    Add Azure Secrets WIF fields (#2250)
    * Add Azure Secrets WIF fields

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -423,6 +423,7 @@ const (
 	FieldAllowedResponseHeaders        = "allowed_response_headers"
 	FieldDelegatedAuthAccessors        = "delegated_auth_accessors"
 	FieldPluginVersion                 = "plugin_version"
+	FieldUseMSGraphAPI                 = "use_microsoft_graph_api"
 
 	/*
 		common environment variables
