d8aeeabfa63c47a1b8cfc7c0c69c0b9f8b8e1dd7
Vinay Gopalan <vinay@hashicorp.com>
2024-06-03 13:59:41 -0700

    add doc string for test env variable constant

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -455,7 +455,8 @@ const (
 	// EnvVarTokenFilename for the TokenFile auth login.
 	EnvVarTokenFilename = "TERRAFORM_VAULT_TOKEN_FILENAME"
 
-	// EnvVarVaultConfigPath to override where the Vault configuration is.
+	// EnvVarVaultConfigPath to override where the Vault configuration is in tests.
+	// Note: only used in tests. not used by the provider to read the Vault config.
 	EnvVarVaultConfigPath = "VAULT_CONFIG_PATH"
 	/*
 		common mount types
