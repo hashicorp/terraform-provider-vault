c6de432d70f95c6c95c6f240788b61a624a2241f
jbdelavoix <jbdelavoix@users.noreply.github.com>
2025-01-30 15:29:32 +0100

    feat(data_vault_namespaces): add recursive search

--- vault/data_source_namespaces.go
+++ vault/data_source_namespaces.go
@@ -6,6 +6,7 @@ package vault
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -21,34 +22,87 @@ func namespacesDataSource() *schema.Resource {
 		ReadContext: provider.ReadContextWrapper(namespacesDataSourceRead),
 
 		Schema: map[string]*schema.Schema{
+			"recursive": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "True to fetch all child namespaces.",
+			},
 			consts.FieldPaths: {
 				Type:        schema.TypeSet,
 				Computed:    true,
 				Description: "Namespace paths.",
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
+			consts.FieldPathsFQ: {
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Description: "The fully qualified namespace paths.",
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
 		},
 	}
 }
 
-func namespacesDataSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client, e := provider.GetClient(d, meta)
-	if e != nil {
-		return diag.FromErr(e)
-	}
+func namespacesReadNamespacePaths(ctx context.Context, client *api.Client, namespace string, recursive bool) ([]string, diag.Diagnostics) {
+	var allNamespaces []string
 
-	log.Printf("[DEBUG] Reading namespaces from Vault")
+	client.SetNamespace(namespace)
 
 	resp, err := client.Logical().ListWithContext(ctx, consts.SysNamespaceRoot)
 	if err != nil {
-		return diag.Errorf("error reading namespaces from Vault: %s", err)
-	}
-	if err := d.Set(consts.FieldPaths, flattenPaths(resp)); err != nil {
-		return diag.Errorf("error setting %q to state: %s", consts.FieldPaths, err)
+		return nil, diag.Errorf("error reading namespaces from Vault: %v", err)
 	}
 
-	id := mountutil.NormalizeMountPath(client.Namespace())
+	prefix := ""
+	if namespace != "" {
+		prefix = namespace + "/"
+	}
+
+	for _, ns := range flattenPaths(resp) {
+		allNamespaces = append(allNamespaces, prefix+ns)
+
+		if recursive {
+			subNamespaces, diags := namespacesReadNamespacePaths(ctx, client, prefix+ns, true)
+			if diags.HasError() {
+				return nil, diags
+			}
+			allNamespaces = append(allNamespaces, subNamespaces...)
+		}
+	}
+
+	return allNamespaces, nil
+}
+
+func namespacesDataSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, err := provider.GetClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	namespace := client.Namespace()
+	id := mountutil.NormalizeMountPath(namespace)
 	d.SetId(id)
+
+	log.Printf("[DEBUG] Reading namespaces from Vault")
+
+	absolutePaths, diags := namespacesReadNamespacePaths(ctx, client, namespace, d.Get("recursive").(bool))
+	if diags.HasError() {
+		return diags
+	}
+
+	if err := d.Set(consts.FieldPathsFQ, absolutePaths); err != nil {
+		return diag.Errorf("error setting %q to state: %v", consts.FieldPathsFQ, err)
+	}
+
+	var relativePaths []string
+	for _, absolutePath := range absolutePaths {
+		relativePaths = append(relativePaths, strings.TrimPrefix(absolutePath, namespace+"/"))
+	}
+
+	if err := d.Set(consts.FieldPaths, relativePaths); err != nil {
+		return diag.Errorf("error setting %q to state: %v", consts.FieldPaths, err)
+	}
 
 	return nil
 }
--- website/docs/d/namespaces.html.md
+++ website/docs/d/namespaces.html.md
@@ -3,26 +3,34 @@ layout: "vault"
 page_title: "Vault: vault_namespaces data source"
 sidebar_current: "docs-vault-datasource-namespaces"
 description: |-
-  Fetches a list of all direct child namespaces in Vault
+  List of all child namespaces in Vault
 ---
 
 # vault\_namespace
 
-Lists all direct child [Namespaces](https://developer.hashicorp.com/vault/docs/enterprise/namespaces) in Vault.
+Lists all child [Namespaces](https://developer.hashicorp.com/vault/docs/enterprise/namespaces) in Vault.
 
 **Note** this feature is available only with Vault Enterprise.
 
 ## Example Usage
 
-### Child namespaces
+### Direct child namespaces
 
 ```hcl
 data "vault_namespaces" "children" {}
 ```
 
-### Nested namespace
+### All child namespaces
 
-To fetch the details of nested namespaces:
+```hcl
+data "vault_namespaces" "children" {
+  recursive = true
+}
+```
+
+### Child namespace details
+
+To fetch the details of child namespaces:
 
 ```hcl
 data "vault_namespaces" "children" {
@@ -43,9 +51,11 @@ The following arguments are supported:
 * `namespace` - (Optional) The namespace to provision the resource in.
   The value should not contain leading or trailing forward slashes.
   The `namespace` is always relative to the provider's configured [namespace](/docs/providers/vault#namespace).
+* `recursive` - (Optional) If `true`, it will returns all child namespaces of the given namespace. Defaults to `false`, which returns only direct child namespaces.
 
 ## Attributes Reference
 
 In addition to the above arguments, the following attributes are exported:
 
-* `paths` - Set of the paths of direct child namespaces.
+* `paths` - Set of the paths of child namespaces.
+* `paths_fq` - Set of the fully qualified paths of child namespaces.
