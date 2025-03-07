8ddc8b3c6946805f8a41a956e3180ff3ff6c55e0
Kay Craig <kay.craig@hashicorp.com>
2025-03-07 11:54:23 -0500

    add base functionality

--- vault/resource_azure_auth_backend_config_test.go
+++ vault/resource_azure_auth_backend_config_test.go
@@ -5,12 +5,12 @@ package vault
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-
 	"github.com/hashicorp/terraform-provider-vault/internal/consts"
 	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 	"github.com/hashicorp/terraform-provider-vault/testutil"
@@ -92,6 +92,63 @@ func TestAccAzureAuthBackend_wif(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, consts.FieldIdentityTokenTTL, "1800"),
 				),
 			},
+		},
+	})
+}
+
+func TestAccAzureAuthBackendConfig_automatedRotation(t *testing.T) {
+	backend := acctest.RandomWithPrefix("tf-test-azure")
+
+	resourceType := "vault_azure_auth_backend_config"
+	resourceName := resourceType + ".config"
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: providerFactories,
+		PreCheck: func() {
+			testutil.TestEntPreCheck(t)
+			SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion119)
+		},
+		CheckDestroy: testCheckMountDestroyed(resourceType, consts.MountTypeAzure, consts.FieldBackend),
+		Steps: []resource.TestStep{
+			{
+				// normal period setting
+				Config: testAccAzureAuthBackendConfig_automatedRotation(backend, "10m", "", "", false),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldBackend, backend),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationPeriod, "10m"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationSchedule, ""),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationWindow, "0"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldDisableAutomatedRotation, "false"),
+				),
+			},
+			{
+				// switch to schedule
+				Config: testAccAzureAuthBackendConfig_automatedRotation(backend, "", "*/20 * * * SAT", "", false),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldBackend, backend),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationPeriod, "0"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationSchedule, "*/20 * * * SAT"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationWindow, "0"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldDisableAutomatedRotation, "false"),
+				),
+			},
+			{
+				// disable it
+				Config: testAccAzureAuthBackendConfig_automatedRotation(backend, "", "", "", true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldBackend, backend),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationPeriod, "0"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationSchedule, ""),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRotationWindow, "0"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldDisableAutomatedRotation, "true"),
+				),
+			},
+			{
+				// do an error
+				Config:      testAccAzureAuthBackendConfig_automatedRotation(backend, "10m", "", "15m", false),
+				ExpectError: regexp.MustCompile("rotation_window does not apply to period"),
+			},
+			testutil.GetImportTestStep(resourceName, false, nil, consts.FieldClientSecret),
 		},
 	})
 }
@@ -225,4 +282,24 @@ resource "vault_azure_auth_backend_config" "config" {
   identity_token_audience = "wif-audience-updated"
   identity_token_ttl 	  = 1800
 }`, backend)
+}
+
+func testAccAzureAuthBackendConfig_automatedRotation(backend, periodDuration, scheduleString, windowDuration string, disableRotation bool) string {
+	return fmt.Sprintf(`
+resource "vault_auth_backend" "azure" {
+  path = "%s"
+  type = "azure"
+}
+
+resource "vault_auth_azure_backend_config" "config" {
+  backend = vault_auth_backend.azure.path
+  tenant_id = "11111111-2222-3333-4444-555555555555"
+  client_id = "11111111-2222-3333-4444-555555555555"
+  client_secret = "12345678901234567890"
+  resource = "http://vault.hashicorp.com"
+  rotation_period = "%s"
+  rotation_schedule = "%s"
+  rotation_window = "%s"
+  disable_automated_rotation = %t
+}`, backend, periodDuration, scheduleString, windowDuration, disableRotation)
 }
