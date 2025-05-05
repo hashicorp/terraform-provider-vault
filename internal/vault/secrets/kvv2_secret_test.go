3ab2b61efb0217b8a62149bf1fac5a2d2fad9a22
Vinay Gopalan <vinay@hashicorp.com>
2025-05-05 13:57:54 -0700

    add test stub

---
+++ internal/vault/secrets/kvv2_secret_test.go
@@ -0,0 +1,58 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package secrets
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/echoprovider"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"github.com/hashicorp/terraform-provider-vault/internal/providertest"
+	"testing"
+)
+
+func TestAccKVV2Secret(t *testing.T) {
+
+	mount := "uswest-kvv2"
+	name := "pgx-root-user"
+	resource.UnitTest(t, resource.TestCase{
+		// Ephemeral resources are only available in 1.10 and later
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_10_0),
+		},
+		// Include the provider we want to test
+		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
+		// Include `echo` as a v6 provider from `terraform-plugin-testing`
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"echo": echoprovider.NewProviderServer(),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testKVV2SecretConfig(mount, name),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("echo.test_krb", tfjsonpath.New("data").AtMapKey("password"), knownvalue.StringExact("password1")),
+				},
+			},
+		},
+	})
+}
+
+func testKVV2SecretConfig(mount, name string) string {
+	return fmt.Sprintf(`
+ephemeral "vault_kvv2_secret" "db_secret" {
+	mount = "%s"
+	name = "%s"
+}
+
+provider "echo" {
+	data = ephemeral.examplecloud_secret.krb.secret_data
+}
+
+resource "echo" "test_krb" {}
+`, mount, name)
+}
