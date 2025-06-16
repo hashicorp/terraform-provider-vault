968b8a1b14d80ec311fc6c823da11ce89ab32467
Drew Mullen <drew.mullen@hashicorp.com>
2025-06-16 18:33:35 -0400

    update tests

--- internal/vault/secrets/ephemeral/terraform_team_token_test.go
+++ internal/vault/secrets/ephemeral/terraform_team_token_test.go
@@ -4,11 +4,13 @@
 package ephemeralsecrets_test
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/echoprovider"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
@@ -25,16 +27,14 @@ import (
 // https://developer.hashicorp.com/terraform/plugin/testing/acceptance-tests/ephemeral-resources#using-echo-provider-in-acceptance-tests
 func TestAccTFTeamToken(t *testing.T) {
 	testutil.SkipTestAcc(t)
-	// mount := acctest.RandomWithPrefix("postgres")
-	// tfName := acctest.RandomWithPrefix("tf")
-	// roleName := acctest.RandomWithPrefix("role")
+	tfName := acctest.RandomWithPrefix("tf")
 
-	// values := testutil.SkipTestEnvUnset(t, "POSTGRES_URL")
-	// connURL := values[0]
+	values := testutil.SkipTestEnvUnset(t, "TEST_TF_TOKEN", "TEST_TF_TEAM_ID")
+	configToken := values[0]
+	tfTeamID := values[1]
 
 	// // catch-all regex to ensure all usernames and passwords are set to some value
 	expectedTokenRegex, err := regexp.Compile("atlasv1")
-	// expectedPasswordRegex, err := regexp.Compile("^\\S+$")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -50,28 +50,46 @@ func TestAccTFTeamToken(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: testTFTeamTokenConfig(),
+				Config: testTFTeamTokenConfig(configToken, tfName, tfTeamID),
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownValue("echo.tf_token", tfjsonpath.New("data").AtMapKey("token"), knownvalue.StringRegexp(expectedTokenRegex)),
 					// statecheck.ExpectKnownValue("echo.test_tf", tfjsonpath.New("data").AtMapKey("password"), knownvalue.StringRegexp(expectedPasswordRegex)),
+					// check if revoke_on_close is set to true
+					// if true, validate the token is revoked in API
+					// else, validate the token still exists in API
+					// TODO: update test config
 				},
 			},
 		},
 	})
 }
 
-func testTFTeamTokenConfig() string {
-	return `
+func testTFTeamTokenConfig(configToken, tfName, tfTeamId string) string {
+	return fmt.Sprintf(`
+resource "vault_terraform_cloud_secret_backend" "test" {
+  token       = "%[1]s"
+  backend     = "%[2]s"
+}
+
+resource "vault_terraform_cloud_secret_role" "test_team" {
+  backend         = vault_terraform_cloud_secret_backend.test.backend
+  name            = "%[2]s_team_id"
+  team_id         = "%[3]s"
+  credential_type = "team"
+  ttl             = 120
+  description     = "%[2]s team role"
+}
+
 ephemeral "vault_terraform_team_token" "tf_token" {
-	mount     = "terraform"
-	role_name = "tfc-mgmt"
-	revoke_on_close = true
+  mount           = vault_terraform_cloud_secret_backend.test.backend
+  role_name       = vault_terraform_cloud_secret_role.test_team.name
+  revoke_on_close = terraform.applying ? true : false
 }
 
 provider "echo" {
-	data = ephemeral.vault_terraform_team_token.tf_token
+  data = ephemeral.vault_terraform_team_token.tf_token
 }
 
 resource "echo" "tf_token" {}
-`
+`, configToken, tfName, tfTeamId)
 }
