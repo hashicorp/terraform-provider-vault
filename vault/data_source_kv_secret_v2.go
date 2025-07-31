a05ad058e1eb2c3f4eaf3bf89aaa22c05471bf7d
Florian Forster <fforster@gitlab.com>
2025-07-31 09:26:20 +0200

    feat(data_source_kv_secret_v2_metadata): Add data source for reading secret metadata.
    This is primarily useful for accessing a secret's version in a non-ephemeral
    way, without also loading the secret itself. The non-ephemeral version can then
    be used as a write-only version argument, controlling upgrade behavior without
    storing the secret in the state.
    
    Issue: https://github.com/hashicorp/terraform-provider-vault/issues/2537

--- vault/data_source_kv_secret_v2.go
+++ vault/data_source_kv_secret_v2.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"maps"
 	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -17,81 +18,107 @@ import (
 	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 )
 
+var kvSecretV2DataSourceMetadataFields = map[string]*schema.Schema{
+	consts.FieldMount: {
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "Path where KV-V2 engine is mounted",
+	},
+
+	consts.FieldName: {
+		Type:     schema.TypeString,
+		Required: true,
+		Description: "Full name of the secret. For a nested secret, " +
+			"the name is the nested path excluding the mount and data " +
+			"prefix. For example, for a secret at 'kvv2/data/foo/bar/baz', " +
+			"the name is 'foo/bar/baz'",
+	},
+
+	consts.FieldVersion: {
+		Type:        schema.TypeInt,
+		Optional:    true,
+		Description: "Version of the secret to retrieve",
+	},
+
+	consts.FieldPath: {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Full path where the KVV2 secret is written.",
+	},
+
+	"created_time": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Time at which the secret was created",
+	},
+
+	"custom_metadata": {
+		Type:        schema.TypeMap,
+		Computed:    true,
+		Description: "Custom metadata for the secret",
+	},
+
+	"deletion_time": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Deletion time for the secret",
+	},
+
+	"destroyed": {
+		Type:        schema.TypeBool,
+		Computed:    true,
+		Description: "Indicates whether the secret has been destroyed",
+	},
+}
+
+var kvSecretV2DataSourceDataFields = map[string]*schema.Schema{
+	consts.FieldDataJSON: {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "JSON-encoded secret data read from Vault.",
+		Sensitive:   true,
+	},
+
+	consts.FieldData: {
+		Type:        schema.TypeMap,
+		Computed:    true,
+		Description: "Map of strings read from Vault.",
+		Sensitive:   true,
+	},
+}
+
 func kvSecretV2DataSource() *schema.Resource {
 	return &schema.Resource{
-		ReadContext:        provider.ReadContextWrapper(kvSecretV2DataSourceRead),
+		ReadContext: provider.ReadContextWrapper(func(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+			const includeData = true
+
+			return kvSecretV2DataSourceRead(ctx, d, meta, includeData)
+		}),
 		DeprecationMessage: "Deprecated. Please use new Ephemeral KVV2 Secret resource `vault_kv_secret_v2` instead",
 
-		Schema: map[string]*schema.Schema{
-			consts.FieldMount: {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Path where KV-V2 engine is mounted",
-			},
-
-			consts.FieldName: {
-				Type:     schema.TypeString,
-				Required: true,
-				Description: "Full name of the secret. For a nested secret, " +
-					"the name is the nested path excluding the mount and data " +
-					"prefix. For example, for a secret at 'kvv2/data/foo/bar/baz', " +
-					"the name is 'foo/bar/baz'",
-			},
-
-			consts.FieldVersion: {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Description: "Version of the secret to retrieve",
-			},
-
-			consts.FieldPath: {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Full path where the KVV2 secret is written.",
-			},
-
-			consts.FieldDataJSON: {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "JSON-encoded secret data read from Vault.",
-				Sensitive:   true,
-			},
-
-			consts.FieldData: {
-				Type:        schema.TypeMap,
-				Computed:    true,
-				Description: "Map of strings read from Vault.",
-				Sensitive:   true,
-			},
-
-			"created_time": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Time at which the secret was created",
-			},
-
-			"custom_metadata": {
-				Type:        schema.TypeMap,
-				Computed:    true,
-				Description: "Custom metadata for the secret",
-			},
-
-			"deletion_time": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Deletion time for the secret",
-			},
-
-			"destroyed": {
-				Type:        schema.TypeBool,
-				Computed:    true,
-				Description: "Indicates whether the secret has been destroyed",
-			},
-		},
+		Schema: mergeSchemaFields(
+			kvSecretV2DataSourceMetadataFields,
+			kvSecretV2DataSourceDataFields,
+		),
 	}
 }
 
-func kvSecretV2DataSourceRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+func mergeSchemaFields(ms ...map[string]*schema.Schema) map[string]*schema.Schema {
+	var size int
+	for _, m := range ms {
+		size += len(m)
+	}
+
+	out := make(map[string]*schema.Schema, size)
+
+	for _, m := range ms {
+		maps.Copy(out, m)
+	}
+
+	return out
+}
+
+func kvSecretV2DataSourceRead(ctx context.Context, d *schema.ResourceData, meta any, includeData bool) diag.Diagnostics {
 	client, e := provider.GetClient(d, meta)
 	if e != nil {
 		return diag.FromErr(e)
@@ -102,6 +129,8 @@ func kvSecretV2DataSourceRead(ctx context.Context, d *schema.ResourceData, meta 
 
 	// prefix for v2 secrets is "data"
 	path := getKVV2Path(mount, name, consts.FieldData)
+
+	d.SetId(path)
 
 	if err := d.Set(consts.FieldPath, path); err != nil {
 		return diag.FromErr(err)
@@ -125,23 +154,6 @@ func kvSecretV2DataSourceRead(ctx context.Context, d *schema.ResourceData, meta 
 	}
 	if secret == nil {
 		return diag.Errorf("no secret found at %q", path)
-	}
-
-	data := secret.Data["data"]
-
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return diag.Errorf("error marshaling JSON for %q: %s", path, err)
-	}
-
-	if err := d.Set(consts.FieldDataJSON, string(jsonData)); err != nil {
-		return diag.FromErr(err)
-	}
-
-	if v, ok := data.(map[string]any); ok {
-		if err := d.Set(consts.FieldData, serializeDataMapToString(v)); err != nil {
-			return diag.FromErr(err)
-		}
 	}
 
 	if v, ok := secret.Data["metadata"]; ok {
@@ -180,7 +192,26 @@ func kvSecretV2DataSourceRead(ctx context.Context, d *schema.ResourceData, meta 
 		}
 	}
 
-	d.SetId(path)
+	if !includeData {
+		return nil
+	}
+
+	data := secret.Data["data"]
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return diag.Errorf("error marshaling JSON for %q: %s", path, err)
+	}
+
+	if err := d.Set(consts.FieldDataJSON, string(jsonData)); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if v, ok := data.(map[string]any); ok {
+		if err := d.Set(consts.FieldData, serializeDataMapToString(v)); err != nil {
+			return diag.FromErr(err)
+		}
+	}
 
 	return nil
 }
