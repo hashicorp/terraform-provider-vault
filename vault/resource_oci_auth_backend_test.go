095b66c10bac6715edae1ee98460b9c6f59004ba
Thy Ton <maithytonn@gmail.com>
2025-09-04 19:09:00 -0700

    merge tune block fields using user provided values and tune API response - GCP and OCI

--- vault/resource_oci_auth_backend_test.go
+++ vault/resource_oci_auth_backend_test.go
@@ -11,6 +11,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/vault/api"
+
+	"github.com/hashicorp/terraform-provider-vault/internal/consts"
 	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 	"github.com/hashicorp/terraform-provider-vault/testutil"
 )
@@ -54,6 +57,83 @@ func TestAccOCIAuthBackendConfig_basic(t *testing.T) {
 				Config: testAccOCIAuthBackendConfig_updated(path),
 				Check:  testAccOCIAuthBackendConfigCheck_attrs(path),
 			},
+		},
+	})
+}
+
+func TestAccOCIAuthBackendConfig_tuning(t *testing.T) {
+	testutil.SkipTestAcc(t)
+
+	path := acctest.RandomWithPrefix("oci-tune")
+	resourceName := "vault_oci_auth_backend.oci"
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories(context.Background(), t),
+		CheckDestroy:             testAccCheckOCIAuthBackendConfigDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOCIAuthBackendConfig_tune_partial(path),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "tune.0.default_lease_ttl", ""),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.max_lease_ttl", ""),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.listing_visibility", ""),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.token_type", ""),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_request_keys.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_request_keys.0", "key1"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_response_keys.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_response_keys.0", "key3"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.passthrough_request_headers.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.passthrough_request_headers.0", "X-Custom-Header"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.passthrough_request_headers.1", "X-Forwarded-To"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.allowed_response_headers.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.allowed_response_headers.0", "X-Custom-Response-Header"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.allowed_response_headers.1", "X-Forwarded-Response-To"),
+				),
+			},
+			{
+				Config: testAccOCIAuthBackendConfig_tune_full(path),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "tune.0.default_lease_ttl", "10m"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.max_lease_ttl", "20m"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.listing_visibility", "hidden"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.token_type", "batch"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_request_keys.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_request_keys.0", "key1"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_request_keys.1", "key2"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_response_keys.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_response_keys.0", "key3"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.audit_non_hmac_response_keys.1", "key4"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.passthrough_request_headers.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.passthrough_request_headers.0", "X-Custom-Header"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.passthrough_request_headers.1", "X-Forwarded-To"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.allowed_response_headers.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.allowed_response_headers.0", "X-Custom-Response-Header"),
+					resource.TestCheckResourceAttr(resourceName, "tune.0.allowed_response_headers.1", "X-Forwarded-Response-To"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccOCIAuthBackendConfig_importTune(t *testing.T) {
+	testutil.SkipTestAcc(t)
+
+	path := acctest.RandomWithPrefix("oci-import-tune")
+	resourceName := "vault_oci_auth_backend.oci"
+	var resAuth api.AuthMount
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories(context.Background(), t),
+		CheckDestroy:             testAccCheckOCIAuthBackendConfigDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOCIAuthBackendConfig_tune_full(path),
+				Check: testutil.TestAccCheckAuthMountExists(resourceName,
+					&resAuth,
+					testProvider.Meta().(*provider.ProviderMeta).MustGetClient()),
+			},
+			testutil.GetImportTestStep(resourceName, false, nil, consts.FieldDisableRemount),
 		},
 	})
 }
@@ -133,4 +213,38 @@ resource "vault_oci_auth_backend" "config" {
   path = "%s"
   home_tenancy_id = "ocid1.tenancy.oc1..aaaaaaaah7zkvaffv26pzyauoe2zbnionqvhvsexamplee557wakiofi4ysgqq"
 }`, backend)
+}
+
+func testAccOCIAuthBackendConfig_tune_partial(path string) string {
+	return fmt.Sprintf(`
+resource "vault_oci_auth_backend" "oci" {
+  path = "%s"
+  home_tenancy_id = "ocid1.tenancy.oc1..aaaaaaaah7zkvaffv26pzyauoe2zbnionqvhvsexamplee557wakiofi4ysgqq"
+  tune {
+    audit_non_hmac_request_keys = ["key1"]
+	audit_non_hmac_response_keys = ["key3"]
+	passthrough_request_headers = ["X-Custom-Header", "X-Forwarded-To"]
+	allowed_response_headers = ["X-Custom-Response-Header", "X-Forwarded-Response-To"]
+  }
+}
+`, path)
+}
+
+func testAccOCIAuthBackendConfig_tune_full(path string) string {
+	return fmt.Sprintf(`
+resource "vault_oci_auth_backend" "oci" {
+  path = "%s"
+  home_tenancy_id = "ocid1.tenancy.oc1..aaaaaaaah7zkvaffv26pzyauoe2zbnionqvhvsexamplee557wakiofi4ysgqq"
+  tune {
+	default_lease_ttl = "10m"
+	max_lease_ttl = "20m"
+	listing_visibility = "hidden"
+	token_type = "batch"
+	audit_non_hmac_request_keys = ["key1", "key2"]
+	audit_non_hmac_response_keys = ["key3", "key4"]
+	passthrough_request_headers = ["X-Custom-Header", "X-Forwarded-To"]
+	allowed_response_headers = ["X-Custom-Response-Header", "X-Forwarded-Response-To"]
+  }
+}
+`, path)
 }
