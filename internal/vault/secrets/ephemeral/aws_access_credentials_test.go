1b60ee7c5e0839cf9212867a1232f99bc7f009d3
harshit.nema@hashicorp.com <harshit.nema@hashicorp.com>
2025-11-18 21:06:58 +0530

    Code and test added for aws_access_credentials ephemeral resource

--- internal/vault/secrets/ephemeral/aws_access_credentials_test.go
+++ internal/vault/secrets/ephemeral/aws_access_credentials_test.go
@@ -14,30 +14,30 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-provider-vault/acctestutil"
+	"github.com/hashicorp/terraform-provider-vault/internal/provider"
 	"github.com/hashicorp/terraform-provider-vault/internal/providertest"
 	"github.com/hashicorp/terraform-provider-vault/testutil"
 )
 
-// TestAccAWSAccessCredentials confirms that AWS credentials
+// TestAccAWSAccessCredentialsCredIamUser confirms that AWS credentials
 // are correctly generated from Vault into the ephemeral resource
-//
-// NOTE: This test currently fails due to timing constraints where ephemeral resources
-// are evaluated during the plan phase before AWS backends/roles are fully created.
-// The implementation is correct and will work with pre-existing AWS infrastructure.
-//
+// Creates the AWS backend and role,
+// then uses the ephemeral resource to generate credentials.
 // Uses the Echo Provider to test values set in ephemeral resources
 // see documentation here for more details:
 // https://developer.hashicorp.com/terraform/plugin/testing/acceptance-tests/ephemeral-resources#using-echo-provider-in-acceptance-tests
-func TestAccAWSAccessCredentials(t *testing.T) {
-	testutil.SkipTestAcc(t)
-
+// Note: This test skips on Vault 1.19 with "number of regions does not match number of endpoints" error.
+// This appears to be a Vault bug where the AWS secrets engine incorrectly validates
+func TestAccAWSAccessCredentialsCredIamUser(t *testing.T) {
 	a, s := testutil.GetTestAWSCreds(t)
 	region := testutil.GetTestAWSRegion(t)
 	mount := acctest.RandomWithPrefix("tf-aws")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
-			testutil.TestAccPreCheck(t)
+			acctestutil.TestAccPreCheck(t)
+			acctestutil.SkipIfAPIVersionEQ(t, provider.VaultVersion119)
 		},
 		// Include the provider we want to test
 		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
@@ -47,7 +47,7 @@ func TestAccAWSAccessCredentials(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAWSAccessCredentialsConfig(mount, a, s, region),
+				Config: testAWSAccessCredentialsConfigIamUser(mount, a, s, region),
 				ConfigStateChecks: []statecheck.StateCheck{
 					// Verify that we got the access_key and secret_key set
 					statecheck.ExpectKnownValue("echo.test", tfjsonpath.New("access_key"), knownvalue.NotNull()),
@@ -62,20 +62,19 @@ func TestAccAWSAccessCredentials(t *testing.T) {
 }
 
 // TestAccAWSAccessCredentialsSTS tests STS token generation
-//
-// NOTE: This test currently fails due to timing constraints where ephemeral resources
-// are evaluated during the plan phase before AWS backends/roles are fully created.
-// The implementation is correct and will work with pre-existing AWS infrastructure.
-func TestAccAWSAccessCredentialsSTS(t *testing.T) {
-	testutil.SkipTestAcc(t)
-
+// creates the AWS backend and role,then uses the ephemeral resource to generate STS credentials.
+// Note: This test skips on Vault 1.19 with "number of regions does not match number of endpoints"
+// error. This is a known Vault bug affecting the AWS secrets engine's STS endpoint.
+func TestAccAWSAccessCredentialsSTSFederationToken(t *testing.T) {
 	a, s := testutil.GetTestAWSCreds(t)
 	region := testutil.GetTestAWSRegion(t)
+	ttl := "15m"
 	mount := acctest.RandomWithPrefix("tf-aws-sts")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
-			testutil.TestAccPreCheck(t)
+			acctestutil.TestAccPreCheck(t)
+			acctestutil.SkipIfAPIVersionEQ(t, provider.VaultVersion119)
 		},
 		// Include the provider we want to test
 		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
@@ -85,7 +84,7 @@ func TestAccAWSAccessCredentialsSTS(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAWSAccessCredentialsSTSConfig(mount, a, s, region),
+				Config: testAWSAccessCredentialsSTSConfigFederationToken(mount, a, s, region, ttl),
 				ConfigStateChecks: []statecheck.StateCheck{
 					// Verify that we got the access_key, secret_key, and security_token set
 					statecheck.ExpectKnownValue("echo.test", tfjsonpath.New("access_key"), knownvalue.NotNull()),
@@ -98,7 +97,38 @@ func TestAccAWSAccessCredentialsSTS(t *testing.T) {
 	})
 }
 
-func testAWSAccessCredentialsConfig(mount, access, secret, region string) string {
+func TestAccAWSAccessCredentialsSTSAssumeRole(t *testing.T) {
+	a, s := testutil.GetTestAWSCreds(t)
+	region := testutil.GetTestAWSRegion(t)
+	mount := acctest.RandomWithPrefix("tf-aws-sts")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctestutil.TestAccPreCheck(t)
+			acctestutil.SkipIfAPIVersionEQ(t, provider.VaultVersion119)
+		},
+		// Include the provider we want to test
+		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
+		// Include `echo` as a v6 provider from `terraform-plugin-testing`
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"echo": echoprovider.NewProviderServer(),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAWSAccessCredentialsSTSAssumeRoleConfig(mount, a, s, region),
+				ConfigStateChecks: []statecheck.StateCheck{
+					// Verify that we got the access_key, secret_key, and security_token set
+					statecheck.ExpectKnownValue("echo.test", tfjsonpath.New("access_key"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("echo.test", tfjsonpath.New("secret_key"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("echo.test", tfjsonpath.New("security_token"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("echo.test", tfjsonpath.New("type"), knownvalue.StringExact("sts")),
+				},
+			},
+		},
+	})
+}
+
+func testAWSAccessCredentialsConfigIamUser(mount, access, secret, region string) string {
 	return fmt.Sprintf(`
 resource "vault_aws_secret_backend" "aws" {
   path = "%s"
@@ -123,10 +153,10 @@ resource "vault_aws_secret_backend_role" "role" {
 }
 
 ephemeral "vault_aws_access_credentials" "creds" {
-  backend = vault_aws_secret_backend.aws.path
-  role    = vault_aws_secret_backend_role.role.name
-  type    = "creds"
-  region  = vault_aws_secret_backend.aws.region
+  backend  = vault_aws_secret_backend.aws.path
+  role     = vault_aws_secret_backend_role.role.name
+  type     = "creds"
+  mount_id = vault_aws_secret_backend_role.role.id
 }
 
 provider "echo" {
@@ -142,7 +172,7 @@ resource "echo" "test" {}
 `, mount, access, secret, region)
 }
 
-func testAWSAccessCredentialsSTSConfig(mount, access, secret, region string) string {
+func testAWSAccessCredentialsSTSConfigFederationToken(mount, access, secret, region, ttl string) string {
 	return fmt.Sprintf(`
 resource "vault_aws_secret_backend" "aws" {
   path = "%s"
@@ -156,6 +186,7 @@ resource "vault_aws_secret_backend_role" "role" {
   backend = vault_aws_secret_backend.aws.path
   name = "test"
   credential_type = "federation_token"
+  default_sts_ttl = 900
   policy_document = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -167,10 +198,12 @@ resource "vault_aws_secret_backend_role" "role" {
 }
 
 ephemeral "vault_aws_access_credentials" "creds" {
-  backend = vault_aws_secret_backend.aws.path
-  role    = vault_aws_secret_backend_role.role.name
-  type    = "sts"
-  region  = vault_aws_secret_backend.aws.region
+  backend  = vault_aws_secret_backend.aws.path
+  role     = vault_aws_secret_backend_role.role.name
+  type     = "sts"
+  region   = vault_aws_secret_backend.aws.region
+  ttl      = "%s"
+  mount_id = vault_aws_secret_backend_role.role.id
 }
 
 provider "echo" {
@@ -183,5 +216,43 @@ provider "echo" {
 }
 
 resource "echo" "test" {}
-`, mount, access, secret, region)
+`, mount, access, secret, region, ttl)
+}
+
+func testAWSAccessCredentialsSTSAssumeRoleConfig(mount, access, secret, region string) string {
+	return fmt.Sprintf(`
+resource "vault_aws_secret_backend" "aws" {
+  path = "%s"
+  description = "Obtain AWS credentials."
+  access_key = "%s"
+  secret_key = "%s"
+  region = "%s"
+}
+
+resource "vault_aws_secret_backend_role" "role" {
+  backend = vault_aws_secret_backend.aws.path
+  name = "test"
+  credential_type = "assumed_role"
+  role_arns       = ["arn:aws:iam::774077852947:role/MyExistingRole"]
+}
+
+ephemeral "vault_aws_access_credentials" "creds" {
+  backend  = vault_aws_secret_backend.aws.path
+  role     = vault_aws_secret_backend_role.role.name
+  type     = "sts"
+  region   = "%s"
+  mount_id = vault_aws_secret_backend_role.role.id
+}
+
+provider "echo" {
+  data = {
+    access_key = ephemeral.vault_aws_access_credentials.creds.access_key
+    secret_key = ephemeral.vault_aws_access_credentials.creds.secret_key
+    security_token = ephemeral.vault_aws_access_credentials.creds.security_token
+    type = ephemeral.vault_aws_access_credentials.creds.type
+  }
+}
+
+resource "echo" "test" {}
+`, mount, access, secret, region, region)
 }
