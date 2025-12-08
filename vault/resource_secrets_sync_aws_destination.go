0e72f6ee14dcb94c9579fa32b2838fa90c291c74
Balaji <balaji.b@hashicorp.com>
2025-12-08 22:23:49 +0530

    secretssync/AWS: Add networking configuration feature for AWS Secrets Sync Destination.

--- vault/resource_secrets_sync_aws_destination.go
+++ vault/resource_secrets_sync_aws_destination.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/hashicorp/terraform-provider-vault/internal/consts"
 	"github.com/hashicorp/terraform-provider-vault/internal/provider"
-	"github.com/hashicorp/terraform-provider-vault/internal/sync"
+	syncutil "github.com/hashicorp/terraform-provider-vault/internal/sync"
 )
 
 const (
@@ -21,7 +21,7 @@ const (
 	awsSyncType = "aws-sm"
 )
 
-// awsSyncWriteFields contains all fields that need to be written to the API
+// awsSyncWriteFields contains base fields that work with all Vault versions (1.16+)
 var awsSyncWriteFields = []string{
 	fieldAccessKeyID,
 	fieldSecretAccessKey,
@@ -33,7 +33,15 @@ var awsSyncWriteFields = []string{
 	consts.FieldExternalID,
 }
 
-// awsSyncReadFields contains all fields that are returned on read from the API
+// awsSync119Fields contains fields that require Vault 1.19+
+var awsSync119Fields = []string{
+	consts.FieldAllowedIPv4Addresses,
+	consts.FieldAllowedIPv6Addresses,
+	consts.FieldAllowedPorts,
+	consts.FieldDisableStrictNetworking,
+}
+
+// awsSyncReadFields contains base fields that are returned on read from the API (all versions)
 var awsSyncReadFields = []string{
 	consts.FieldRegion,
 	consts.FieldCustomTags,
@@ -88,17 +96,110 @@ func awsSecretsSyncDestinationResource() *schema.Resource {
 				Optional:    true,
 				Description: "Extra protection that must match the trust policy granting access to the AWS IAM role ARN.",
 			},
+			consts.FieldAllowedIPv4Addresses: {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "Allowed IPv4 addresses for outbound connections from Vault to AWS Secrets Manager. Can also be set via an IP address range using CIDR notation.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			consts.FieldAllowedIPv6Addresses: {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "Allowed IPv6 addresses for outbound connections from Vault to AWS Secrets Manager. Can also be set via an IP address range using CIDR notation.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			consts.FieldAllowedPorts: {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "Allowed ports for outbound connections from Vault to AWS Secrets Manager.",
+				Elem: &schema.Schema{
+					Type: schema.TypeInt,
+				},
+			},
+			consts.FieldDisableStrictNetworking: {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Disable strict networking mode. When set to true, Vault will not enforce allowed IP addresses and ports.",
+			},
 		},
 	})
 }
 
 func awsSecretsSyncDestinationCreateUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	return syncutil.SyncDestinationCreateUpdate(ctx, d, meta, awsSyncType, awsSyncWriteFields, awsSyncReadFields)
+	client, e := provider.GetClient(d, meta)
+	if e != nil {
+		return diag.FromErr(e)
+	}
+
+	name := d.Get(consts.FieldName).(string)
+	path := syncutil.SecretsSyncDestinationPath(name, awsSyncType)
+
+	data := map[string]interface{}{}
+
+	// Check if Vault 1.19+ fields are being used
+	isVaultVersion119 := provider.IsAPISupported(meta, provider.VaultVersion119)
+
+	// Validate version support if user is trying to set 1.19+ fields on older Vault
+	if !isVaultVersion119 {
+		for _, field := range awsSync119Fields {
+			if _, ok := d.GetOk(field); ok {
+				return diag.Errorf("networking configuration fields (allowed_ipv4_addresses, allowed_ipv6_addresses, allowed_ports, disable_strict_networking) require Vault version 1.19.0 or later")
+			}
+		}
+	}
+
+	// Process base fields (all versions)
+	for _, k := range awsSyncWriteFields {
+		if v, ok := d.GetOk(k); ok {
+			data[k] = v
+		}
+	}
+
+	// Process Vault 1.19+ fields only if version is supported
+	if isVaultVersion119 {
+		for _, k := range awsSync119Fields {
+			if v, ok := d.GetOk(k); ok {
+				// Handle TypeSet fields by converting to list
+				switch k {
+				case consts.FieldAllowedIPv4Addresses, consts.FieldAllowedIPv6Addresses, consts.FieldAllowedPorts:
+					if set, ok := v.(*schema.Set); ok {
+						data[k] = set.List()
+					}
+				default:
+					data[k] = v
+				}
+			}
+		}
+	}
+
+	_, err := client.Logical().WriteWithContext(ctx, path, data)
+	if err != nil {
+		return diag.Errorf("error writing sync destination data to %q: %s", path, err)
+	}
+
+	if d.IsNewResource() {
+		d.SetId(name)
+	}
+
+	return awsSecretsSyncDestinationRead(ctx, d, meta)
 }
 
 func awsSecretsSyncDestinationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// Start with base fields (all versions)
+	readFields := awsSyncReadFields
+
+	// Add Vault 1.19+ fields only if version is supported
+	if provider.IsAPISupported(meta, provider.VaultVersion119) {
+		readFields = append(readFields, awsSync119Fields...)
+	}
+
 	// since other fields come back as '******', we only set the non-sensitive region fields
-	return syncutil.SyncDestinationRead(ctx, d, meta, awsSyncType, awsSyncReadFields, map[string]string{
+	return syncutil.SyncDestinationRead(ctx, d, meta, awsSyncType, readFields, map[string]string{
 		consts.FieldGranularity: consts.FieldGranularityLevel,
 	})
 }
--- vault/resource_secrets_sync_aws_destination_test.go
+++ vault/resource_secrets_sync_aws_destination_test.go
@@ -73,9 +73,94 @@ func TestAWSSecretsSyncDestination(t *testing.T) {
 			testutil.GetImportTestStep(resourceName, false, nil,
 				fieldAccessKeyID,
 				fieldSecretAccessKey,
+				consts.FieldDisableStrictNetworking, // Vault API doesn't return false when not set
 			),
 		},
 	})
+}
+
+func TestAWSSecretsSyncDestination_Networking(t *testing.T) {
+	destName := acctest.RandomWithPrefix("tf-sync-dest-aws-net")
+
+	resourceName := "vault_secrets_sync_aws_destination.test"
+
+	accessKey, secretKey := testutil.GetTestAWSCreds(t)
+	region := testutil.GetTestAWSRegion(t)
+	resource.Test(t, resource.TestCase{
+		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories(context.Background(), t),
+		PreCheck: func() {
+			testutil.TestAccPreCheck(t)
+			SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion119)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAWSSecretsSyncDestinationConfig_networking(accessKey, secretKey, region, destName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldName, destName),
+					resource.TestCheckResourceAttr(resourceName, fieldAccessKeyID, accessKey),
+					resource.TestCheckResourceAttr(resourceName, fieldSecretAccessKey, secretKey),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldRegion, region),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldType, awsSyncType),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldAllowedIPv4Addresses+".#", "2"),
+					resource.TestCheckTypeSetElemAttr(resourceName, consts.FieldAllowedIPv4Addresses+".*", "192.168.1.0/24"),
+					resource.TestCheckTypeSetElemAttr(resourceName, consts.FieldAllowedIPv4Addresses+".*", "10.0.0.0/8"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldAllowedIPv6Addresses+".#", "1"),
+					resource.TestCheckTypeSetElemAttr(resourceName, consts.FieldAllowedIPv6Addresses+".*", "2001:db8::/32"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldAllowedPorts+".#", "2"),
+					resource.TestCheckTypeSetElemAttr(resourceName, consts.FieldAllowedPorts+".*", "443"),
+					resource.TestCheckTypeSetElemAttr(resourceName, consts.FieldAllowedPorts+".*", "8200"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldDisableStrictNetworking, "false"),
+				),
+			},
+			{
+				Config: testAWSSecretsSyncDestinationConfig_networkingUpdated(accessKey, secretKey, region, destName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldName, destName),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldAllowedIPv4Addresses+".#", "1"),
+					resource.TestCheckTypeSetElemAttr(resourceName, consts.FieldAllowedIPv4Addresses+".*", "172.16.0.0/12"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldAllowedPorts+".#", "1"),
+					resource.TestCheckTypeSetElemAttr(resourceName, consts.FieldAllowedPorts+".*", "443"),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldDisableStrictNetworking, "true"),
+				),
+			},
+			testutil.GetImportTestStep(resourceName, false, nil,
+				fieldAccessKeyID,
+				fieldSecretAccessKey,
+				consts.FieldDisableStrictNetworking, // Vault API doesn't return the value during import
+			),
+		},
+	})
+}
+
+func testAWSSecretsSyncDestinationConfig_networking(accessKey, secretKey, region, destName string) string {
+	return fmt.Sprintf(`
+resource "vault_secrets_sync_aws_destination" "test" {
+  name                        = "%s"
+  access_key_id               = "%s"
+  secret_access_key           = "%s"
+  region                      = "%s"
+  granularity                 = "secret-path"
+  allowed_ipv4_addresses      = ["192.168.1.0/24", "10.0.0.0/8"]
+  allowed_ipv6_addresses      = ["2001:db8::/32"]
+  allowed_ports               = [443, 8200]
+  disable_strict_networking   = false
+}
+`, destName, accessKey, secretKey, region)
+}
+
+func testAWSSecretsSyncDestinationConfig_networkingUpdated(accessKey, secretKey, region, destName string) string {
+	return fmt.Sprintf(`
+resource "vault_secrets_sync_aws_destination" "test" {
+  name                        = "%s"
+  access_key_id               = "%s"
+  secret_access_key           = "%s"
+  region                      = "%s"
+  granularity                 = "secret-path"
+  allowed_ipv4_addresses      = ["172.16.0.0/12"]
+  allowed_ports               = [443]
+  disable_strict_networking   = true
+}
+`, destName, accessKey, secretKey, region)
 }
 
 func testAWSSecretsSyncDestinationConfig_initial(accessKey, secretKey, region, destName, templ string) string {
