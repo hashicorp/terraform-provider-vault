c1d6e2b33056daaf30c43201615f6eeda498a988
Vijayavel Sekar <vijayavel.sekar@hashicorp.com>
2026-01-07 15:01:23 +0530

    VAULT-41658 Added write only fields for nomad secret backend

--- vault/resource_nomad_secret_backend.go
+++ vault/resource_nomad_secret_backend.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -19,7 +20,7 @@ import (
 
 func nomadSecretAccessBackendResource() *schema.Resource {
 	fields := map[string]*schema.Schema{
-		"backend": {
+		consts.FieldBackend: {
 			Type:        schema.TypeString,
 			Default:     "nomad",
 			Optional:    true,
@@ -28,70 +29,107 @@ func nomadSecretAccessBackendResource() *schema.Resource {
 				return strings.Trim(v.(string), "/")
 			},
 		},
-		"address": {
-			Type:        schema.TypeString,
-			Optional:    true,
-			Description: `Specifies the address of the Nomad instance, provided as "protocol://host:port" like "http://127.0.0.1:4646".`,
+		consts.FieldAddress: {
+			Type:     schema.TypeString,
+			Optional: true,
+			Description: "Specifies the address of the Nomad instance, provided as " +
+				`"protocol://host:port" like "http://127.0.0.1:4646".`,
 		},
-		"ca_cert": {
+		consts.FieldCACert: {
 			Type:        schema.TypeString,
 			Optional:    true,
 			Description: `CA certificate to use when verifying Nomad server certificate, must be x509 PEM encoded.`,
 		},
-		"client_cert": {
-			Type:        schema.TypeString,
-			Optional:    true,
-			Sensitive:   true,
-			Description: `Client certificate used for Nomad's TLS communication, must be x509 PEM encoded and if this is set you need to also set client_key.`,
+		consts.FieldClientCert: {
+			Type:      schema.TypeString,
+			Optional:  true,
+			Sensitive: true,
+			Description: "Client certificate used for Nomad's TLS communication, " +
+				"must be x509 PEM encoded and if this is set you need to also set client_key.",
 		},
-		"client_key": {
-			Type:        schema.TypeString,
-			Optional:    true,
-			Sensitive:   true,
-			Description: `Client key used for Nomad's TLS communication, must be x509 PEM encoded and if this is set you need to also set client_cert.`,
+		consts.FieldClientKey: {
+			Type:      schema.TypeString,
+			Optional:  true,
+			Sensitive: true,
+			Description: "Client key used for Nomad's TLS communication, " +
+				"must be x509 PEM encoded and if this is set you need to also set client_cert.",
+			ConflictsWith: []string{consts.FieldClientKeyWO},
 		},
-		"default_lease_ttl_seconds": {
+		consts.FieldClientKeyWO: {
+			Type:      schema.TypeString,
+			Optional:  true,
+			Sensitive: true,
+			WriteOnly: true,
+			Description: "Write-only client key used for Nomad's TLS communication, " +
+				"must be x509 PEM encoded and if this is set you need to also set client_cert.",
+			ConflictsWith: []string{consts.FieldClientKey},
+		},
+		consts.FieldClientKeyWOVersion: {
+			Type:         schema.TypeInt,
+			Optional:     true,
+			Description:  `Version counter for write-only client_key.`,
+			RequiredWith: []string{consts.FieldClientKeyWO},
+		},
+		consts.FieldDefaultLeaseTTLSeconds: {
 			Type:        schema.TypeInt,
 			Optional:    true,
 			Computed:    true,
 			Description: `Default lease duration for secrets in seconds.`,
 		},
-		"description": {
+		consts.FieldDescription: {
 			Type:        schema.TypeString,
 			Optional:    true,
 			Description: `Human-friendly description of the mount for the backend.`,
 		},
-		"local": {
-			Type:        schema.TypeBool,
-			Required:    false,
-			Optional:    true,
-			Description: `Mark the secrets engine as local-only. Local engines are not replicated or removed by replication. Tolerance duration to use when checking the last rotation time.`,
+		consts.FieldLocal: {
+			Type:     schema.TypeBool,
+			Required: false,
+			Optional: true,
+			Description: "Mark the secrets engine as local-only. Local engines are not replicated or " +
+				"removed by replication. Tolerance duration to use when checking the last rotation time.",
 		},
-		"max_lease_ttl_seconds": {
+		consts.FieldMaxLeaseTTLSeconds: {
 			Type:        schema.TypeInt,
 			Optional:    true,
 			Computed:    true,
 			Description: "Maximum possible lease duration for secrets in seconds.",
 		},
-		"max_token_name_length": {
-			Type:        schema.TypeInt,
-			Optional:    true,
-			Computed:    true,
-			Description: `Specifies the maximum length to use for the name of the Nomad token generated with Generate Credential. If omitted, 0 is used and ignored, defaulting to the max value allowed by the Nomad version.`,
+		consts.FieldMaxTokenNameLength: {
+			Type:     schema.TypeInt,
+			Optional: true,
+			Computed: true,
+			Description: "Specifies the maximum length to use for the name of the Nomad token generated " +
+				"with Generate Credential. If omitted, 0 is used and ignored, defaulting to the max value " +
+				"allowed by the Nomad version.",
 		},
-		"max_ttl": {
+		consts.FieldMaxTTL: {
 			Type:        schema.TypeInt,
 			Optional:    true,
 			Computed:    true,
 			Description: "Maximum possible lease duration for secrets in seconds.",
 		},
-		"token": {
-			Type:        schema.TypeString,
-			Optional:    true,
-			Sensitive:   true,
-			Description: `Specifies the Nomad Management token to use.`,
+		consts.FieldToken: {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Sensitive:     true,
+			Description:   `Specifies the Nomad Management token to use.`,
+			ConflictsWith: []string{consts.FieldTokenWO},
 		},
-		"ttl": {
+		consts.FieldTokenWO: {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Sensitive:     true,
+			WriteOnly:     true,
+			Description:   `Write-only Nomad Management token to use.`,
+			ConflictsWith: []string{consts.FieldToken},
+		},
+		consts.FieldTokenWOVersion: {
+			Type:         schema.TypeInt,
+			Optional:     true,
+			Description:  `Version counter for write-only token.`,
+			RequiredWith: []string{consts.FieldTokenWO},
+		},
+		consts.FieldTTL: {
 			Type:        schema.TypeInt,
 			Optional:    true,
 			Computed:    true,
@@ -129,7 +167,7 @@ func createNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData
 		return diag.FromErr(e)
 	}
 
-	backend := d.Get("backend").(string)
+	backend := d.Get(consts.FieldBackend).(string)
 
 	log.Printf("[DEBUG] Mounting Nomad backend at %q", backend)
 	if err := createMount(ctx, d, meta, client, backend, consts.MountTypeNomad); err != nil {
@@ -140,28 +178,32 @@ func createNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData
 	d.SetId(backend)
 
 	data := map[string]interface{}{}
-	if v, ok := d.GetOkExists("address"); ok {
-		data["address"] = v
+	if v, ok := d.GetOk(consts.FieldAddress); ok {
+		data[consts.FieldAddress] = v
 	}
 
-	if v, ok := d.GetOkExists("ca_cert"); ok {
-		data["ca_cert"] = v
+	if v, ok := d.GetOk(consts.FieldCACert); ok {
+		data[consts.FieldCACert] = v
 	}
 
-	if v, ok := d.GetOkExists("client_cert"); ok {
-		data["client_cert"] = v
+	if v, ok := d.GetOk(consts.FieldClientCert); ok {
+		data[consts.FieldClientCert] = v
 	}
 
-	if v, ok := d.GetOkExists("client_key"); ok {
-		data["client_key"] = v
+	if v, ok := d.GetOk(consts.FieldClientKey); ok {
+		data[consts.FieldClientKey] = v.(string)
+	} else if clientKeyWo, _ := d.GetRawConfigAt(cty.GetAttrPath(consts.FieldClientKeyWO)); !clientKeyWo.IsNull() {
+		data[consts.FieldClientKey] = clientKeyWo.AsString()
 	}
 
-	if v, ok := d.GetOkExists("max_token_name_length"); ok {
-		data["max_token_name_length"] = v
+	if v, ok := d.GetOk(consts.FieldMaxTokenNameLength); ok {
+		data[consts.FieldMaxTokenNameLength] = v
 	}
 
-	if v, ok := d.GetOkExists("token"); ok {
-		data["token"] = v
+	if v, ok := d.GetOk(consts.FieldToken); ok {
+		data[consts.FieldToken] = v.(string)
+	} else if tokenWo, _ := d.GetRawConfigAt(cty.GetAttrPath(consts.FieldTokenWO)); !tokenWo.IsNull() {
+		data[consts.FieldToken] = tokenWo.AsString()
 	}
 
 	configPath := fmt.Sprintf("%s/config/access", backend)
@@ -171,12 +213,12 @@ func createNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData
 	}
 
 	dataLease := map[string]interface{}{}
-	if v, ok := d.GetOkExists("max_ttl"); ok {
-		dataLease["max_ttl"] = v
+	if v, ok := d.GetOk(consts.FieldMaxTTL); ok {
+		dataLease[consts.FieldMaxTTL] = v
 	}
 
-	if v, ok := d.GetOkExists("ttl"); ok {
-		dataLease["ttl"] = v
+	if v, ok := d.GetOk(consts.FieldTTL); ok {
+		dataLease[consts.FieldTTL] = v
 	}
 
 	configLeasePath := fmt.Sprintf("%s/config/lease", backend)
@@ -197,7 +239,7 @@ func readNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData, 
 
 	backend := d.Id()
 
-	if err := d.Set("backend", backend); err != nil {
+	if err := d.Set(consts.FieldBackend, backend); err != nil {
 		return diag.FromErr(err)
 	}
 	if err := readMount(ctx, d, meta, true, true); err != nil {
@@ -218,33 +260,36 @@ func readNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData, 
 		return nil
 	}
 
-	if val, ok := resp.Data["address"]; ok {
-		if err := d.Set("address", val); err != nil {
-			return diag.Errorf("error setting state key 'address': %s", err)
+	if val, ok := resp.Data[consts.FieldAddress]; ok {
+		if err := d.Set(consts.FieldAddress, val); err != nil {
+			return diag.Errorf("error setting state key '%s': %s", consts.FieldAddress, err)
 		}
 	}
 
-	if val, ok := resp.Data["ca_cert"]; ok {
-		if err := d.Set("ca_cert", val); err != nil {
-			return diag.Errorf("error setting state key 'ca_cert': %s", err)
+	if val, ok := resp.Data[consts.FieldCACert]; ok {
+		if err := d.Set(consts.FieldCACert, val); err != nil {
+			return diag.Errorf("error setting state key '%s': %s", consts.FieldCACert, err)
 		}
 	}
 
-	if val, ok := resp.Data["client_cert"]; ok {
-		if err := d.Set("client_cert", val); err != nil {
-			return diag.Errorf("error setting state key 'client_cert': %s", err)
+	if val, ok := resp.Data[consts.FieldClientCert]; ok {
+		if err := d.Set(consts.FieldClientCert, val); err != nil {
+			return diag.Errorf("error setting state key '%s': %s", consts.FieldClientCert, err)
 		}
 	}
 
-	if val, ok := resp.Data["client_key"]; ok {
-		if err := d.Set("client_key", val); err != nil {
-			return diag.Errorf("error setting state key 'client_key': %s", err)
+	// Don't read back client_key if using write-only version
+	if _, ok := d.GetOk(consts.FieldClientKeyWO); !ok {
+		if val, ok := resp.Data[consts.FieldClientKey]; ok {
+			if err := d.Set(consts.FieldClientKey, val); err != nil {
+				return diag.Errorf("error setting state key '%s': %s", consts.FieldClientKey, err)
+			}
 		}
 	}
 
-	if val, ok := resp.Data["max_token_name_length"]; ok {
-		if err := d.Set("max_token_name_length", val); err != nil {
-			return diag.Errorf("error setting state key 'max_token_name_length': %s", err)
+	if val, ok := resp.Data[consts.FieldMaxTokenNameLength]; ok {
+		if err := d.Set(consts.FieldMaxTokenNameLength, val); err != nil {
+			return diag.Errorf("error setting state key '%s': %s", consts.FieldMaxTokenNameLength, err)
 		}
 	}
 
@@ -262,15 +307,15 @@ func readNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData, 
 		return nil
 	}
 
-	if val, ok := resp.Data["max_ttl"]; ok {
-		if err := d.Set("max_ttl", val); err != nil {
-			return diag.Errorf("error setting state key 'max_ttl': %s", err)
+	if val, ok := resp.Data[consts.FieldMaxTTL]; ok {
+		if err := d.Set(consts.FieldMaxTTL, val); err != nil {
+			return diag.Errorf("error setting state key '%s': %s", consts.FieldMaxTTL, err)
 		}
 	}
 
-	if val, ok := resp.Data["ttl"]; ok {
-		if err := d.Set("ttl", val); err != nil {
-			return diag.Errorf("error setting state key 'ttl': %s", err)
+	if val, ok := resp.Data[consts.FieldTTL]; ok {
+		if err := d.Set(consts.FieldTTL, val); err != nil {
+			return diag.Errorf("error setting state key '%s': %s", consts.FieldTTL, err)
 		}
 	}
 
@@ -300,28 +345,38 @@ func updateNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData
 	configPath := fmt.Sprintf("%s/config/access", backend)
 	log.Printf("[DEBUG] Updating %q", configPath)
 
-	if raw, ok := d.GetOk("address"); ok {
-		data["address"] = raw
+	if raw, ok := d.GetOk(consts.FieldAddress); ok {
+		data[consts.FieldAddress] = raw
 	}
 
-	if raw, ok := d.GetOk("ca_cert"); ok {
-		data["ca_cert"] = raw
+	if raw, ok := d.GetOk(consts.FieldCACert); ok {
+		data[consts.FieldCACert] = raw
 	}
 
-	if raw, ok := d.GetOk("client_cert"); ok {
-		data["client_cert"] = raw
+	if raw, ok := d.GetOk(consts.FieldClientCert); ok {
+		data[consts.FieldClientCert] = raw
 	}
 
-	if raw, ok := d.GetOk("client_key"); ok {
-		data["client_key"] = raw
+	if raw, ok := d.GetOk(consts.FieldClientKey); ok {
+		data[consts.FieldClientKey] = raw.(string)
+	} else if d.HasChange(consts.FieldClientKeyWOVersion) {
+		clientKeyWo, _ := d.GetRawConfigAt(cty.GetAttrPath(consts.FieldClientKeyWO))
+		if !clientKeyWo.IsNull() {
+			data[consts.FieldClientKey] = clientKeyWo.AsString()
+		}
 	}
 
-	if raw, ok := d.GetOk("max_token_name_length"); ok {
-		data["max_token_name_length"] = raw
+	if raw, ok := d.GetOk(consts.FieldMaxTokenNameLength); ok {
+		data[consts.FieldMaxTokenNameLength] = raw
 	}
 
-	if raw, ok := d.GetOk("token"); ok {
-		data["token"] = raw
+	if raw, ok := d.GetOk(consts.FieldToken); ok {
+		data[consts.FieldToken] = raw.(string)
+	} else if d.HasChange(consts.FieldTokenWOVersion) {
+		tokenWo, _ := d.GetRawConfigAt(cty.GetAttrPath(consts.FieldTokenWO))
+		if !tokenWo.IsNull() {
+			data[consts.FieldToken] = tokenWo.AsString()
+		}
 	}
 
 	if _, err := client.Logical().Write(configPath, data); err != nil {
@@ -334,12 +389,12 @@ func updateNomadAccessConfigResource(ctx context.Context, d *schema.ResourceData
 
 	dataLease := map[string]interface{}{}
 
-	if raw, ok := d.GetOk("max_ttl"); ok {
-		dataLease["max_ttl"] = raw
+	if raw, ok := d.GetOk(consts.FieldMaxTTL); ok {
+		dataLease[consts.FieldMaxTTL] = raw
 	}
 
-	if raw, ok := d.GetOk("ttl"); ok {
-		dataLease["ttl"] = raw
+	if raw, ok := d.GetOk(consts.FieldTTL); ok {
+		dataLease[consts.FieldTTL] = raw
 	}
 
 	if _, err := client.Logical().Write(configLeasePath, dataLease); err != nil {
