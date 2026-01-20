27b71e1c347dc3299edea7024100307c3d3c2cd4
Srikrishna Iyer <srikrishna.iyer@hashicorp.com>
2026-01-20 17:13:06 +0530

    Remove duplicates

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -619,8 +619,6 @@ const (
 	FieldDataJSONWOVersion          = "data_json_wo_version"
 	FieldPrivateKeyWO               = "private_key_wo"
 	FieldPrivateKeyWOVersion        = "private_key_wo_version"
-	FieldSecretKeyWO                = "secret_key_wo"
-	FieldSecretKeyWOVersion         = "secret_key_wo_version"
 	FieldOIDCClientSecretWO         = "oidc_client_secret_wo"
 	FieldOIDCClientSecretWOVersion  = "oidc_client_secret_wo_version"
 	FieldServiceAccountJWTWO        = "service_account_jwt_wo"
