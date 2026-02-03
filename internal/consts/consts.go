247d17b6abdaa71a774c66d15d8627d975925dca
Tejashwini Ingalagi <tejashwini.ingalagi@hashicorp.com>
2026-02-03 14:47:23 +0530

    duplicate field name removed

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -476,7 +476,6 @@ const (
 	FieldAllowedManagedKeys             = "allowed_managed_keys"
 	FieldIdentityTokenKey               = "identity_token_key"
 	FieldCIDRList                       = "cidr_list"
-	FieldTokenBoundCIDRs                = "token_bound_cidrs"
 	FieldSecretID                       = "secret_id"
 	FieldSecretIDWO                     = "secret_id_wo"
 	FieldSecretIDWOVersion              = "secret_id_wo_version"
