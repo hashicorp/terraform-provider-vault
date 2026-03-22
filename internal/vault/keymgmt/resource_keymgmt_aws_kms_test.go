3e512f0625484e1f8ef2c1b5425dcf7ce714b627
Santosh Yelamarthi <santosh.yelamarthi@hashicorp.com>
2026-03-22 23:02:21 +0530

    Updated tests

--- internal/vault/keymgmt/resource_keymgmt_aws_kms_test.go
+++ internal/vault/keymgmt/resource_keymgmt_aws_kms_test.go
@@ -5,6 +5,8 @@ package keymgmt_test
 
 import (
 	"fmt"
+	"os"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
@@ -137,7 +139,112 @@ func TestAccKeymgmtAWSKMS_multiple(t *testing.T) {
 	})
 }
 
-func testAccKeymgmtAWSKMSImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
+func TestAccKeymgmtAWSKMS_namespace(t *testing.T) {
+	accessKey, secretKey := testutil.GetTestAWSCreds(t)
+	awsRegion := testutil.GetTestAWSRegion(t)
+
+	mount := acctest.RandomWithPrefix("tf-test-keymgmt")
+	kmsName := acctest.RandomWithPrefix("awskms")
+	namespace := acctest.RandomWithPrefix("test-namespace")
+	resourceType := "vault_keymgmt_aws_kms"
+	resourceName := resourceType + ".test"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
+		PreCheck: func() {
+			acctestutil.TestEntPreCheck(t)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testKeymgmtAWSKMSConfigNamespace(namespace, mount, kmsName, awsRegion, accessKey, secretKey),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldMount, mount),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldName, kmsName),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldKeyCollection, awsRegion),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldNamespace, namespace),
+				),
+			},
+			{
+				ResourceName:                         resourceName,
+				ImportState:                          true,
+				ImportStateIdFunc:                    testAccKeymgmtAWSKMSImportStateIdFunc(resourceName),
+				ImportStateVerify:                    true,
+				ImportStateVerifyIdentifierAttribute: consts.FieldMount,
+				ImportStateVerifyIgnore: []string{
+					consts.FieldCredentialsWO,
+					consts.FieldCredentialsWOVersion,
+				},
+				PreConfig: func() {
+					t.Setenv(consts.EnvVarVaultNamespaceImport, namespace)
+				},
+			},
+			{
+				// Cleanup step: unset the env var and verify no drift
+				Config:   testKeymgmtAWSKMSConfigNamespace(namespace, mount, kmsName, awsRegion, accessKey, secretKey),
+				PlanOnly: true,
+				PreConfig: func() {
+					os.Unsetenv(consts.EnvVarVaultNamespaceImport)
+				},
+			},
+		},
+	})
+}
+
+func TestAccKeymgmtAWSKMS_credentialRotation(t *testing.T) {
+	accessKey, secretKey := testutil.GetTestAWSCreds(t)
+	awsRegion := testutil.GetTestAWSRegion(t)
+
+	mount := acctest.RandomWithPrefix("tf-test-keymgmt")
+	kmsName := acctest.RandomWithPrefix("awskms")
+	resourceType := "vault_keymgmt_aws_kms"
+	resourceName := resourceType + ".test"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
+		PreCheck:                 func() { acctestutil.TestEntPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testKeymgmtAWSKMSConfig(mount, kmsName, awsRegion, accessKey, secretKey),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldMount, mount),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldName, kmsName),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldCredentialsWOVersion, "1"),
+					// credentials_wo is write-only and must never appear in state
+					resource.TestCheckNoResourceAttr(resourceName, consts.FieldCredentialsWO),
+				),
+			},
+			{
+				Config: testKeymgmtAWSKMSConfigWithVersion(mount, kmsName, awsRegion, accessKey, secretKey, 2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, consts.FieldMount, mount),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldName, kmsName),
+					resource.TestCheckResourceAttr(resourceName, consts.FieldCredentialsWOVersion, "2"),
+					// credentials_wo must remain absent from state after credential rotation
+					resource.TestCheckNoResourceAttr(resourceName, consts.FieldCredentialsWO),
+				),
+			},
+		},
+	})
+}
+
+func TestAccKeymgmtAWSKMS_invalidMount(t *testing.T) {
+	accessKey, secretKey := testutil.GetTestAWSCreds(t)
+	awsRegion := testutil.GetTestAWSRegion(t)
+	kmsName := acctest.RandomWithPrefix("awskms")
+
+	resource.Test(t, resource.TestCase{
+		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
+		PreCheck:                 func() { acctestutil.TestEntPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config:      testKeymgmtAWSKMSConfigInvalidMount(kmsName, awsRegion, accessKey, secretKey),
+				ExpectError: regexp.MustCompile(".+"),
+			},
+		},
+	})
+}
+
+func testAccKeymgmtAWSKMSImportStateIdFunc(resourceName string) resource.ImportStateIdFunc{
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[resourceName]
 		if !ok {
@@ -217,4 +324,68 @@ resource "vault_keymgmt_aws_kms" "test2" {
 }
 `, mount, kmsName1, accessKey, secretKey,
 		kmsName2, accessKey, secretKey)
+}
+
+func testKeymgmtAWSKMSConfigNamespace(namespace, mount, kmsName, region, accessKey, secretKey string) string {
+	return fmt.Sprintf(`
+resource "vault_namespace" "test" {
+  path = %q
+}
+
+resource "vault_mount" "keymgmt" {
+  namespace = vault_namespace.test.path
+  path      = %q
+  type      = "keymgmt"
+}
+
+resource "vault_keymgmt_aws_kms" "test" {
+  namespace      = vault_namespace.test.path
+  mount          = vault_mount.keymgmt.path
+  name           = %q
+  key_collection = %q
+
+  credentials_wo = {
+    access_key = "%s"
+    secret_key = "%s"
+  }
+  credentials_wo_version = 1
+}
+`, namespace, mount, kmsName, region, accessKey, secretKey)
+}
+
+func testKeymgmtAWSKMSConfigWithVersion(mount, kmsName, region, accessKey, secretKey string, version int) string {
+	return fmt.Sprintf(`
+resource "vault_mount" "keymgmt" {
+  path = %q
+  type = "keymgmt"
+}
+
+resource "vault_keymgmt_aws_kms" "test" {
+  mount          = vault_mount.keymgmt.path
+  name           = %q
+  key_collection = %q
+
+  credentials_wo = {
+    access_key = "%s"
+    secret_key = "%s"
+  }
+  credentials_wo_version = %d
+}
+`, mount, kmsName, region, accessKey, secretKey, version)
+}
+
+func testKeymgmtAWSKMSConfigInvalidMount(kmsName, region, accessKey, secretKey string) string {
+	return fmt.Sprintf(`
+resource "vault_keymgmt_aws_kms" "test" {
+  mount          = "nonexistent-mount"
+  name           = %q
+  key_collection = %q
+
+  credentials_wo = {
+    access_key = "%s"
+    secret_key = "%s"
+  }
+  credentials_wo_version = 1
+}
+`, kmsName, region, accessKey, secretKey)
 }
