50d042674ff53582c5b922f3cb64287edf75f051
Tejashwini Ingalagi <tejashwini.ingalagi@hashicorp.com>
2026-03-31 12:35:22 +0530

    Vault 42758 alicloud ephemeral implementation (#2824)
    * alicloud ephemeral implementation

--- internal/provider/fwprovider/provider.go
+++ internal/provider/fwprovider/provider.go
@@ -271,7 +271,7 @@ func (p *fwprovider) EphemeralResources(_ context.Context) []func() ephemeral.Ep
 		ephemeralsecrets.NewGCPOAuth2AccessTokenEphemeralResource,
 		ephemeralsecrets.NewAWSAccessCredentialsEphemeralSecretResource,
 		ephemeralsecrets.NewAWSStaticAccessCredentialsEphemeralSecretResource,
-		alicloud.NewAliCloudAccessCredentialsEphemeralResource,
+		ephemeralsecrets.NewAliCloudAccessCredentialsEphemeralResource,
 		ephemeralauth.NewApproleAuthBackendRoleSecretIDEphemeralResource,
 		ephemeralsecrets.NewKubernetesServiceAccountTokenEphemeralResource,
 		cloudfoundry.NewCFAuthLoginEphemeralResource,
