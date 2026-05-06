68ddf32a45617ef0cb3bf2796083cb703085acb6
Siddharth Kannan <siddharth.kannan@ibm.com>
2026-05-06 16:43:18 +0530

    added testcase for valid signatures

--- internal/vault/secrets/gcpkms/gcpkms_verify_datasource_test.go
+++ internal/vault/secrets/gcpkms/gcpkms_verify_datasource_test.go
@@ -43,6 +43,34 @@ func TestGCPKMSVerifyDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestGCPKMSVerifyDataSource_validSignature(t *testing.T) {
+	credentials, keyRing := testutil.GetTestGCPKMSCreds(t)
+
+	path := acctest.RandomWithPrefix("tf-test-gcpkms")
+	keyName := acctest.RandomWithPrefix("test-key")
+
+	dataSourceType := "vault_gcpkms_verify"
+	dataSourceName := "data." + dataSourceType + ".test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctestutil.TestAccPreCheck(t) },
+		ProtoV5ProviderFactories: providertest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testGCPKMSVerifyDataSource_validSignatureConfig(path, keyName, keyRing, credentials),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, consts.FieldMount, path),
+					resource.TestCheckResourceAttr(dataSourceName, consts.FieldKeyName, keyName),
+					resource.TestCheckResourceAttr(dataSourceName, consts.FieldKeyVersion, "1"),
+					resource.TestCheckResourceAttrSet(dataSourceName, consts.FieldDigest),
+					resource.TestCheckResourceAttrSet(dataSourceName, consts.FieldSignature),
+					resource.TestCheckResourceAttr(dataSourceName, consts.FieldValid, "true"),
+				),
+			},
+		},
+	})
+}
+
 func TestGCPKMSVerifyDataSource_namespace(t *testing.T) {
 	credentials, keyRing := testutil.GetTestGCPKMSCreds(t)
 
@@ -176,4 +204,60 @@ data "vault_gcpkms_verify" "test" {
   signature   = "dGVzdC1zaWduYXR1cmU="
 }
 `, path, credentials, keyName, keyRing)
+}
+
+func testGCPKMSVerifyDataSource_validSignatureConfig(path, keyName, keyRing, credentials string) string {
+	// Pre-compute a valid digest and signature pair
+	// This uses base64sha256 of "test data for verification"
+	// Digest: base64(sha256("test data for verification"))
+	testDigest := "YBbp+LPAKcJjVz0JvKs0YJvVKN8dGZ3qN0Y5vF5XYXQ="
+
+	return fmt.Sprintf(`
+resource "vault_mount" "test" {
+  path = "%s"
+  type = "gcpkms"
+}
+
+resource "vault_gcpkms_secret_backend" "test" {
+  mount                  = vault_mount.test.path
+  credentials_wo         = <<-EOT
+%s
+EOT
+  credentials_wo_version = 1
+}
+
+resource "vault_gcpkms_secret_backend_key" "test" {
+  mount            = vault_mount.test.path
+  name             = "%s"
+  key_ring         = "%s"
+  purpose          = "asymmetric_sign"
+  algorithm        = "rsa_sign_pss_2048_sha256"
+  protection_level = "software"
+}
+
+# Use vault_generic_endpoint to sign the data
+resource "vault_generic_endpoint" "sign" {
+  depends_on           = [vault_gcpkms_secret_backend_key.test]
+  path                 = "${vault_mount.test.path}/sign/${vault_gcpkms_secret_backend_key.test.name}"
+  disable_read         = true
+  disable_delete       = true
+  ignore_absent_fields = true
+  write_fields         = ["signature"]
+  
+  data_json = jsonencode({
+    key_version = 1
+    digest      = "%s"
+  })
+}
+
+# Verify the signature using the data source
+data "vault_gcpkms_verify" "test" {
+  depends_on  = [vault_generic_endpoint.sign]
+  mount       = vault_mount.test.path
+  key_name    = vault_gcpkms_secret_backend_key.test.name
+  key_version = 1
+  digest      = "%s"
+  signature   = jsondecode(vault_generic_endpoint.sign.write_data_json)["signature"]
+}
+`, path, credentials, keyName, keyRing, testDigest, testDigest)
 }
