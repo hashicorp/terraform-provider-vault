32a6d5fb7d0092061f4c8a8c76cb7649c2a4be55
claire bontempo <cbontempo@hashicorp.com>
2026-05-18 13:10:29 -0700

    add new formats for vault versions >= 2.1.0

--- vault/resource_pki_secret_backend_cert.go
+++ vault/resource_pki_secret_backend_cert.go
@@ -95,7 +95,7 @@ func pkiSecretBackendCertResource() *schema.Resource {
 				Description:  "The format of data.",
 				ForceNew:     true,
 				Default:      "pem",
-				ValidateFunc: validation.StringInSlice([]string{"pem", "der", "pem_bundle"}, false),
+				ValidateFunc: validation.StringInSlice([]string{"pem", "der", "pem_bundle", "pkcs12_bundle", "jks_bundle"}, false),
 			},
 			consts.FieldPrivateKeyFormat: {
 				Type:         schema.TypeString,
@@ -104,6 +104,48 @@ func pkiSecretBackendCertResource() *schema.Resource {
 				ForceNew:     true,
 				Default:      "der",
 				ValidateFunc: validation.StringInSlice([]string{"der", "pkcs8"}, false),
+			},
+			consts.FieldPKCS12Password: {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Password for encrypting the PKCS#12 
+		archive when format is set to "pkcs12_bundle". If not provided, 
+		defaults to "changeit". It is recommended to use the default password
+		and protect the file using other means or use a high-entropy password.`,
+				ForceNew: true,
+				Default:  "changeit",
+			},
+			consts.FieldPKCS12Encoder: {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Encoder profile to use for PKCS#12 archives when 
+format is set to "pkcs12_bundle". Valid values are "modern2026" and 
+"modern2023". Defaults to "modern2026", which uses the newer PKCS#12 
+integrity format (PBMAC1).`,
+				ForceNew:     true,
+				Default:      "modern2026",
+				ValidateFunc: validation.StringInSlice([]string{"modern2026", "modern2023"}, false),
+			},
+			consts.FieldJKSPassword: {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Password for encrypting the Java keystore
+		when format is set to "jks_bundle". If not provided, 
+		defaults to "changeit". It is recommended to use the default password
+		and protect the file using other means or use a high-entropy password.`,
+				ForceNew: true,
+				Default:  "changeit",
+			},
+			consts.FieldJKSAlias: {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `The entry alias in the Java keystore (JKS) when format is set to "jks_bundle" 
+				and bundle contains a single PrivateKeyEntry. This field is case-sensitive, but relying
+			on case-only differences for unique aliases is not recommended. Defaults to "1".
+			This parameter is ignored by endpoints that return multiple TrustedCertificateEntry values
+			(trust stores), and entry aliases are assigned incrementing numeric strings starting at "1".`,
+				ForceNew: true,
+				Default:  "1",
 			},
 			consts.FieldExcludeCNFromSans: {
 				Type:        schema.TypeBool,
@@ -237,6 +279,14 @@ func pkiSecretBackendCertCreate(ctx context.Context, d *schema.ResourceData, met
 		consts.FieldFormat,
 		consts.FieldPrivateKeyFormat,
 		consts.FieldNotAfter,
+	}
+	// Only add additional format parameters if supported
+	if provider.IsAPISupported(meta, provider.VaultVersion210) {
+		certAPIFields = append(certAPIFields,
+			consts.FieldPKCS12Password,
+			consts.FieldPKCS12Encoder,
+			consts.FieldJKSPassword,
+			consts.FieldJKSAlias)
 	}
 
 	certBooleanAPIFields := []string{
