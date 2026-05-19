94cabb3c55c6e6cc983f466e1c8b5ddf48e81172
claire bontempo <cbontempo@hashicorp.com>
2026-05-19 10:27:09 -0700

    add to backend_root resource

--- vault/resource_pki_secret_backend_root_cert_test.go
+++ vault/resource_pki_secret_backend_root_cert_test.go
@@ -86,6 +86,50 @@ func TestPkiSecretBackendRootCertificate_notAfter(t *testing.T) {
 	testPkiSecretBackendRootCertificate(t, path, config, resourceName, checks, nil)
 }
 
+func TestPkiSecretBackendRootCertificate_pkcs12Format(t *testing.T) {
+	path := "pki-" + strconv.Itoa(acctest.RandInt())
+
+	resourceName := "vault_pki_secret_backend_root_cert.test"
+	config := testPkiSecretBackendRootCertificateConfig_basic(path, `
+  format            = "pkcs12_bundle"
+  pkcs12_password   = "123-secure-password"
+  pkcs12_encoder    = "modern2023"`)
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(resourceName, consts.FieldBackend, path),
+		resource.TestCheckResourceAttr(resourceName, consts.FieldFormat, "pkcs12_bundle"),
+		resource.TestCheckResourceAttr(resourceName, consts.FieldPKCS12Password, "123-secure-password"),
+		resource.TestCheckResourceAttr(resourceName, consts.FieldPKCS12Encoder, "modern2023"),
+		resource.TestCheckResourceAttrSet(resourceName, consts.FieldSerialNumber),
+	}
+
+	testPkiSecretBackendRootCertificate(t, path, config, resourceName, checks, func(t *testing.T) {
+		SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion210)
+	})
+}
+
+func TestPkiSecretBackendRootCertificate_jksFormat(t *testing.T) {
+	path := "pki-" + strconv.Itoa(acctest.RandInt())
+
+	resourceName := "vault_pki_secret_backend_root_cert.test"
+	config := testPkiSecretBackendRootCertificateConfig_basic(path, `
+  format            = "jks_bundle"
+  jks_password      = "super-secure-password"
+  jks_alias         = "myapp"`)
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(resourceName, consts.FieldBackend, path),
+		resource.TestCheckResourceAttr(resourceName, consts.FieldFormat, "jks_bundle"),
+		resource.TestCheckResourceAttr(resourceName, consts.FieldJKSPassword, "super-secure-password"),
+		resource.TestCheckResourceAttr(resourceName, consts.FieldJKSAlias, "myapp"),
+		resource.TestCheckResourceAttrSet(resourceName, consts.FieldSerialNumber),
+	}
+
+	testPkiSecretBackendRootCertificate(t, path, config, resourceName, checks, func(t *testing.T) {
+		SkipIfAPIVersionLT(t, testProvider.Meta(), provider.VaultVersion210)
+	})
+}
+
 // TestPkiSecretBackendRootCertificate_usePSS tests the use_pss field
 func TestPkiSecretBackendRootCertificate_usePSS(t *testing.T) {
 	path := "pki-" + strconv.Itoa(acctest.RandInt())
@@ -337,9 +381,6 @@ func checkCertificateNameConstraints(resourceName string, s *terraform.State) er
 		}
 
 		b, _ := pem.Decode([]byte(certPEM))
-		if err != nil {
-			return err
-		}
 
 		cert, err = x509.ParseCertificate(b.Bytes)
 		if err != nil {
@@ -493,7 +534,7 @@ func TestPkiSecretBackendRootCertificate_multiIssuer(t *testing.T) {
 			{
 				Config: testPkiSecretBackendRootCertificateConfig_multiIssuerInternal(path, issuerName, keyName),
 				Check: resource.ComposeTestCheckFunc(
-					append(internalChecks)...,
+					internalChecks...,
 				),
 			},
 			{
@@ -572,7 +613,7 @@ func TestPkiSecretBackendRootCertificate_managedKeys(t *testing.T) {
 			{
 				Config: testPkiSecretBackendRootCertificateConfig_managedKeys(path, managedKeyName, accessKey, secretKey),
 				Check: resource.ComposeTestCheckFunc(
-					append(checks)...,
+					checks...,
 				),
 			},
 		},
@@ -594,7 +635,6 @@ resource "vault_pki_secret_backend_root_cert" "test" {
   backend              = vault_mount.test.path
   type                 = "internal"
   common_name          = "test Root CA"
-  format               = "pem"
   private_key_format   = "der"
   key_type             = "rsa"
   key_bits             = 4096
